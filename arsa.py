bb0f0ad5e69b929ef11289f49ed37b5966d71eaa
Gerion Entrup <entrup@sra.uni-hannover.de>
2018-10-24 16:13:56 +0200

    arsa.py: PEP8

--- arsa.py
+++ arsa.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# vim: set et ts=4 sw=4:
 
 """Automated Realtime System Analysis"""
 
@@ -15,61 +16,71 @@ import stepmanager
 
 from steps import OilStep
 
-def execute_shellcommands(commands,shell_flag):
-	"""execute_shellcommands is used to  generate the makefile, build the pass and run the pass on the application code
-	"""
-	try:
-		proc = subprocess.Popen(commands,shell=shell_flag,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-		stdout, stderr = proc.communicate()
-		print(stdout)
 
-		if proc.returncode != 0:
+def execute_shellcommands(commands, shell_flag):
+    """execute_shellcommands is used to  generate the makefile, build the pass
+    and run the pass on the application code
+    """
+    try:
+        proc = subprocess.Popen(commands, shell=shell_flag,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        stdout, stderr = proc.communicate()
+        print(stdout)
 
-			logging.error('Call of: '+  " ".join(str(x) for x in commands) + '\nfailed with:')
-			stderr = stderr.decode('utf8').strip('\n')
-			for line in stderr.split('\n'):
-				logging.error(line)
+        if proc.returncode != 0:
 
-	except subprocess.CalledProcessError as e:
-		raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
-	return
+            logging.error('Call of: ' + " ".join(str(x) for x in commands) +
+                          '\nfailed with:')
+            stderr = stderr.decode('utf8').strip('\n')
+            for line in stderr.split('\n'):
+                logging.error(line)
+
+    except subprocess.CalledProcessError:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+    return
 
 
 
 def main():
+    """Entry point for ARSA."""
 
-	parser = argparse.ArgumentParser(prog=sys.argv[0],
-									description=sys.modules[__name__].__doc__)
-	parser.add_argument('--verbose', '-v', help="be verbose",
-						action="store_true", default=False)
-	parser.add_argument('--os', '-O', help="specify the operation system",
-						choices=['freertos', 'osek'], default='osek')
-	parser.add_argument('input_files', help="all LLVM-IR input files",
-						nargs='+')
-	
-	parser.add_argument('--application_file', help="application file which shalle be transformed in .ll",
-					nargs='+')
-	
-	args = parser.parse_args()
-	print("TEST")
-	#generate IR (.ll file) of application in dependency of the transmitted argument
-	if args.os == "freertos":
-		folder = "FreeRTOS"
-	else:
-		folder = "OSEK"
+    parser = argparse.ArgumentParser(prog=sys.argv[0],
+                                     description=sys.modules[__name__].__doc__)
+    parser.add_argument('--verbose', '-v', help="be verbose",
+                        action="store_true", default=False)
+    parser.add_argument('--os', '-O', help="specify the operation system",
+                        choices=['freertos', 'osek'], default='osek')
+    parser.add_argument('input_files', help="all LLVM-IR input files",
+                        nargs='+')
 
-	print(args.application_file)
-	
-	commands = ["clang-6.0", "-S", "-emit-llvm", "../appl/" + folder + "/g.cc" , "--std=c++11",  "-o", "../test/data/appl.ll", "-target", "i386-pc-linux-gnu"]
+    parser.add_argument('--application_file',
+                        help="application file which shall be transformed in .ll",
+                        nargs='+')
 
-	execute_shellcommands(commands, False)
-	
-	g = graph.PyGraph()
+    args = parser.parse_args()
+    print("TEST")
+    # generate IR (.ll file) of application in dependency
+    # of the transmitted argument
+    if args.os == "freertos":
+        folder = "FreeRTOS"
+    else:
+        folder = "OSEK"
 
-	p_manager = stepmanager.StepManager(g, vars(args))
+    print(args.application_file)
 
-	p_manager.execute(['LLVMStep'])
-	
+    commands = ["clang-6.0", "-S", "-emit-llvm", "../appl/" + folder + "/g.cc",
+                "--std=c++11", "-o", "../test/data/appl.ll",
+                "-target", "i386-pc-linux-gnu"]
+
+    execute_shellcommands(commands, False)
+
+    g = graph.PyGraph()
+
+    p_manager = stepmanager.StepManager(g, vars(args))
+
+    p_manager.execute(['LLVMStep'])
+
 
 if __name__ == '__main__':
-	main()
+    main()
