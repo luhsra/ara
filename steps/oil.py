708ccc1531d9821a2140b008f3070bc08c7384ac
Gerion Entrup <entrup@sra.uni-hannover.de>
2019-05-06 15:36:39 +0200

    oilstep: correct reading of json dict

--- steps/oil.py
+++ steps/oil.py
@@ -11,8 +11,7 @@ from native_step import Step
 class OilStep(Step):
     """Reads an oil file and writes all information to the graph."""
 
-    @staticmethod
-    def fill_graph(g, oil: Dict[str, Any], inst, inst_class, attrs=None):
+    def fill_graph(self, g, oil: Dict[str, Any], inst, inst_class, attrs=None):
         """Create new graph instance.
 
         Arguments:
@@ -25,17 +24,21 @@ class OilStep(Step):
         attrs -- optional dict of attributes and setter functions
                  Used to set all instance attributes.
         """
+        self._log.debug(f"Filling {inst}")
         for elem in oil.get(inst, []):
+            self._log.debug(f"Found in oil: {elem}")
             vertex = inst_class(g, elem['name'])
             if attrs is not None:
                 for attr in attrs:
-                    attrs[attr](elem[attr])
+                    attrs[attr](vertex, elem[attr])
             g.set_vertex(vertex)
 
     def run(self, g: graph.PyGraph):
         # load the json outputstructure with json
         with open(self._config['oil']) as f:
             oil = json.load(f)
+        assert("cpu" in oil)
+        oil = oil["cpu"]
 
         # TODO validate json
 
