15c0b3d427ea0acb9ef9dffd381fbfe7b8ad3e92
Gerion Entrup <entrup@sra.uni-hannover.de>
2019-05-06 16:19:33 +0200

    test/oil: better test

--- test/oil/oil.py
+++ test/oil/oil.py
@@ -1,22 +1,61 @@
 #!/usr/bin/env python3.6
 
 import logging
-import sys
+import os.path
 
 import stepmanager
 import graph
 
 from native_step import Step
 
+TASKS_1 = {
+    'Handler11': {
+        "schedule": True,
+        "priority": 4,
+        "activation": 1,
+        "autostart": True
+    },
+    "Handler12": {
+        "schedule": True,
+        "priority": 3,
+        "activation": 1,
+        "autostart": False
+    },
+    "Handler13": {
+        "schedule": True,
+        "priority": 5,
+        "activation": 1,
+        "autostart": False
+    }}
+
+
+def validate_1(g):
+    """Validate 1.oil."""
+    tasks = g.get_type_vertices('Task')
+
+    for t in tasks:
+        assert t.get_name() in TASKS_1
+        task = TASKS_1[t.get_name()]
+        assert t.is_scheduled() == task['schedule']
+        assert t.get_priority() == task['priority']
+        assert t.is_autostarted() == task['autostart']
+        assert t.get_activation() == task['activation']
+
+
+TESTS = [('1.oil', validate_1)]
+
 def main():
+    s_dir = os.path.dirname(os.path.realpath(__file__))
+
     logging.basicConfig(level=logging.DEBUG)
-    g = graph.PyGraph()
-    config = {'oil': sys.argv[1], 'os': 'osek'}
-    p_manager = stepmanager.StepManager(g, config)
+    for oil, validate in TESTS:
+        g = graph.PyGraph()
+        config = {'oil': os.path.join(s_dir, oil), 'os': 'osek'}
+        p_manager = stepmanager.StepManager(g, config)
 
-    p_manager.execute(['OilStep'])
+        p_manager.execute(['OilStep'])
 
-    print(g)
+        validate(g)
 
 
 if __name__ == '__main__':
