a724d98a71348bd79025d97ecdd6135f947741e4
Gerion Entrup <entrup@sra.uni-hannover.de>
2019-05-16 16:23:11 +0200

    steps/python_validation: formatting

--- steps/python_validation.py
+++ steps/python_validation.py
@@ -13,9 +13,9 @@ from functools import reduce
 
 
 def validate_syscalls( valid_calls,vertex, inverse):
-    
+
     for outgoing_edge in vertex.get_outgoing_edges():
-        
+
         valid = False
         #check if the syscall is in list of valid syscalls
         for valid_call in valid_calls:
@@ -23,31 +23,31 @@ def validate_syscalls( valid_calls,vertex, inverse):
             target_class = valid_call[0]
             #target syscall type
             syscall_type = valid_call[1]
-            
+
             abb = outgoing_edge.get_abb_reference()
-            
+
             abb_syscall_type = abb.get_syscall_type()
-            
+
             if syscall_type == abb_syscall_type:
-                
+
                 valid = True
                 break
-        
+
         if inverse == valid:
             print("Warning: invalid syscall typ", syscall_type, "in vertex" , vertex.get_name())
             break
 
 
 def validate_osek_syscalls_in_different_abstractions( g: graph.PyGraph ):
-    
+
     isr_list = g.get_type_vertices("ISR")
-        
+
     #iterate about the isrs
     for isr in isr_list:
-        
+
         #different syscalls allwoend in isrs from different type
         if isr.get_category() == 1:
-            
+
             #list of all valid syscalls
             valid_calls = [ (graph.get_type_hash("RTOS"),graph.syscall_definition_type.enable),
                             (graph.get_type_hash("RTOS"), graph.syscall_definition_type.disable),
@@ -56,9 +56,9 @@ def validate_osek_syscalls_in_different_abstractions( g: graph.PyGraph ):
             ]
             validate_syscalls(valid_calls,isr,False)
 
-        
+
         elif isr.get_category() == 2:
-            
+
             #list of all invalid syscalls
             valid_calls = [ (graph.get_type_hash("RTOS"), graph.syscall_definition_type.start_scheduler),
                             (graph.get_type_hash("Event"), graph.syscall_definition_type.receive),
@@ -68,23 +68,23 @@ def validate_osek_syscalls_in_different_abstractions( g: graph.PyGraph ):
                             (graph.get_type_hash("RTOS"), graph.syscall_definition_type.schedule)
             ]
             validate_syscalls(valid_calls,isr,True)
-    
+
     task_list = g.get_type_vertices("Task")
-    
+
     #iterate about the tasks
     for task in task_list:
-        
+
             #list of all invalid syscalls
             valid_calls = [ (graph.get_type_hash("RTOS"), graph.syscall_definition_type.start_scheduler),
 
             ]
             validate_syscalls(valid_calls,task,True)
-    
+
     hook_list = g.get_type_vertices("Hook")
-    
+
     #iterate about the hooks
     for hook in hook_list:
-        
+
         if hook.get_hook_type() != graph.hook_type.no_hook:
             #list of all valid syscalls
             valid_calls = [ (graph.get_type_hash("RTOS"), graph.syscall_definition_type.start_scheduler),
@@ -98,23 +98,23 @@ def validate_osek_syscalls_in_different_abstractions( g: graph.PyGraph ):
 
 def validate_osek_task_termination(g: graph.PyGraph ):
     task_list = g.get_type_vertices("Task")
-        
+
     #iterate about the tasks
     for task in task_list:
-        #get last outgoing syscall 
+        #get last outgoing syscall
         edges = task.get_outgoing_edges()
         if not edges:
             continue
         last_syscall = edges[-1]
         #get abb reference from syscall
         abb_reference = last_syscall.get_abb_reference()
-        if abb_reference is not None: 
+        if abb_reference is not None:
             #check if last outgoing syscall is a termination or chain task syscall
             syscall_type = abb_reference.get_syscall_type()
             if syscall_type is not graph.syscall_definition_type.destroy and syscall_type is not graph.syscall_definition_type.chain:
                 print("Warning: No termination or chain as last syscall in task", task.get_name())
-            
-            #TODO check that termination/chain syscall postdominates all other 
+
+            #TODO check that termination/chain syscall postdominates all other
             #else:
                 ##terminate or chain syscall detected, so this syscall has to postdominate all other
                 #for syscall in edges:
@@ -124,37 +124,37 @@ def validate_osek_task_termination(g: graph.PyGraph ):
                         ##check that abb is postdominated by chain/terminate syscall
                         #if not abb_reference.postdominates(tmp_abb_reference):
                             #print("warning")
-        
-        
-        
-        
-    
-    
+
+
+
+
+
+
 
 
 class Python_ValidationStep(Step):
     """Merges the ABB."""
-        
+
     def get_dependencies(self):
-        
+
         return ['ValidationStep']
-    
-    
+
+
 
     def run(self, g: graph.PyGraph):
-        
+
         print("Run PythonValidationStep")
-        
+
         os =  self._config["os"]
-        
+
         if os == "osek":
             validate_osek_syscalls_in_different_abstractions(g)
-            
+
             validate_osek_task_termination(g)
-                    
-                
-        
-            
-          
-                    
-                
+
+
+
+
+
+
+
