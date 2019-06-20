7fbf19c91cdaa7a708108bf96953ffe0b5daa5d8
Gerion Entrup <entrup@sra.uni-hannover.de>
2019-06-20 19:12:41 +0200

    run_test_step: init logging

--- test/native_step_test/run_test_step.py
+++ test/native_step_test/run_test_step.py
@@ -4,7 +4,10 @@ import stepmanager
 import logging
 import sys
 
+from ara import init_logging
+
 from native_step import Step, provide_test_steps, provide_steps
+
 
 def provide(config):
     """Provide all classes for the StepManager."""
@@ -13,9 +16,9 @@ def provide(config):
     for step in provide_test_steps(config):
         yield step
 
+
 def main():
     """Test for correct splitting of basic blocks."""
-    logging.basicConfig(level=logging.DEBUG)
     g = graph.PyGraph()
     os_name = sys.argv[1]
     test_step = sys.argv[2]
@@ -24,6 +27,8 @@ def main():
               'input_files': i_files}
     p_manager = stepmanager.StepManager(g, config,
                                         provides=provide)
+    max_l = max([len(x.get_name()) for x in p_manager.get_steps()])
+    init_logging(logging.DEBUG, max_l)
 
     p_manager.execute([test_step])
 
