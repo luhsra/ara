cd8e25cd631b3b008012cedb1d561a10d330e09d
Björn Fiedler <fiedler@sra.uni-hannover.de>
2019-07-02 15:09:55 +0200

    llvm_basic_optimization: hopefully more debug info

--- steps/native/llvm_basic_optimization.cpp
+++ steps/native/llvm_basic_optimization.cpp
@@ -2,18 +2,21 @@
 
 #include "llvm_basic_optimization.h"
 
-#include <list>
-#include <iostream>
-#include <llvm/IR/BasicBlock.h>
-#include <llvm/IR/Module.h>
-#include <llvm/IR/Instructions.h>
-#include <llvm/Support/raw_os_ostream.h>
-#include <llvm/Support/Casting.h>
 #include "llvm_common.h"
+
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Transforms/InstCombine/InstCombine.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Scalar/GVN.h"
+
+#include <iostream>
+#include <list>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/Casting.h>
+#include <llvm/Support/raw_os_ostream.h>
 
 namespace step {
 	using namespace llvm;
@@ -28,28 +31,29 @@ namespace step {
 	}
 
 	void LLVMBasicOptimization::run(graph::Graph& graph) {
-	  logger.debug() << "hello llvm" << std::endl;
-	  auto module =  graph.get_llvm_module();
-	  auto& theContext = module->getContext();
-	  logger.err() << module << std::endl;
-	  std::unique_ptr<legacy::FunctionPassManager> fpm = make_unique<legacy::FunctionPassManager>(module.get());
-	  // Do simple "peephole" optimizations and bit-twiddling optzns.
-	  fpm->add(createInstructionCombiningPass());
-	  // Reassociate expressions.
-	  fpm->add(createReassociatePass());
-	  // Eliminate Common SubExpressions.
-	  fpm->add(createGVNPass());
-	  // Simplify the control flow graph (deleting unreachable blocks, etc).
-	  fpm->add(createCFGSimplificationPass());
-	  
-	  fpm->doInitialization();
+		setCurrentDebugType("gvn");
+		DebugFlag = true;
+		logger.debug() << "hello llvm" << std::endl;
+		auto module = graph.get_llvm_module();
+		auto& theContext = module->getContext();
+		logger.err() << module << std::endl;
+		std::unique_ptr<legacy::FunctionPassManager> fpm = make_unique<legacy::FunctionPassManager>(module.get());
+		// Do simple "peephole" optimizations and bit-twiddling optzns.
+		fpm->add(createInstructionCombiningPass());
+		// Reassociate expressions.
+		fpm->add(createReassociatePass());
+		// Eliminate Common SubExpressions.
+		fpm->add(createGVNPass());
+		// Simplify the control flow graph (deleting unreachable blocks, etc).
+		fpm->add(createCFGSimplificationPass());
 
-	  for (auto& function : *module) {
-		function.viewCFG();
-		fpm->run(function);
-		function.viewCFG();
-	  }
-	  
-	  
+		fpm->doInitialization();
+
+		for (auto& function : *module) {
+			function.viewCFG();
+			fpm->run(function);
+			function.viewCFG();
+		}
+		fpm->doFinalization();
 	}
 } // namespace step
