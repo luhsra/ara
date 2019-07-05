7be619e219cebaec7b5df5da93d740a0cf3ae935
Björn Fiedler <fiedler@sra.uni-hannover.de>
2019-07-05 10:40:52 +0200

    llvm_basic_optimizations: working

--- appl/FreeRTOS/dead_code.cc
+++ appl/FreeRTOS/dead_code.cc
@@ -1,12 +1,11 @@
 int main(int argc, char ** argv) {
-  //  int foo = 3;
-  //  int bar = 3 + 5;
-  int fob = argc*2 + argc*2;
-//  bar += foo;
-//  if (foo)
-// 	return 9;
-//  else
-// 	return 1;
-//  return 0;
-  return fob;
+	int foo = 3;
+	int bar = 3 + 5;
+	int fob = argc * 2 + argc * 2;
+	bar += foo;
+	if (foo)
+		return fob;
+	else
+		return bar;
+	return fob;
 }
