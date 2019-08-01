7d8ffdb1b6a4899517c38d3da94d53fc88c86402
Gerion Entrup <entrup@sra.uni-hannover.de>
2019-08-01 15:34:59 +0200

    steps/native/util.h: common functions
    unused atm

---
+++ steps/native/util.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+// print std container
+
+template<class T>
+inline std::ostream& operator<<(std::ostream& str, const std::vector<T>& vec) {
+	str << "std::vector(";
+	bool first = true;
+	for (const auto& elem : vec) {
+		if (first) {
+			first = false;
+		} else {
+			str << ", ";
+		}
+		str << elem;
+	}
+	str << ")";
+	return str;
+}
