cc7e26c990637372848b16d4f103206b4a4702d9
Gerion Entrup <entrup@sra.uni-hannover.de>
2019-08-30 14:21:14 +0200

    tests: add syscall test

--- test/syscall.py
+++ test/syscall.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3.6
 import graph
+import json
 
-from graph import ABBType, abb_filter_by
+from graph import ABBType
 
-from init_test import init_test
+from init_test import init_test, fail_if
 
+def f_exp(ty):
+    def actual_filter(abb):
+        return abb.type == ty
+    return actual_filter
 
 def main():
     """Test for correct syscall mapping."""
 
     m_graph, data, _ = init_test(['Syscall'])
-    abbs = m_graph.newgraph.abbs()
+    abbs = m_graph.new_graph.abbs()
     stats = {}
-    #for function in abbs.functions():
-    #    syscalls = sum(1 for _ in (abb_filter_by(function, ABBType.syscall)))
-    #    calls = sum(1 for _ in (abb_filter_by(function, ABBType.call)))
-    #    stats[function.name] = (syscalls, calls)
-    #assert_equal(test_data, stats)
+    for function in abbs.functions():
+        syscalls = sum(1 for _ in
+                       (filter(f_exp(ABBType.syscall), function.vertices())))
+        calls = sum(1 for _ in
+                    (filter(f_exp(ABBType.call), function.vertices())))
+        stats[function.name] = {"syscalls": syscalls, "calls": calls}
+    fail_if(data != stats, "Data not equal")
 
 
 if __name__ == '__main__':
