4354c28a3802509df373f98456af893092948d71
Gerion Entrup <entrup@sra.uni-hannover.de>
2019-09-06 15:01:00 +0200

    add precompiled header support
    PCH are activated per default in Meson. To deactivate them use:
    meson configure -Db_pch=false
    
    PCH seems to be a little bit faster. This are the build times for the
    whole project with and without PCH on my machine:
    
          | no PCH | PCH
    ------+--------+----
    GCC   | 308s   | 232s
    Clang | 359s   | 282s

---
+++ libgraph/pch/graph_pch.h
@@ -0,0 +1,17 @@
+#ifndef ARA_GRAPH_PCH_H
+#define ARA_GRAPH_PCH_H
+
+#include "boost/graph/graph_concepts.hpp"
+#include "boost/iterator/transform_iterator.hpp"
+#include "boost/range/iterator.hpp"
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/subgraph.hpp>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Module.h>
+#include <cassert>
+#include <functional>
+#include <memory>
+#include <type_traits>
+#include <Python.h>
+
+#endif // ARA_GRAPH_PCH_H
