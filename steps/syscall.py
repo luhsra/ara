9a6745899db8f6eb016ad5129d6fa5767f0e76df
Gerion Entrup <entrup@sra.uni-hannover.de>
2019-09-17 12:57:18 +0200

    steps/syscall: label functions also as syscall
    Also handle syscalls via function pointers. Currently very fuzzy.

--- steps/syscall.py
+++ steps/syscall.py
@@ -10,17 +10,26 @@ class Syscall(Step):
     """Label abb call nodes as system call nodes."""
 
     def get_dependencies(self):
-        return ["LLVMMap"]
+        return ["ICFG"]
 
     def run(self, g: graph.PyGraph):
         syscalls = [x['name'] for x in OS_API]
         abbs = g.new_graph.abbs()
+
+        for function in abbs.children():
+            if function.name in syscalls:
+                function.syscall = True
+
         syscall_counter = 0
         for abb in abbs.vertices():
             if abb.type != graph.ABBType.computation:
-                if abb.get_call() in syscalls:
-                    self._log.debug(
-                        f"Found syscall {abb.get_call()} in {abb.name}")
-                    abb.type = graph.ABBType.syscall
-                    syscall_counter += 1
+                for edge in abb.out_edges():
+                    if edge.type == graph.CFType.icf:
+                        func = abbs.get_subgraph(edge.target())
+                        if func.syscall:
+                            self._log.debug(
+                                f"Found syscall {func.name} in {abb.name}")
+                            abb.type = graph.ABBType.syscall
+                            syscall_counter += 1
+
         self._log.info(f"Found {syscall_counter} syscalls.")
