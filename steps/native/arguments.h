63b96f8f9a3909401b8882df7caa6813ff5d743c
Gerion Entrup <entrup@sra.uni-hannover.de>
2019-12-13 11:14:48 +0100

    arguments.h: fix vector functionality
    unique_ptr can now be pushed, too

--- steps/native/arguments.h
+++ steps/native/arguments.h
@@ -17,12 +17,16 @@ namespace ara {
 		Arguments(const Arguments&) = delete;
 		Arguments(Arguments&& o) : MetaArguments(std::move(o)), owned_values(std::move(o.owned_values)) {}
 
+		void push_back(const llvm::Constant& c) { MetaArguments::push_back(c); }
+
 		void push_back(std::unique_ptr<const llvm::Constant> value) {
 			MetaArguments::push_back(std::ref(*value));
 			owned_values.push_back(std::move(value));
 		}
 
-		void emplace_bock(std::unique_ptr<const llvm::Constant> value) {
+		void emplace_back(const llvm::Constant& c) { MetaArguments::emplace_back(c); }
+
+		void emplace_back(std::unique_ptr<const llvm::Constant> value) {
 			MetaArguments::emplace_back(std::ref(*value));
 			owned_values.emplace_back(std::move(value));
 		}
