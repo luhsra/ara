46b760cc6be8642db6a1d538b7b514620b4c526d
Gerion Entrup <entrup@sra.uni-hannover.de>
2019-12-20 13:21:06 +0100

    steps/ValueAnalysis: store retrieved values in ABB

--- steps/native/value_analysis.cpp
+++ steps/native/value_analysis.cpp
@@ -4,6 +4,8 @@
 
 #include "value_analyzer.h"
 
+#include <boost/python.hpp>
+
 namespace ara::step {
 
 	template <typename Graph>
@@ -14,13 +16,13 @@ namespace ara::step {
 			    llvm::dyn_cast<llvm::CallBase>(&reinterpret_cast<llvm::BasicBlock*>(cfg.entry_bb[abb])->front());
 			if (called_func) {
 				Arguments args = va.get_values(*called_func);
+
+				// currently, the args object cannot be stored within the system graph, so check that it does not delete
+				// data when going out of scope
+				assert(!args.owns_objects());
+
+				cfg.arguments[abb] = boost::python::object(boost::python::handle<>(args.get_python_list()));
 				logger.debug() << "Retrieved " << args.size() << " arguments for call " << *called_func << std::endl;
-				for (const llvm::Constant& v : args) {
-					logger.debug() << "Retrieved value: " << v << std::endl;
-				}
-				if (args.size() >= 2) {
-					logger.debug() << "First argument is " << args[1] << std::endl;
-				}
 			} else {
 				logger.warn() << "Something went wrong." << std::endl;
 			}
