40e718f819691f8cfeca4990b31753ec8fd25b1b
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-01-21 22:00:17 +0100

    graph/argument: enable value retrieving with CallPath

--- graph/argument.py
+++ graph/argument.py
@@ -1,5 +1,13 @@
 import pyllco
 
+import graph_tool
+import graph_tool.search
+
+from collections import namedtuple
+
+# make this a data class once Ubuntu has a newer Python
+CallPath = namedtuple('CallPath', ['graph', 'node'])
+
 class Argument:
     """Combined type for a function argument.
 
@@ -15,13 +23,27 @@ class Argument:
         return (f"Argument({repr(self.consts[tuple()])}, ambiguous={ambi}, "
                 f"{repr(self.attributes)})")
 
+    def _get_call_path(self, call_path):
+        g = graph_tool.GraphView(call_path.graph, reversed=True)
+        path = []
+        for e in graph_tool.search.dfs_iterator(g, call_path.node):
+            path.append(g.vp.cfglink[e.source()])
+        return tuple(path)
+
     def add_variant(self, call_path, constant):
         self.consts[call_path] = constant
 
-    def is_function(self):
-        return isinstance(self.constant, pyllco.Function)
+    def get(self, call_path=None, raw=False):
+        if call_path is not None:
+            cp = self._get_call_path(call_path)
+            if cp in self.consts:
+                constant = self.consts[cp]
+            else:
+                constant = self.consts[tuple()]
 
-    def get(self, call_path=None):
-        if self.is_function():
-            return self.constant.get_name()
-        return self.constant.get(attrs=self.attributes)
+        if raw:
+            return constant
+
+        if isinstance(constant, pyllco.Function):
+            return constant.get_name()
+        return constant.get(attrs=self.attributes)
