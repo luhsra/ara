5ba03031f441a07886863be79970a727f8f4c9a5
Bjoern Fiedler <fiedler@sra.uni-hannover.de>
2020-02-14 17:42:24 +0100

    tools/compare_nm: colorize changes of size and segment

--- tools/compare_nm.py
+++ tools/compare_nm.py
@@ -5,55 +5,70 @@ import logging
 from pprint import pprint
 from collections import defaultdict
 import re
+import os
 
+RED = ("\033[1;31m", "\033[1;0m")
+BLUE = ("\033[1;34m", "\033[1;0m")
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('NM_TOOL', help='nm binary')
     parser.add_argument('NAME ELF', help='tuples to analyze, '
                         'e.g. special: special_version.elf',
                         nargs='+')
+    parser.add_argument('-C', '--force-color', action='store_true',
+                        help='force colorized changes')
     args = parser.parse_args()
+
+    if not (args.force_color or os.isatty(1)):
+        global RED, BLUE
+        RED = ("","")
+        BLUE = RED
 
     inp = getattr(args, 'NAME ELF', [])
     if len(inp) % 2:
         parser.print_help()
     pairs = [(inp[i], inp[i+1]) for i in range(0,len(inp), 2)]
 
-    data = {}
-    keys = {}
+    data = defaultdict(lambda:defaultdict(lambda:defaultdict(lambda:' ')))
+    keys = defaultdict(lambda:set())
     for pair in pairs:
         name, filename = pair
-        data[name] = parse_elf(args.NM_TOOL, filename)
-        keys[name] = set(data[name].keys())
+        parse_elf(args.NM_TOOL, name, filename, data, keys)
 
 
+    all_keys = keys[name].union(*keys.values())
     common_keys = keys[name].intersection(*keys.values())
     unique_keys = keys[name].union(*keys.values()) - common_keys
 
-    lens = {name: len(name) for name in data.keys()}
+    lens = {name: len(name) for name in keys.keys()}
 
-    fields = [f'{n:^{lens[n]+2}}' for n in data.keys()]
+    fields = [f'{n:^{lens[n]+2}}' for n in keys.keys()]
     fields += ['name']
     print(" | ".join(fields))
 
 
     for keyset in [unique_keys, common_keys]:
         for key in sorted(keyset):
-            sizes = [data[n][key]['size'] for n in data.keys()]
-            if len(set(sizes)) == 1:
+            d = data[key]
+            sizes = [d[k]['size'] for k in keys.keys()]
+            segments = set([i['segment'] for i in d.values()])
+            if len(set(sizes)) == 1 and len(segments) == 1:
                 continue
-            fields = [f"{data[n][key]['size']:{lens[n]}} {data[n][key]['segment']}"
-                      for n in data.keys()]
+            segments -= set(" ")
+            size_color = ("","") if len(set(sizes)-set(" ")) == 1 else BLUE
+            seg_color = ("","") if len(segments) == 1 else RED
+            fields = [f"{size_color[0]}{d[n]['size']:{lens[n]}}{size_color[1]} "
+                      f"{seg_color[0]}{d[n]['segment']}{seg_color[1]}"
+                      for n in keys.keys()]
             fields += [f"{key}"]
             print(" | ".join(fields))
         print('\n')
 
 
-def parse_elf(nm, elf):
+def parse_elf(nm, elf_name, elf, data, keys):
     result = subprocess.run([nm, '-SP', elf],
                             check=True,
                             stdout=subprocess.PIPE)
-    ret = defaultdict(lambda: defaultdict(lambda:' '))
     for line in result.stdout.decode().strip().split('\n'):
         match = re.match("(?P<name>\S+) (?P<seg>\S) (?P<addr>\S+) (?P<size>\S*)",
                          line)
@@ -63,13 +78,13 @@ def parse_elf(nm, elf):
                 continue
             return
         name = match.group('name')
-        ret[name] = {
+        data[name][elf_name] = {
             'segment': match.group('seg'),
             'size': int(match.group('size') or '0', 16),
             'addr': match.group('addr'),
             'name': name,
-            }
-    return ret
+        }
+        keys[elf_name].add(name)
 
 
 if __name__ == '__main__':
