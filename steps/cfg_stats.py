09eb79b59dfa08390d004f55b90c57fec2903ac8
Manuel Breiden <manuel.breiden@gmx.de>
2020-02-18 16:40:28 +0100

    steps: adds counting of abbs in cfg_stats.py

--- steps/cfg_stats.py
+++ steps/cfg_stats.py
@@ -3,6 +3,7 @@ import graph
 
 from native_step import Step
 from .option import Option, Integer
+from graph import ABBType
 
 import graph_tool
 
@@ -16,7 +17,13 @@ class CFGStats(Step):
     def run(self, g: graph.Graph):
         self._log.info("Executing CFGStats step.")
 
-        # TODO Count Atomic Basic Blocks
+        num_abbs = 0;
+
+        for v in g.cfg.vertices() :
+            if g.cfg.vp.type[v] in [ABBType.computation, ABBType.call, ABBType.syscall] :
+                num_abbs += 1
+
+        self._log.info("Number of ABBs: " + str(num_abbs))
 
         num_cycles = 0;
         
