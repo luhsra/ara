17e998fc335c08c1f91c8e347c4a4b05f055d276
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-02-21 17:47:21 +0100

    SSE: refactor InstanceGraph in two classes
    A new analysis for flow insensitive interaction should come, that uses a
    lot of code InstanceGraph also uses. This commit is the initial
    refactoring to handle both analyses without code duplication.

--- steps/sse.py
+++ steps/sse.py
@@ -70,7 +70,7 @@ class Flavor:
         return new_states
 
 
-class InstanceGraph(Flavor):
+class FlowInsensitiveAnalysis(Flavor):
     def __init__(self, step, g, entry_func, side_data):
         super().__init__(step, g, entry_func)
         self.call_map = self._create_call_map(entry_func)
@@ -134,20 +134,85 @@ class InstanceGraph(Flavor):
             call_map[cg.vp.cfglink[v]] = v
         return call_map
 
-    @functools.lru_cache(maxsize=32)
-    def _create_dom_tree(self, func):
-        """Create a dominator tree of the local control flow for func."""
-        abb = self._g.cfg.get_entry_abb(func)
-        comp = label_out_component(self._lcfg, self._lcfg.vertex(abb))
-        func_cfg = graph.CFGView(self._lcfg,
-                                 vfilt=comp)
-        dom_tree = dominator_tree(func_cfg, func_cfg.vertex(abb))
+    def _init_execution(self, state):
+        pass
 
-        return dom_tree
+    def _init_fake_state(self, state, abb):
+        pass
+
+    def _evaluate_fake_state(self, state, abb):
+        pass
+
+    def _handle_call(self, old_state, new_state, abb):
+        pass
 
     def _get_categories(self):
-        return SyscallCategory.CREATE
+        return SyscallCategory.ALL
 
+    def execute(self, state):
+        new_states = []
+        self._init_execution(state)
+        for abb in state.next_abbs:
+            # don't handle already visted vertices
+            if self.visited[state.call][abb]:
+                continue
+            self.visited[state.call][abb] = True
+
+            # syscall handling
+            if self._icfg.vp.type[abb] == graph.ABBType.syscall:
+                fake_state = state.copy()
+                self._init_fake_state(fake_state, abb)
+                assert self._g.os is not None
+                print(getattr(self._g.os, "xQueueGenericCreate"))
+                new_state = self._g.os.interpret(self._g.cfg, abb, fake_state,
+                                                 categories=self._get_categories())
+                self._evaluate_fake_state(new_state, abb)
+                new_states.append(new_state)
+
+            # call handling
+            elif self._icfg.vp.type[abb] == graph.ABBType.call:
+                for n in self._icfg.vertex(abb).out_neighbors():
+                    new_state = state.copy()
+                    new_state.next_abbs = [n]
+                    new_state.call = self.call_map[abb]
+                    self._handle_call(state, new_state, abb)
+                    new_states.append(new_state)
+
+            # exit handling
+            elif (self._icfg.vp.is_exit[abb] and
+                  self._icfg.vertex(abb).out_degree() > 0):
+                new_state = state.copy()
+                call = new_state.callgraph.vp.cfglink[new_state.call]
+                neighbors = self._lcfg.vertex(call).out_neighbors()
+                new_state.next_abbs = [next(neighbors)]
+                new_state.call = next(state.call.in_neighbors())
+                new_states.append(new_state)
+
+            # computation block handling
+            else:
+                for n in self._icfg.vertex(abb).out_neighbors():
+                    new_state = state.copy()
+                    new_state.next_abbs = [n]
+                    new_states.append(new_state)
+
+        return new_states
+
+    def schedule(self, state):
+        # we do simply not care
+        return
+
+    def finish(self, sstg):
+        if self._step.dump.get():
+            uuid = self._step._step_manager.get_execution_id()
+            dot_file = f'Instances.{uuid}.{self._entry_func}.dot'
+            dot_file = self._step.dump_prefix.get() + dot_file
+            self._step._step_manager.chain_step({"name": "Printer",
+                                                 "dot": dot_file,
+                                                 "graph_name": 'Instances',
+                                                 "subgraph": 'instances'})
+
+
+class InstanceGraph(FlowInsensitiveAnalysis):
     def _dominates(self, abb_x, abb_y):
         """Does abb_x dominate abb_y?"""
         func = self._g.cfg.get_function(abb_x)
@@ -162,6 +227,17 @@ class InstanceGraph(Flavor):
         return False
 
     @functools.lru_cache(maxsize=32)
+    def _create_dom_tree(self, func):
+        """Create a dominator tree of the local control flow for func."""
+        abb = self._g.cfg.get_entry_abb(func)
+        comp = label_out_component(self._lcfg, self._lcfg.vertex(abb))
+        func_cfg = graph.CFGView(self._lcfg,
+                                 vfilt=comp)
+        dom_tree = dominator_tree(func_cfg, func_cfg.vertex(abb))
+
+        return dom_tree
+
+    @functools.lru_cache(maxsize=32)
     def _find_exit_abbs(self, func):
         return [x for x in func.out_neighbors()
                 if self._g.cfg.vp.is_exit[x] or self._g.cfg.vp.is_loop_head[x]]
@@ -171,6 +247,10 @@ class InstanceGraph(Flavor):
         func = self._g.cfg.get_function(abb)
         return not all([self._dominates(abb, x)
                         for x in self._find_exit_abbs(func)])
+
+    def _init_fake_state(self, state, abb):
+        state.branch = (self.func_branch[state.call] or
+                        self._is_in_condition_or_loop(abb))
 
     def _extract_entry_points(self):
         for v in self._g.instances.vertices():
@@ -197,70 +277,35 @@ class InstanceGraph(Flavor):
                         self.side_data.add(func_name)
                 self.new_entry_points.add(os_obj)
 
-    def execute(self, state):
-        new_states = []
+    def _evaluate_fake_state(self, new_state, abb):
+        self._g.instances = new_state.instances
+        self._extract_entry_points()
+
+    def _init_execution(self, state):
         if self._g.instances is not None:
             state.instances = self._g.instances
-        for abb in state.next_abbs:
-            if self.visited[state.call][abb]:
-                continue
-            self.visited[state.call][abb] = True
 
-            if self._icfg.vp.type[abb] == graph.ABBType.syscall:
-                fake_state = state.copy()
-                fake_state.branch = (self.func_branch[state.call] or
-                                     self._is_in_condition_or_loop(abb))
-                assert self._g.os is not None
-                new_state = self._g.os.interpret(self._g.cfg, abb, fake_state,
-                                                 categories=self._get_categories())
-                self._g.instances = new_state.instances
-                self._extract_entry_points()
-                new_states.append(new_state)
+    def _handle_call(self, old_state, new_state, abb):
+        new_state.branch = (self.func_branch[old_state.call] or
+                            self._is_in_condition_or_loop(abb))
+        self.func_branch[new_state.call] = new_state.branch
 
-            elif self._icfg.vp.type[abb] == graph.ABBType.call:
-                for n in self._icfg.vertex(abb).out_neighbors():
-                    new_state = state.copy()
-                    new_state.next_abbs = [n]
-                    new_state.branch = (self.func_branch[state.call] or
-                                        self._is_in_condition_or_loop(abb))
-                    new_state.call = self.call_map[abb]
-                    self.func_branch[new_state.call] = new_state.branch
-
-                    new_states.append(new_state)
-
-            elif (self._icfg.vp.is_exit[abb] and
-                  self._icfg.vertex(abb).out_degree() > 0):
-                new_state = state.copy()
-                call = new_state.callgraph.vp.cfglink[new_state.call]
-                neighbors = self._lcfg.vertex(call).out_neighbors()
-                new_state.next_abbs = [next(neighbors)]
-                new_state.call = next(state.call.in_neighbors())
-                new_states.append(new_state)
-
-            else:
-                for n in self._icfg.vertex(abb).out_neighbors():
-                    new_state = state.copy()
-                    new_state.next_abbs = [n]
-
-                    new_states.append(new_state)
-
-        return new_states
-
-    def schedule(self, state):
-        # we do simply not care
-        return
+    def _get_categories(self):
+        return SyscallCategory.CREATE
 
     def finish(self, sstg):
+        super().finish(sstg)
         self._log.debug(f"_create_dom_tree {self._create_dom_tree.cache_info()}")
         self._log.debug(f"_find_exit_abbs  {self._find_exit_abbs.cache_info()}")
-        if self._step.dump.get():
-            uuid = self._step._step_manager.get_execution_id()
-            dot_file = f'Instances.{uuid}.{self._entry_func}.dot'
-            dot_file = self._step.dump_prefix.get() + dot_file
-            self._step._step_manager.chain_step({"name": "Printer",
-                                                 "dot": dot_file,
-                                                 "graph_name": 'Instances',
-                                                 "subgraph": 'instances'})
+
+
+class InteractionAnalysis(FlowInsensitiveAnalysis):
+    def _evaluate_fake_state(self, new_state, abb):
+        self._g.instances = new_state.instances
+
+    def _init_execution(self, state):
+        if self._g.instances is not None:
+            state.instances = self._g.instances
 
 
 # TODO make this a dataclass, when ready
