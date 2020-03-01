a2619a34b6ffb54d0a0eba2bf03e11208965a604
Manuel Breiden <manuel.breiden@gmx.de>
2020-03-02 00:29:14 +0100

    steps/cfg_stats.py: exclude edges between ABBs and their function from the calculation

--- steps/cfg_stats.py
+++ steps/cfg_stats.py
@@ -4,6 +4,7 @@ import graph
 from native_step import Step
 from .option import Option, Integer
 from graph import ABBType
+from graph import CFType
 
 import graph_tool
 
@@ -18,7 +19,7 @@ class CFGStats(Step):
         self._log.info("Executing CFGStats step.")
         cfg = g.cfg
 
-        # Calculate number of ABBs
+        # Count number of ABBs
         num_abbs = 0;
 
         for v in cfg.vertices() :
@@ -27,7 +28,7 @@ class CFGStats(Step):
 
         self._log.info("Number of ABBs: " + str(num_abbs))
 
-        # Calculate number of Cycles
+        # Count number of Cycles
         num_cycles = 0;
         
         for c in graph_tool.topology.all_circuits(cfg,True) :
@@ -35,12 +36,22 @@ class CFGStats(Step):
 
         self._log.info("Number of Cycles: " + str(num_cycles))
 
-        # Calculate cyclomatic complexity
+
+        # Count number of control flow sensitive edges
+        num_edges = 0;
+
+        for e in cfg.edges() :
+            if cfg.ep.type[e] in [CFType.lcf, CFType.gcf, CFType.icf] :
+                num_edges += 1
+
+        # Count number of connected components
         num_components = 0
         property_map, hist = graph_tool.topology.label_components(cfg, None, cfg.is_directed(), False)
         for comp in hist :
             num_components += 1
-        v = cfg.num_edges() - cfg.num_vertices() + 2 * num_components
+
+        # Calculate cyclomatic complexity
+        v = num_edges - num_abbs + 2 * num_components
 
         self._log.info("Cyclomatic complexity: " + str(v))
 
