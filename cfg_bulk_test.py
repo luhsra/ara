4ad4236f536059bfe6e6e39bb258f0b4b58b2b13
Manuel Breiden <manuel.breiden@gmx.de>
2020-03-06 00:04:53 +0100

    test: delete local cfg statistics tests since the icfg includes the values

--- cfg_bulk_test.py
+++ cfg_bulk_test.py
@@ -19,24 +19,15 @@ def replace_pass_list(pass_list):
         cfg_json['steps'][0]['pass_list'] = pass_list
     with open('../test/cfg_stats.json', 'w') as f:
         json.dump(cfg_json, f, indent=4)
-    with open('../test/icfg_stats.json', 'r') as f:
-        icfg_json = json.load(f)
-        icfg_json['steps'][0]['pass_list'] = pass_list
-    with open('../test/icfg_stats.json', 'w') as f:
-        json.dump(icfg_json, f, indent=4)
 
-with open('gps_stats.txt', 'w') as gps_stats, open('gps_ipstats.txt', 'w') as gps_ipstats, open('smart_stats.txt', 'w') as smart_stats, open('smart_ipstats.txt', 'w') as smart_ipstats:
+with open('gps_stats.txt', 'w') as gps_stats, open('smart_stats.txt', 'w') as smart_stats:
         for pass_list in pass_lists :
             replace_pass_list(pass_list)
             gps_test = subprocess.Popen(['meson', 'test', 'gps_stats', '-v'], stdout=subprocess.PIPE)
             gps_stats.write(pass_list + " " + gps_test.communicate()[0].decode('utf-8'))
-            gps_iptest = subprocess.Popen(['meson', 'test', 'gps_ipstats', '-v'], stdout=subprocess.PIPE)
-            gps_ipstats.write(pass_list + " " + gps_iptest.communicate()[0].decode('utf-8'))
             smart_test = subprocess.Popen(['meson', 'test', 'smart_stats', '-v'], stdout=subprocess.PIPE)
             smart_stats.write(pass_list + " " + smart_test.communicate()[0].decode('utf-8'))
-            smart_iptest = subprocess.Popen(['meson', 'test', 'smart_ipstats', '-v'], stdout=subprocess.PIPE)
-            smart_ipstats.write(pass_list + " " + smart_iptest.communicate()[0].decode('utf-8'))
 
 # clean stat files using sed
-subprocess.Popen(['sed', '-i', '/module.*/{N;s/\\n/ /;};s/\(module(.*)\).*/\\1/g;/module.*/{N;s/\\n/ /;};/Ok/d;/Fail/d;/Timeout/d;/1\\/1/d;/Unexpected/d;/Skipped/d;/^$/d', 'gps_stats.txt', 'gps_ipstats.txt', 'smart_stats.txt', 'smart_ipstats.txt'])
+subprocess.Popen(['sed', '-i', '/module.*/{N;s/\\n/ /;};s/\(module(.*)\).*/\\1/g;/module.*/{N;s/\\n/ /;};/Ok/d;/Fail/d;/Timeout/d;/1\\/1/d;/Unexpected/d;/Skipped/d;/^$/d', 'gps_stats.txt', 'smart_stats.txt'])
 
