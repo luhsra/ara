a8932255236e832b00a1b6e34df95ba63ffb77a8
Manuel Breiden <manuel.breiden@gmx.de>
2020-03-06 17:15:21 +0100

    steps/cfg_stats: fix component and abb counting

--- steps/cfg_stats.py
+++ steps/cfg_stats.py
@@ -19,40 +19,51 @@ class CFGStats(Step):
         self._log.info("Executing CFGStats step.")
         cfg = g.cfg
 
-        # Count number of ABBs
+        # Count number of Nodes (ABBs + non implemented functions)
         num_abbs = 0;
 
         for v in cfg.vertices() :
-            if cfg.vp.type[v] in [ABBType.computation, ABBType.call, ABBType.syscall] :
-                num_abbs += 1
+            if g.cfg.vp.is_function[v]:
+                continue
+            num_abbs += 1
 
         self._log.info("Number of ABBs: " + str(num_abbs))
 
-        # Count number of Cycles
-        #num_cycles = 0;
-        
-        #for c in graph_tool.topology.all_circuits(cfg,True) :
-        #    num_cycles += 1
-
-        #self._log.info("Number of Cycles: " + str(num_cycles))
-
-
         # Count number of control flow sensitive edges
-        num_edges = 0;
+
+        num_ledges = 0;
+        num_iedges = 0;
 
         for e in cfg.edges() :
-            if cfg.ep.type[e] in [CFType.lcf, CFType.gcf, CFType.icf] :
-                num_edges += 1
+            if cfg.ep.type[e] == CFType.lcf:
+                num_ledges += 1
+            if cfg.ep.type[e] == CFType.icf:
+                num_iedges += 1
+
+        self._log.info("Number of local edges: " + str(num_ledges))
+        self._log.info("Number of interp. edges: " + str(num_iedges))
 
         # Count number of connected components
-        num_components = 0
-        property_map, hist = graph_tool.topology.label_components(cfg, None, cfg.is_directed(), False)
-        for comp in hist :
-            num_components += 1
+        lcfg = graph.CFGView(cfg, efilt=lambda e: cfg.ep.type[e] != graph.CFType.icf)
+        icfg = graph.CFGView(cfg, efilt=lambda e: cfg.ep.type[e] != graph.CFType.lcf)
 
-        # Calculate cyclomatic complexity
-        v = num_edges - num_abbs + 2 * num_components
+        num_lcomp = 0
+        lproperty_map, lhist = graph_tool.topology.label_components(lcfg, None, None, False)
+        for comp in lhist :
+            num_lcomp += 1
+        self._log.info("Number of local components: " + str(num_lcomp))
 
-        self._log.info("Cyclomatic complexity: " + str(v))
-        sys.stdout.write(str(num_abbs) + " " + str(v) + "\n")
+        num_icomp = 0
+        iproperty_map, ihist = graph_tool.topology.label_components(icfg, None, None, False)
+        for comp in ihist :
+            num_icomp += 1
+        self._log.info("Number of interprocedural components: " + str(num_icomp))
 
+        # Calculate cyclomatic complexities
+        lv = num_ledges - num_abbs + 2 * num_lcomp
+        iv = num_iedges - num_abbs + 2 * num_icomp
+
+        self._log.info("Local cyclomatic complexity: " + str(lv))
+        self._log.info("Interprocedural cyclomatic complexity: " + str(iv))
+        # Only used for bulk testing
+        sys.stdout.write(str(num_abbs) + " " + str(lv) + " " + str(iv) + "\n")
