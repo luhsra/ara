2453c54c2824b4090f3bf131acc724356ddf81b7
Bjoern Fiedler <fiedler@sra.uni-hannover.de>
2020-03-17 17:24:14 +0100

    gen: create tasks only for task objects

--- generator/coder/syscall_full_initialized.py
+++ generator/coder/syscall_full_initialized.py
@@ -18,21 +18,22 @@ class InitializedFullSystemCalls(GenericSystemCalls):
         #TODO: Queue Verwaltungsstruktur
         self.generator.source_file.includes.add(
             Include('InitializedFreeRTOSObjects.h'))
-        self.generate_dataobjects_task_stacks()
-        self.generate_data_objects_tcb_mem()
-        self.generate_data_objects_ready_list()
+        task_list = [self.ara_graph.instances.vp.obj[v]
+                     for v in self.ara_graph.instances.vertices()
+                     if isinstance(self.ara_graph.instances.vp.obj[v], Task)]
+        self.generate_dataobjects_task_stacks(task_list)
+        self.generate_data_objects_tcb_mem(task_list)
+        self.generate_data_objects_ready_list(task_list)
         self.generate_limitation_warnings()
 
-    def generate_dataobjects_task_stacks(self):
+    def generate_dataobjects_task_stacks(self, task_list):
         '''generate the stack space for the tasks'''
-        for v in self.ara_graph.instances.vertices():
-            task = self.ara_graph.instances.vp.obj[v]
+        for task in task_list:
             self.arch_rules.initialized_stack(task)
 
-    def generate_data_objects_tcb_mem(self):
+    def generate_data_objects_tcb_mem(self, task_list):
         '''generate the memory for the tcbs'''
-        for v in self.ara_graph.instances.vertices():
-            task = self.ara_graph.instances.vp.obj[v]
+        for task in task_list:
             self.arch_rules.static_unchanged_tcb(task, initialized=True)
             if not task.is_regular and task.name == 'idle_task':
                 self.generator.source_file.data_manager.add(
@@ -43,14 +44,16 @@ class InitializedFullSystemCalls(GenericSystemCalls):
 
     def generate_system_code(self):
         super().generate_system_code()
-        self.generate_system_code_init_tasks()
+        task_list = [self.ara_graph.instances.vp.obj[v]
+                     for v in self.ara_graph.instances.vertices()
+                     if isinstance(self.ara_graph.instances.vp.obj[v], Task)]
+        self.generate_system_code_init_tasks(task_list)
 
 
-    def generate_system_code_init_tasks(self):
+    def generate_system_code_init_tasks(self, task_list):
         self.generator.source_file.includes.add(Include('task.h'))
         idle_task = None
-        for v in self.ara_graph.instances.vertices():
-            task = self.ara_graph.instances.vp.obj[v]
+        for task in task_list:
             if not task.is_regular:
                 if task.name == 'idle_task':
                     idle_task = task
@@ -75,12 +78,10 @@ class InitializedFullSystemCalls(GenericSystemCalls):
         dummy_xTaskCreate.add(Statement("return pdPASS"))
         self.generator.source_file.function_manager.add(dummy_xTaskCreate)
 
-    def generate_data_objects_ready_list(self):
+    def generate_data_objects_ready_list(self, tasks):
         #TODO: max prio from analyzer
         #max_prio = self.ara_graph.get_config('configMAX_PRIORITIES')
         max_prio = 5
-        tasks = [self.ara_graph.instances.vp.obj[t]
-                 for t in self.ara_graph.instances.vertices()]
 
         # ready_lists = DataObjectArray("PRIVILEGED_DATA List_t", "pxReadyTasksLists", 'configMAX_PRIORITIES')
         ready_lists = self.arch_rules.TasksLists("pxReadyTasksLists",
