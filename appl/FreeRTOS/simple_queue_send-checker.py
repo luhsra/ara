f65e55149baca71e224e25b385813924eed912f6
Bjoern Fiedler <fiedler@sra.uni-hannover.de>
2020-03-17 17:48:00 +0100

    appl: FreeRTOS: queue sending to higher and lower prio
    expected behaviour:
    sender sends ttttuuuuvvvvwwww
    it is expected to be interrupted by higher prio receiver printing each
    character in uppercase.
    Thus, we expect the following output:
    TtTtTtUuUuUuVvVvVvWwWwWw
    
    Nect, we send the same sequence ttttuuuuvvvvwwww to a lower prio
    receiver task.
    here, we expect the sender to continue after sending. The queue has a
    capacity of three elements, resulting in blocking the sender after
    three t's, then alternating with the receiver and a fan out of the
    receiver.
    The second receiver prints in ROT13 to be distinguishable from the
    first one.
    tttugugugvhvhvhwiwiwijjj

---
+++ appl/FreeRTOS/simple_queue_send-checker.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import re
+def check(emulator, elf, result):
+    if result.returncode:
+        exit(result.returncode)
+    if not b't' in result.stdout:
+        raise RuntimeError("sender not working")
+    if not b'TtTtTtUuUuUuVvVvVvWwWwWw' in result.stdout:
+        raise RuntimeError("Higher prio receiver is not working")
+    if not b'tttugugugvhvhvhwiwiwijjj' in result.stdout:
+        raise RuntimeError('lower prio receiver is missing')
---
+++ appl/FreeRTOS/simple_queue_send.cc
@@ -0,0 +1,96 @@
+#include <FreeRTOS.h>
+#include <FreeRTOSConfig.h>
+#include <task.h>
+#include <queue.h>
+#include <output.h>
+#include "time_markers.h"
+#include <platform.h>
+
+
+TIME_MARKER(task2_go);
+TIME_MARKER(task1_go);
+TIME_MARKER(main_start);
+TIME_MARKER(done_InitBoard);
+TIME_MARKER(done_hello_print);
+TIME_MARKER(done_tastCreate);
+TIME_MARKER(done_queueCreate);
+
+QueueHandle_t queue_handle_1;
+QueueHandle_t queue_handle_3;
+
+void vTask2(void * param) {
+  STORE_TIME_MARKER(task2_go);
+  for (;;) {
+    char c;
+    BaseType_t success = xQueueReceive(queue_handle_1, &c, 500);
+    if (! success) {
+      kout << "ERROR: queueReceive failed" << endl;
+    }
+    kout << (char) (c ^ 0x20); // Print with inverted cASE
+  }
+  for (;;) vTaskDelay(1000);
+}
+
+void vTask3(void * param) {
+  STORE_TIME_MARKER(task2_go);
+  for (;;) {
+    char c;
+    BaseType_t success = xQueueReceive(queue_handle_3, &c, 500);
+    if (! success) {
+      kout << "ERROR: queueReceive failed" << endl;
+    }
+    kout << (char) (c - 13); // print rot13
+  }
+  for (;;) vTaskDelay(1000);
+}
+
+void vTask1(void * param) {
+  STORE_TIME_MARKER(task1_go);
+  char small_t = 't';
+  for (int n=0; n < 4; ++n) {
+    for (int i = 0; i < 3; ++i) {
+      xQueueSend(queue_handle_1, &small_t, 500);
+      kout << small_t;
+    }
+    small_t++;
+  }
+  vTaskDelay(20);
+  small_t = 't';
+  for (int n=0; n < 4; ++n) {
+    for (int i = 0; i < 3; ++i) {
+      xQueueSend(queue_handle_3, &small_t, 500);
+      kout << small_t;
+    }
+    small_t++;
+  }
+  vTaskDelay(20);
+  StopBoard();
+}
+
+int main() {
+  STORE_TIME_MARKER(main_start);
+  InitBoard();
+  STORE_TIME_MARKER(done_InitBoard);
+  kout.init();
+
+  kout << 'z' << endl;
+  kout << "hello from main" << endl;
+  STORE_TIME_MARKER(done_hello_print);
+
+  xTaskCreate(vTask1, "zzz", 100, NULL, 2, NULL);
+  xTaskCreate(vTask2, "xxx", 100, NULL, 3, NULL);
+  xTaskCreate(vTask3, "xxx", 100, NULL, 1, NULL);
+  STORE_TIME_MARKER(done_tastCreate);
+
+  queue_handle_1 = xQueueCreate(3, sizeof(char));
+  if (queue_handle_1 == NULL) {
+    kout << "ERROR: Queue1 creation failed" << endl;
+  }
+  queue_handle_3 = xQueueCreate(3, sizeof(char));
+  if (queue_handle_3 == NULL) {
+    kout << "ERROR: Queue3 creation failed" << endl;
+  }
+  STORE_TIME_MARKER(done_queueCreate);
+  vTaskStartScheduler();
+
+}
