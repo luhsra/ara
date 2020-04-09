4866f08077a60e06e05704fab7780c67e1a0c675
Björn Fiedler <fiedler@sra.uni-hannover.de>
2020-04-09 21:10:22 +0200

    appl/FreeRTOS: make more test apps compilable
    stack sizes were to big to fit in .bss

--- appl/FreeRTOS/icfg.cc
+++ appl/FreeRTOS/icfg.cc
@@ -48,8 +48,8 @@ int other_function(int a) {
 }
 
 int main(void) {
-	xTaskCreate(vTask1, "Task 1", 51000, NULL, 1, NULL);
-	xTaskCreate(vTask2, "Task 2", 1000, NULL, 1, NULL);
+	xTaskCreate(vTask1, "Task 1", 510, NULL, 1, NULL);
+	xTaskCreate(vTask2, "Task 2", 100, NULL, 1, NULL);
 
 	int e = do_stuff(23, 90);
 
--- appl/FreeRTOS/sse.cc
+++ appl/FreeRTOS/sse.cc
@@ -59,12 +59,12 @@ void vTask2(void* pvParameters);
 void createTasks(const char* name1, const char* name2) {
 	xTaskCreate(vTask1,   /* Pointer to the function that implements the task. */
 	            name1,    /* Text name for the task.  This is to facilitate debugging only. */
-	            1000,     /* Stack depth - most small microcontrollers will use much less stack than this. */
+	            100,     /* Stack depth - most small microcontrollers will use much less stack than this. */
 	            NULL,     /* We are not using the task parameter. */
 	            1,        /* This task will run at priority 1. */
 	            NULL);    /* We are not using the task handle. */
 
-	xTaskCreate(vTask2, name2, 1000, NULL, 1, NULL);
+	xTaskCreate(vTask2, name2, 100, NULL, 1, NULL);
 	return;
 }
 
@@ -76,7 +76,7 @@ int main(void) {
 	createTasks("Task 3", "Task 4");
 
 	for (int i = 0; i < 5; ++i) {
-		xTaskCreate(vTask2, "Task5", 1000, NULL, 1, NULL);
+		xTaskCreate(vTask2, "Task5", 100, NULL, 1, NULL);
 	}
 
 	vTaskStartScheduler();
@@ -88,7 +88,7 @@ int main(void) {
 
 void vTask1(void* pvParameters) {
 	volatile unsigned long ul;
-	xTaskCreate(vTask2, "Task6", 1000, NULL, 1, NULL);
+	xTaskCreate(vTask2, "Task6", 100, NULL, 1, NULL);
 	for (;;) {
 
 		/* Delay for a period. */
