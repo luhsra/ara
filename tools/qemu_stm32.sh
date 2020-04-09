a3d56912f5b2f96f25393c9adc1b61a479d00fd4
Björn Fiedler <fiedler@sra.uni-hannover.de>
2020-04-09 21:20:08 +0200

    qemu: don't mess up the terminal!

--- tools/qemu_stm32.sh
+++ tools/qemu_stm32.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-STTY_SETTINGS="$( stty -g )"
-
-function finish {
-	stty "$STTY_SETTINGS"
-}
-trap finish EXIT QUIT HUP INT ABRT TERM
 
 echo $@
 # qemu-system-stm32 -machine stm32-p103 -nographic -serial mon:stdio -kernel ${1}
-qemu-system-stm32 -machine stm32-p103 -nographic -serial stdio -monitor none -kernel $@
+qemu-system-stm32 -machine stm32-p103 -nographic -serial file:/dev/stdout -monitor none -kernel $@
