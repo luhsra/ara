b85d964f8d5503f1cf20e1b076b9202d363a35f6
Björn Fiedler <fiedler@sra.uni-hannover.de>
2020-04-30 16:04:59 +0200

    libs/freertos: move queue decls into header for static initialization

--- libs/freertos/full_static/queue.c
+++ libs/freertos/full_static/queue.c
@@ -65,18 +65,7 @@ being used for. */
 #define uxQueueType pcHead
 #define queueQUEUE_IS_MUTEX NULL
 
-typedef struct QueuePointers {
-	int8_t* pcTail;     /*< Points to the byte at the end of the queue storage area.  Once more byte is allocated than
-	                       necessary to store the queue items, this is used as a marker. */
-	int8_t* pcReadFrom; /*< Points to the last place that a queued item was read from when the structure is used as a
-	                       queue. */
-} QueuePointers_t;
-
-typedef struct SemaphoreData {
-	TaskHandle_t xMutexHolder;        /*< The handle of the task that holds the mutex. */
-	UBaseType_t uxRecursiveCallCount; /*< Maintains a count of the number of times a recursive mutex has been
-	                                     recursively 'taken' when the structure is used as a mutex. */
-} SemaphoreData_t;
+// moved to header
 
 /* Semaphores do not actually store or copy data, so have an item size of
 zero. */
@@ -91,55 +80,7 @@ performed just because a higher priority task has been woken. */
 #define queueYIELD_IF_USING_PREEMPTION() portYIELD_WITHIN_API()
 #endif
 
-/*
- * Definition of the queue used by the scheduler.
- * Items are queued by copy, not reference.  See the following link for the
- * rationale: https://www.freertos.org/Embedded-RTOS-Queues.html
- */
-typedef struct QueueDefinition /* The old naming convention is used to prevent breaking kernel aware debuggers. */
-{
-	int8_t* pcHead;    /*< Points to the beginning of the queue storage area. */
-	int8_t* pcWriteTo; /*< Points to the free next place in the storage area. */
-
-	union {
-		QueuePointers_t xQueue;     /*< Data required exclusively when this structure is used as a queue. */
-		SemaphoreData_t xSemaphore; /*< Data required exclusively when this structure is used as a semaphore. */
-	} u;
-
-	List_t xTasksWaitingToSend; /*< List of tasks that are blocked waiting to post onto this queue.  Stored in priority
-	                               order. */
-	List_t xTasksWaitingToReceive; /*< List of tasks that are blocked waiting to read from this queue.  Stored in
-	                                  priority order. */
-
-	volatile UBaseType_t uxMessagesWaiting; /*< The number of items currently in the queue. */
-	UBaseType_t
-	    uxLength; /*< The length of the queue defined as the number of items it will hold, not the number of bytes. */
-	UBaseType_t uxItemSize; /*< The size of each items that the queue will hold. */
-
-	volatile int8_t cRxLock; /*< Stores the number of items received from the queue (removed from the queue) while the
-	                            queue was locked.  Set to queueUNLOCKED when the queue is not locked. */
-	volatile int8_t cTxLock; /*< Stores the number of items transmitted to the queue (added to the queue) while the
-	                            queue was locked.  Set to queueUNLOCKED when the queue is not locked. */
-
-#if ((configSUPPORT_STATIC_ALLOCATION == 1) && (configSUPPORT_DYNAMIC_ALLOCATION == 1))
-	uint8_t ucStaticallyAllocated; /*< Set to pdTRUE if the memory used by the queue was statically allocated to ensure
-	                                  no attempt is made to free the memory. */
-#endif
-
-#if (configUSE_QUEUE_SETS == 1)
-	struct QueueDefinition* pxQueueSetContainer;
-#endif
-
-#if (configUSE_TRACE_FACILITY == 1)
-	UBaseType_t uxQueueNumber;
-	uint8_t ucQueueType;
-#endif
-
-} xQUEUE;
-
-/* The old xQUEUE name is maintained above then typedefed to the new Queue_t
-name below to enable the use of older kernel aware debuggers. */
-typedef xQUEUE Queue_t;
+// moved to header
 
 /*-----------------------------------------------------------*/
 
--- libs/include/freertos/queue.h
+++ libs/include/freertos/queue.h
@@ -73,6 +73,76 @@ typedef struct QueueDefinition* QueueSetMemberHandle_t;
 #define queueQUEUE_TYPE_BINARY_SEMAPHORE ((uint8_t)3U)
 #define queueQUEUE_TYPE_RECURSIVE_MUTEX ((uint8_t)4U)
 
+
+/* Constants used with the cRxLock and cTxLock structure members. */
+#define queueUNLOCKED ((int8_t)-1)
+#define queueLOCKED_UNMODIFIED ((int8_t)0)
+
+typedef struct QueuePointers {
+	int8_t* pcTail;     /*< Points to the byte at the end of the queue storage area.  Once more byte is allocated than
+	                       necessary to store the queue items, this is used as a marker. */
+	int8_t* pcReadFrom; /*< Points to the last place that a queued item was read from when the structure is used as a
+	                       queue. */
+} QueuePointers_t;
+
+typedef struct SemaphoreData {
+	TaskHandle_t xMutexHolder;        /*< The handle of the task that holds the mutex. */
+	UBaseType_t uxRecursiveCallCount; /*< Maintains a count of the number of times a recursive mutex has been
+	                                     recursively 'taken' when the structure is used as a mutex. */
+} SemaphoreData_t;
+
+/*
+ * Definition of the queue used by the scheduler.
+ * Items are queued by copy, not reference.  See the following link for the
+ * rationale: https://www.freertos.org/Embedded-RTOS-Queues.html
+ */
+typedef struct QueueDefinition /* The old naming convention is used to prevent breaking kernel aware debuggers. */
+{
+	int8_t* pcHead;    /*< Points to the beginning of the queue storage area. */
+	int8_t* pcWriteTo; /*< Points to the free next place in the storage area. */
+
+	union {
+		QueuePointers_t xQueue;     /*< Data required exclusively when this structure is used as a queue. */
+		SemaphoreData_t xSemaphore; /*< Data required exclusively when this structure is used as a semaphore. */
+	} u;
+
+	List_t xTasksWaitingToSend; /*< List of tasks that are blocked waiting to post onto this queue.  Stored in priority
+	                               order. */
+	List_t xTasksWaitingToReceive; /*< List of tasks that are blocked waiting to read from this queue.  Stored in
+	                                  priority order. */
+
+	volatile UBaseType_t uxMessagesWaiting; /*< The number of items currently in the queue. */
+	UBaseType_t
+	    uxLength; /*< The length of the queue defined as the number of items it will hold, not the number of bytes. */
+	UBaseType_t uxItemSize; /*< The size of each items that the queue will hold. */
+
+	volatile int8_t cRxLock; /*< Stores the number of items received from the queue (removed from the queue) while the
+	                            queue was locked.  Set to queueUNLOCKED when the queue is not locked. */
+	volatile int8_t cTxLock; /*< Stores the number of items transmitted to the queue (added to the queue) while the
+	                            queue was locked.  Set to queueUNLOCKED when the queue is not locked. */
+
+#if ((configSUPPORT_STATIC_ALLOCATION == 1) && (configSUPPORT_DYNAMIC_ALLOCATION == 1))
+	uint8_t ucStaticallyAllocated; /*< Set to pdTRUE if the memory used by the queue was statically allocated to ensure
+	                                  no attempt is made to free the memory. */
+#endif
+
+#if (configUSE_QUEUE_SETS == 1)
+	struct QueueDefinition* pxQueueSetContainer;
+#endif
+
+#if (configUSE_TRACE_FACILITY == 1)
+	UBaseType_t uxQueueNumber;
+	uint8_t ucQueueType;
+#endif
+
+} xQUEUE;
+
+/* The old xQUEUE name is maintained above then typedefed to the new Queue_t
+name below to enable the use of older kernel aware debuggers. */
+typedef xQUEUE Queue_t;
+
+
+
 /**
  * queue. h
  * <pre>
