b7a55bfc50c67f43875dae34b86eb5a53a9ff46a
Björn Fiedler <fiedler@sra.uni-hannover.de>
2020-05-14 21:13:26 +0200

    tools: start gdb in tmux

--- tools/gdb_qemu_stm32.sh
+++ tools/gdb_qemu_stm32.sh
@@ -3,9 +3,10 @@
 file=`mktemp`
 trap "rm -f -- $file" INT EXIT
 echo "target remote :33754" > $file
-qemu-system-stm32 -gdb tcp::33754 -machine stm32-p103 -nographic -serial mon:stdio -S -no-shutdown -kernel $@ &
-qemu_pid=$!
-trap "rm -f -- $file; kill $qemu_pid 2>/dev/null" INT EXIT
-cat $file
-gdb-multiarch -x $file ${1}
+#qemu_pid=$!
+#trap "rm -f -- $file; kill $qemu_pid 2>/dev/null" INT EXIT
+tmux new-session\
+	 "qemu-system-stm32 -gdb tcp::33754 -machine stm32-p103 -nographic -serial file:/dev/stdout -monitor none -S -no-shutdown -kernel $@; read" ';'\
+	 new-window \
+	 "cat $file; gdb-multiarch -x $file ${1} ; read" ';'
 
--- tools/gdb_stm32.sh
+++ tools/gdb_stm32.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-st-util &
-gdb-multiarch ${1} -ex "target extended-remote :4242" -ex "layout split"
+tmux new-session \
+st-util ';' \
+new-window \
+"gdb-multiarch ${1} -ex 'target extended-remote :4242' -ex 'layout split'" ';'
 pkill st-util
