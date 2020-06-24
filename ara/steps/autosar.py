a0452f888c1412301a0c3e189b74cd01565511b0
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-06-24 19:11:37 +0200

    LoadOIL: detect tasks
    very minimal implementation but should be enough for the minexample

--- ara/steps/autosar.py
+++ ara/steps/autosar.py
@@ -1,11 +1,29 @@
 from .os_util import syscall
 from .os_base import OSBase
 
+class Task:
+    def __init__(self, cfg, name, function, priority, activation,
+                 autostart, schedule, cpu_id):
+        self.cfg = cfg
+        self.name = name
+        self.function = function
+        self.priority = priority
+        self.activation = activation
+        self.autostart = autostart
+        self.schedule = schedule
+        self.cpu_id = cpu_id
+
 class AUTOSAR(OSBase):
+    vertex_properties = [('label', 'string', 'instance name'),
+                         ('obj', 'object', 'instance object (e.g. Task)')]
+    edge_properties = [('label', 'string', 'syscall name')]
 
     @staticmethod
-    def init(state):
-        pass
+    def init(instances):
+        for prop in AUTOSAR.vertex_properties:
+            instances.vp[prop[0]] = instances.new_vp(prop[1])
+        for prop in AUTOSAR.edge_properties:
+            instances.ep[prop[0]] = instances.new_ep(prop[1])
 
     @syscall
     def AUTOSAR_ActivateTask(cfg, abb, state):
