ef610057c5c0d628295b1e0e174669e9625dab3c
Fredo Nowak <fre.nowak@stud.uni-hannover.de>
2020-07-27 14:02:12 +0200

    created stub for file containing timing information

---
+++ appl/AUTOSAR/minexample_timing.py
@@ -0,0 +1,20 @@
+class Timings:
+    """This class contains all timing information for the minexample application.
+    For each ABB there needs to be a function returning their min and max execution times."""
+
+    def get_timings(self, abb_nr, context):
+        f = getattr(self, 'ABB_' + abb_nr + '_timings', None)
+        if f is not None:
+            return f(context)
+        return (0, None)
+    
+    def get_min_time(self, abb_nr, context):
+        res = self.get_timings(abb_nr, context)
+        return res[0]
+
+    def get_man_time(self, abb_nr, context):
+        res = self.get_timings(abb_nr, context)
+        return res[1]
+
+    def ABB_1_timings(self, context):
+        return (0, None)
