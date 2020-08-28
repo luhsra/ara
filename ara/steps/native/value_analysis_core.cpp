7f241fad76763ca26391bf5013250169d92a133c
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-08-28 18:53:19 +0200

    ValueAnalysisCore: move code into source file

--- ara/steps/native/value_analysis_core.cpp
+++ ara/steps/native/value_analysis_core.cpp
@@ -19,8 +19,46 @@
 #undef VERSION_BKP
 
 using namespace boost::property_tree;
+using namespace SVF;
 
 namespace ara::step {
+
+	void ValueAnalysisCore::getCallPaths(const llvm::Function* f,
+	                                     std::vector<std::vector<const llvm::Instruction*>>& paths,
+	                                     std::vector<const llvm::Instruction*>& curPath) {
+		SVF::PAG* pag = SVF::PAG::getPAG();
+		// this is actually a singleton, so the creation was done in SVFAnalyses
+		SVF::Andersen* ander = SVF::AndersenWaveDiff::createAndersenWaveDiff(pag);
+		SVF::PTACallGraph* callgraph = ander->getPTACallGraph();
+		// TODO check out icfg->getCallBlockNode(inst) which directly gets the callblocknode
+		// callee is getCallee(inst), caller is svfModule->getSVFFunction(f)
+		if (const SVFFunction* sf = LLVMModuleSet::getLLVMModuleSet()->getSVFFunction(f)) {
+			PTACallGraphNode* cgn = callgraph->getCallGraphNode(sf);
+			/* check if further path exists */
+			if (cgn->hasIncomingEdge()) {
+				/* incoming edges of this node */
+				for (auto edgit = cgn->InEdgeBegin(); edgit != cgn->InEdgeEnd(); ++edgit) {
+					PTACallGraphEdge* edg = *edgit;
+					PTACallGraphEdge::CallInstSet cis = edg->getDirectCalls();
+					for (const CallBlockNode* cbn : cis) {
+						const Function* cf = cbn->getCallSite()->getFunction();
+						const Instruction* ci = cbn->getCallSite();
+						curPath.push_back(ci);
+						getCallPaths(cf, paths, curPath);
+						/**
+						 * when the recursion above finishes, we have reached a root node
+						 * therefore we go back (down) one node and check its other parents
+						 */
+						curPath.pop_back();
+					}
+				}
+			}
+			/* end of this callpath reached */
+			else {
+				paths.push_back(curPath);
+			}
+		}
+	}
 
 	const llvm::Value* ValueAnalysisCore::temp_traverse(const SVFGNode* node, const SVFG& vfg,
 	                                                    std::vector<const SVFGNode*>& visited) {
@@ -318,6 +356,147 @@ namespace ara::step {
 			}
 		}
 		return vps;
+	}
+
+	Arguments ValueAnalysisCore::get_value(const llvm::BasicBlock& bb, const llvm::CallBase& called_func,
+	                                       const SVFG& vfg) {
+		vstd.clear();
+		Arguments args;
+		llvm::AttributeSet attrs;
+		llvm::Function* func = called_func.getCalledFunction();
+		if (func) {
+			// this->logger.debug() << *bb << std::endl;
+			// this->logger.debug() << "Called function: " << *called_func << std::endl;
+			this->logger.debug() << "Function name: \033[34m" << func->getName().str() << "\033[0m" << std::endl;
+			this->logger.debug() << "Number of args: " << func->arg_size() << std::endl;
+			this->logger.debug() << "------------" << std::endl;
+			// llvm::AttributeSet attrs = func->getAttributes().getFnAttributes();
+			llvm::AttributeList attrl = called_func.getAttributes();
+			for (unsigned it = attrl.index_begin(); it != attrl.index_end(); ++it) {
+				// index is sometimes negative (2**32 - 1 or something)??
+				// logger.debug() << it << ": " << attrl.getAsString(it) << std::endl;
+			}
+
+			const llvm::ConstantTokenNone* token = llvm::ConstantTokenNone::get(called_func.getContext());
+			const llvm::Constant* none_c = llvm::dyn_cast<llvm::Constant>(token);
+
+			std::vector<ValPath> vps = collectUsesOnVFG(vfg, called_func);
+			int t = 0;
+			for (auto vp : vps) {
+				attrs = attrl.getAttributes(t + 1);
+				if (std::get<0>(vp).size() != std::get<1>(vp).size()) {
+					logger.debug() << "===Number of Values: " << std::get<0>(vp).size() << std::endl;
+					logger.debug() << "===Number of Paths:  " << std::get<1>(vp).size() << std::endl;
+				}
+				if (std::get<0>(vp).size() < 1) {
+					logger.debug() << "no vals" << std::endl;
+					continue;
+				}
+
+				/**
+				 * for unambiguous values:
+				 * 	- push back an Argument with just the one value and no alternatives/paths
+				 */
+				if (std::get<0>(vp).size() == 1) {
+					const llvm::Value* constVal = std::get<0>(vp).at(0);
+					if (const Function* func = llvm::dyn_cast<llvm::Function>(constVal)) {
+						logger.debug() << "value is function: " << demangle(func->getName().str()) << std::endl;
+					} else {
+						logger.debug() << "unambiguous value: " << *constVal << std::endl;
+					}
+					args.push_back(Argument(attrs, *constVal));
+					continue;
+				}
+
+				Argument a(attrs, *none_c);
+
+				/**
+				 * i does not correspond to the index of the argument in the function
+				 * it is just here for debugging purposes
+				 */
+				long unsigned int i = 0;
+				/* we need paths >= values for this to work */
+				assert(std::get<1>(vp).size() >= std::get<0>(vp).size());
+				std::string entryFun;
+				for (auto v : std::get<0>(vp)) {
+					/* end iterator points behind the last element so we subtract 1 before dereferencing it
+					 */
+					entryFun = (*(std::get<1>(vp).at(i).end() - 1))->getFunction()->getName().str();
+
+					/* erase the syscall itself from the instruction list */
+					std::get<1>(vp).at(i).erase(std::get<1>(vp).at(i).begin());
+
+					if (const Function* func = llvm::dyn_cast<llvm::Function>(v)) {
+						logger.debug() << "Value (function) " << i << ": " << demangle(func->getName().str())
+						               << std::endl;
+					} else {
+						logger.debug() << "Value " << i << ": " << *v << std::endl;
+					}
+					logger.debug() << "PATH " << i << ": \n        |--";
+					for (auto inst : std::get<1>(vp).at(i)) {
+						logger.debug() << *inst << "\n        ---";
+					}
+					logger.debug() << "\033[33m" << demangle(entryFun) << "\033[0m|" << std::endl;
+
+					a.add_variant(std::get<1>(vp).at(i), *v);
+					i++;
+				}
+				/* print extra / leftover paths */
+				if (i < std::get<1>(vp).size()) {
+					logger.debug() << "\033[32mLEFTOVER PATHS (" << i << " to " << std::get<1>(vp).size() - 1
+					               << ")\033[0m" << std::endl;
+					for (long unsigned int j = i; j < std::get<1>(vp).size(); j++) {
+						std::get<1>(vp).at(j).erase(std::get<1>(vp).at(j).begin());
+						logger.debug() << "PATH " << j << ": \n        |--";
+						for (auto inst : std::get<1>(vp).at(j)) {
+							logger.debug() << *inst << "\n        ---";
+						}
+						logger.debug() << "\033[33m" << demangle(entryFun) << "\033[0m|" << std::endl;
+					}
+				}
+
+				args.set_entry_fun(entryFun);
+				args.push_back(a);
+				t++;
+			}
+		}
+		/* return value */
+		if (called_func.hasOneUse()) {
+			const llvm::User* ur = called_func.user_back();
+			llvm::Value* retval = ur->getOperand(1);
+			logger.debug() << "return: " << *retval << std::endl;
+			args.set_return_value(std::make_unique<Argument>(llvm::AttributeSet(), *retval));
+		}
+
+		// std::pair<Arguments, std::vector<std::vector<unsigned>>> args_pair = va.get_values(called_func);
+		// Arguments& args = args_pair.first;
+
+		// // statistic printing
+		// if (dump_stats) {
+		// 	ptree arg_stats;
+		// 	arg_stats.put("basic_block", bb->getName().str());
+		// 	llvm::Function* func = called_func.getCalledFunction();
+		// 	if (func) {
+		// 		arg_stats.put("called_function", func->getName().str());
+		// 	}
+		// 	int i = 0;
+		// 	for (auto& numbers : args_pair.second) {
+		// 		std::stringstream ss;
+		// 		ss << "argument " << i++;
+		// 		ptree stat_list;
+		// 		for (unsigned num : numbers) {
+		// 			stat_list.push_back(std::make_pair("", ptree(std::to_string(num))));
+		// 		}
+		// 		arg_stats.add_child(ss.str(), stat_list);
+		// 	}
+		// 	std::string abb_name = cfg.name[abb];
+		// 	stats.add_child(abb_name, arg_stats);
+		// }
+
+		this->logger.debug() << "Retrieved " << args.size() << " arguments for call " << called_func << std::endl;
+		this->logger.debug() << "================================================" << std::endl;
+		std::cout << std::endl;
+		return args;
 	}
 
 	llvm::json::Array ValueAnalysisCore::get_configured_dependencies() {
--- ara/steps/native/value_analysis_core.h
+++ ara/steps/native/value_analysis_core.h
@@ -13,8 +13,6 @@
 #include <cxxabi.h>
 #include <graph.h>
 
-using namespace SVF;
-
 namespace ara::step {
 	class ValueAnalysisCore : public EntryPointStep<ValueAnalysisCore> {
 	  private:
@@ -38,14 +36,16 @@ namespace ara::step {
 		}
 
 		/* a list of values and their (hopefully) corresponding paths along which they are retrieved */
-		typedef std::tuple<std::vector<const Constant*>, std::vector<std::vector<const Instruction*>>> ValPath;
+		typedef std::tuple<std::vector<const llvm::Constant*>, std::vector<std::vector<const llvm::Instruction*>>>
+		    ValPath;
 
-		const llvm::Value* temp_traverse(const SVFGNode* node, const SVFG& vfg, std::vector<const SVFGNode*>& visited);
-		std::vector<const SVFGNode*> vstd;
+		const llvm::Value* temp_traverse(const SVF::SVFGNode* node, const SVF::SVFG& vfg,
+		                                 std::vector<const SVF::SVFGNode*>& visited);
+		std::vector<const SVF::SVFGNode*> vstd;
 
-		const llvm::Constant* handle_value(const llvm::Value* value, const SVFG& vfg, const VFGNode* node);
-		ValPath retrieve_value(const SVFG& vfg, const llvm::Value& value);
-		std::vector<ValPath> collectUsesOnVFG(const SVFG& vfg, const llvm::CallBase& call);
+		const llvm::Constant* handle_value(const llvm::Value* value, const SVF::SVFG& vfg, const SVF::VFGNode* node);
+		ValPath retrieve_value(const SVF::SVFG& vfg, const llvm::Value& value);
+		std::vector<ValPath> collectUsesOnVFG(const SVF::SVFG& vfg, const llvm::CallBase& call);
 
 		/**
 		 * given a function, find its corresponding callgraph node and iterate over
@@ -54,44 +54,13 @@ namespace ara::step {
 		 *
 		 * this should just be bottom-up DFS
 		 */
-		void getCallPaths(const Function* f, std::vector<std::vector<const Instruction*>>& paths,
-		                  std::vector<const Instruction*>& curPath) {
-			SVF::PAG* pag = SVF::PAG::getPAG();
-			// this is actually a singleton, so the creation was done in SVFAnalyses
-			SVF::Andersen* ander = SVF::AndersenWaveDiff::createAndersenWaveDiff(pag);
-			SVF::PTACallGraph* callgraph = ander->getPTACallGraph();
-			// TODO check out icfg->getCallBlockNode(inst) which directly gets the callblocknode
-			// callee is getCallee(inst), caller is svfModule->getSVFFunction(f)
-			if (const SVFFunction* sf = LLVMModuleSet::getLLVMModuleSet()->getSVFFunction(f)) {
-				PTACallGraphNode* cgn = callgraph->getCallGraphNode(sf);
-				/* check if further path exists */
-				if (cgn->hasIncomingEdge()) {
-					/* incoming edges of this node */
-					for (auto edgit = cgn->InEdgeBegin(); edgit != cgn->InEdgeEnd(); ++edgit) {
-						PTACallGraphEdge* edg = *edgit;
-						PTACallGraphEdge::CallInstSet cis = edg->getDirectCalls();
-						for (const CallBlockNode* cbn : cis) {
-							const Function* cf = cbn->getCallSite()->getFunction();
-							const Instruction* ci = cbn->getCallSite();
-							curPath.push_back(ci);
-							getCallPaths(cf, paths, curPath);
-							/**
-							 * when the recursion above finishes, we have reached a root node
-							 * therefore we go back (down) one node and check its other parents
-							 */
-							curPath.pop_back();
-						}
-					}
-				}
-				/* end of this callpath reached */
-				else {
-					paths.push_back(curPath);
-				}
-			}
-		}
+		void getCallPaths(const llvm::Function* f, std::vector<std::vector<const llvm::Instruction*>>& paths,
+		                  std::vector<const llvm::Instruction*>& curPath);
+
+		Arguments get_value(const llvm::BasicBlock& bb, const llvm::CallBase& called_func, const SVF::SVFG& vfg);
 
 		template <typename Graph>
-		void get_values(Graph& g, const SVFG& vfg) {
+		void get_values(Graph& g, const SVF::SVFG& vfg) {
 			graph::CFG cfg = graph.get_cfg();
 			// ptree stats;
 			for (auto abb :
@@ -100,150 +69,12 @@ namespace ara::step {
 				llvm::CallBase* called_func = llvm::dyn_cast<llvm::CallBase>(&bb->front());
 				// logger.debug() << "call base aka instruction: " << *called_func << std::endl;
 				if (called_func) {
-					vstd.clear();
-					Arguments args;
-					llvm::AttributeSet attrs;
-					llvm::Function* func = called_func->getCalledFunction();
-					if (func) {
-						// this->logger.debug() << *bb << std::endl;
-						// this->logger.debug() << "Called function: " << *called_func << std::endl;
-						this->logger.debug()
-						    << "Function name: \033[34m" << func->getName().str() << "\033[0m" << std::endl;
-						this->logger.debug() << "Number of args: " << func->arg_size() << std::endl;
-						this->logger.debug() << "------------" << std::endl;
-						// llvm::AttributeSet attrs = func->getAttributes().getFnAttributes();
-						llvm::AttributeList attrl = called_func->getAttributes();
-						for (unsigned it = attrl.index_begin(); it != attrl.index_end(); ++it) {
-							// index is sometimes negative (2**32 - 1 or something)??
-							// logger.debug() << it << ": " << attrl.getAsString(it) << std::endl;
-						}
-
-						const llvm::ConstantTokenNone* token = llvm::ConstantTokenNone::get(called_func->getContext());
-						const llvm::Constant* none_c = llvm::dyn_cast<llvm::Constant>(token);
-
-						std::vector<ValPath> vps = collectUsesOnVFG(vfg, *called_func);
-						int t = 0;
-						for (auto vp : vps) {
-							attrs = attrl.getAttributes(t + 1);
-							if (std::get<0>(vp).size() != std::get<1>(vp).size()) {
-								logger.debug() << "===Number of Values: " << std::get<0>(vp).size() << std::endl;
-								logger.debug() << "===Number of Paths:  " << std::get<1>(vp).size() << std::endl;
-							}
-							if (std::get<0>(vp).size() < 1) {
-								logger.debug() << "no vals" << std::endl;
-								continue;
-							}
-
-							/**
-							 * for unambiguous values:
-							 * 	- push back an Argument with just the one value and no alternatives/paths
-							 */
-							if (std::get<0>(vp).size() == 1) {
-								const llvm::Value* constVal = std::get<0>(vp).at(0);
-								if (const Function* func = llvm::dyn_cast<llvm::Function>(constVal)) {
-									logger.debug()
-									    << "value is function: " << demangle(func->getName().str()) << std::endl;
-								} else {
-									logger.debug() << "unambiguous value: " << *constVal << std::endl;
-								}
-								args.push_back(Argument(attrs, *constVal));
-								continue;
-							}
-
-							Argument a(attrs, *none_c);
-
-							/**
-							 * i does not correspond to the index of the argument in the function
-							 * it is just here for debugging purposes
-							 */
-							long unsigned int i = 0;
-							/* we need paths >= values for this to work */
-							assert(std::get<1>(vp).size() >= std::get<0>(vp).size());
-							std::string entryFun;
-							for (auto v : std::get<0>(vp)) {
-								/* end iterator points behind the last element so we subtract 1 before dereferencing it
-								 */
-								entryFun = (*(std::get<1>(vp).at(i).end() - 1))->getFunction()->getName().str();
-
-								/* erase the syscall itself from the instruction list */
-								std::get<1>(vp).at(i).erase(std::get<1>(vp).at(i).begin());
-
-								if (const Function* func = llvm::dyn_cast<llvm::Function>(v)) {
-									logger.debug() << "Value (function) " << i << ": "
-									               << demangle(func->getName().str()) << std::endl;
-								} else {
-									logger.debug() << "Value " << i << ": " << *v << std::endl;
-								}
-								logger.debug() << "PATH " << i << ": \n        |--";
-								for (auto inst : std::get<1>(vp).at(i)) {
-									logger.debug() << *inst << "\n        ---";
-								}
-								logger.debug() << "\033[33m" << demangle(entryFun) << "\033[0m|" << std::endl;
-
-								a.add_variant(std::get<1>(vp).at(i), *v);
-								i++;
-							}
-							/* print extra / leftover paths */
-							if (i < std::get<1>(vp).size()) {
-								logger.debug() << "\033[32mLEFTOVER PATHS (" << i << " to "
-								               << std::get<1>(vp).size() - 1 << ")\033[0m" << std::endl;
-								for (long unsigned int j = i; j < std::get<1>(vp).size(); j++) {
-									std::get<1>(vp).at(j).erase(std::get<1>(vp).at(j).begin());
-									logger.debug() << "PATH " << j << ": \n        |--";
-									for (auto inst : std::get<1>(vp).at(j)) {
-										logger.debug() << *inst << "\n        ---";
-									}
-									logger.debug() << "\033[33m" << demangle(entryFun) << "\033[0m|" << std::endl;
-								}
-							}
-
-							args.set_entry_fun(entryFun);
-							args.push_back(a);
-							t++;
-						}
-					}
-					/* return value */
-					if (called_func->hasOneUse()) {
-						const llvm::User* ur = called_func->user_back();
-						llvm::Value* retval = ur->getOperand(1);
-						logger.debug() << "return: " << *retval << std::endl;
-						args.set_return_value(std::make_unique<Argument>(llvm::AttributeSet(), *retval));
-					}
-
-					// std::pair<Arguments, std::vector<std::vector<unsigned>>> args_pair = va.get_values(*called_func);
-					// Arguments& args = args_pair.first;
-
-					// // statistic printing
-					// if (dump_stats) {
-					// 	ptree arg_stats;
-					// 	arg_stats.put("basic_block", bb->getName().str());
-					// 	llvm::Function* func = called_func->getCalledFunction();
-					// 	if (func) {
-					// 		arg_stats.put("called_function", func->getName().str());
-					// 	}
-					// 	int i = 0;
-					// 	for (auto& numbers : args_pair.second) {
-					// 		std::stringstream ss;
-					// 		ss << "argument " << i++;
-					// 		ptree stat_list;
-					// 		for (unsigned num : numbers) {
-					// 			stat_list.push_back(std::make_pair("", ptree(std::to_string(num))));
-					// 		}
-					// 		arg_stats.add_child(ss.str(), stat_list);
-					// 	}
-					// 	std::string abb_name = cfg.name[abb];
-					// 	stats.add_child(abb_name, arg_stats);
-					// }
-
+					Arguments args = get_value(safe_deref(bb), safe_deref(called_func), vfg);
 					cfg.arguments[abb] = boost::python::object(boost::python::handle<>(args.get_python_list()));
-					this->logger.debug() << "Retrieved " << args.size() << " arguments for call " << *called_func
-					                     << std::endl;
-					this->logger.debug() << "================================================" << std::endl;
-					std::cout << std::endl;
 				} else {
-					this->logger.warn() << "Something went wrong." << std::endl;
-					this->logger.debug() << "In function: " << bb->getParent()->getName().str() << std::endl;
-					this->logger.debug() << "Basicblock: " << *bb << std::endl;
+					std::stringstream ss;
+					ss << "Syscall ABB (" << cfg.name[abb] << ") has abigoues function";
+					fail(ss.str());
 				}
 			}
 			// if (dump_stats) {
