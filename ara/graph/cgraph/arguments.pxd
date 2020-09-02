59ebc81dd6bf8aa99cfc9289cdc10177f48bfb0b
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-09-03 00:54:53 +0200

    WIP: try to connect graph_data and libgraph

--- ara/graph/cgraph/arguments.pxd
+++ ara/graph/cgraph/arguments.pxd
@@ -1,12 +1,15 @@
 # cython: language_level=3
 # vim: set et ts=4 sw=4:
 
+from libcpp cimport bool
+
 cdef extern from "arguments.h" namespace "ara::graph":
     cdef cppclass CallPath:
         pass
 
     cdef cppclass Argument:
-        pass
+        bool is_determined()
+        bool is_constant()
 
     cdef cppclass Arguments:
         pass
--- ara/graph/cgraph/graph_data.pyx
+++ ara/graph/cgraph/graph_data.pyx
@@ -6,9 +6,16 @@ from .graph_data cimport PyGraphData
 from .arguments cimport Argument as CArgument, Arguments as CArguments
 
 from libcpp.memory cimport unique_ptr, shared_ptr
+from cython.operator cimport dereference as deref
 
 cdef class Argument:
     cdef shared_ptr[CArgument] _c_argument
+
+    def is_determined(self):
+        return deref(self._c_argument).is_determined()
+
+    def is_constant(self):
+        return deref(self._c_argument).is_constant()
 
 cdef class Arguments:
     cdef unique_ptr[CArguments] _c_arguments
