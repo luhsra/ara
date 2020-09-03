f5b0ab672a101e8c2864269c33d3358d58c7948f
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-09-04 01:03:27 +0200

    common/util.h: make safe_deref compatible with smart pointers

--- ara/common/common/util.h
+++ ara/common/common/util.h
@@ -3,9 +3,29 @@
  */
 #pragma once
 
+#include <type_traits>
+#include <cassert>
+
 namespace ara {
+	template <typename T>
+	class remove_pointer_ {
+		template <typename U = T>
+		static auto test(int) -> std::remove_reference<decltype(*std::declval<U>())>;
+		static auto test(...) -> std::remove_cv<T>;
+
+	  public:
+		using type = typename decltype(test(0))::type;
+	};
+
+	template <typename T>
+	using remove_pointer = typename remove_pointer_<T>::type;
+
+	/**
+	 * Dereference any kind of pointer, raw or smart, and return a reference.
+	 * While dereferencing, it checks for a null pointer and fails in error case.
+	 */
 	template <class T>
-	T& safe_deref(T* t) {
+	inline remove_pointer<T>& safe_deref(T&& t) {
 		assert(t != nullptr && "t is not null");
 		return *t;
 	}
