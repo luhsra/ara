fb7bdb05a0d9ab3ba77afb33338caa9f5ee68411
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-09-04 17:08:50 +0200

    common/llvm_common: put in ara namespace

--- ara/common/common/llvm_common.h
+++ ara/common/common/llvm_common.h
@@ -5,17 +5,20 @@
 #include <llvm/IR/Instructions.h>
 #include <memory>
 
-/**
- * @brief get string representation of llvm type
- * @param argument llvm::type to print
- */
-std::string print_type(llvm::Type* argument);
+namespace ara {
 
-/**
- * @brief check if the instruction is just llvm specific
- * @param instr instrucion to analyze
- */
-bool isCallToLLVMIntrinsic(const llvm::Instruction* inst);
-bool isInlineAsm(const llvm::Instruction* inst);
+	/**
+	 * @brief get string representation of llvm type
+	 * @param argument llvm::type to print
+	 */
+	std::string print_type(llvm::Type* argument);
+
+	/**
+	 * @brief check if the instruction is just llvm specific
+	 * @param instr instrucion to analyze
+	 */
+	bool isCallToLLVMIntrinsic(const llvm::Instruction* inst);
+	bool isInlineAsm(const llvm::Instruction* inst);
+} // namespace ara
 
 #endif
--- ara/common/llvm_common.cpp
+++ ara/common/llvm_common.cpp
@@ -3,26 +3,30 @@
 
 using namespace llvm;
 
-std::string print_type(Type* argument) {
-	std::string type_str;
-	raw_string_ostream rso(type_str);
-	argument->print(rso);
-	return rso.str() + "\"\n";
-}
+namespace ara {
 
-bool isCallToLLVMIntrinsic(const Instruction* inst) {
-	if (const CallBase* call = dyn_cast<CallBase>(inst)) {
-		const Function* func = call->getCalledFunction();
-		if (func && func->isIntrinsic()) {
-			return true;
+	std::string print_type(Type* argument) {
+		std::string type_str;
+		raw_string_ostream rso(type_str);
+		argument->print(rso);
+		return rso.str() + "\"\n";
+	}
+
+	bool isCallToLLVMIntrinsic(const Instruction* inst) {
+		if (const CallBase* call = dyn_cast<CallBase>(inst)) {
+			const Function* func = call->getCalledFunction();
+			if (func && func->isIntrinsic()) {
+				return true;
+			}
 		}
+		return false;
 	}
-	return false;
-}
 
-bool isInlineAsm(const Instruction* inst) {
-	if (const CallBase* call = dyn_cast<CallBase>(inst)) {
-		return call->isInlineAsm();
+	bool isInlineAsm(const Instruction* inst) {
+		if (const CallBase* call = dyn_cast<CallBase>(inst)) {
+			return call->isInlineAsm();
+		}
+		return false;
 	}
-	return false;
-}
+
+} // namespace ara
