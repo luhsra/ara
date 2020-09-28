e78bbe242d469d1a1bc9e479bf85a55668f63b4a
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-09-29 01:47:14 +0200

    test, value_analysis: add more test cases

--- appl/FreeRTOS/svf.cc
+++ appl/FreeRTOS/svf.cc
@@ -1,6 +1,12 @@
 #include "FreeRTOS.h"
+#include "semphr.h"
 #include "FreeRTOSConfig.h"
 #include "task.h"
+
+void task1(void*) { }
+
+static xSemaphoreHandle mutex;
+static xTaskHandle t;
 
 void do_stuff(int a) {
 	vTaskStepTick(a);
@@ -20,10 +26,16 @@ int main() {
 	int b = 333;
 	int c = 6;
 	int d = 4465;
+	xTaskHandle t2 = xTaskCreateStatic(task1, "TaskStatic", 11114, NULL, 1, NULL, NULL);
+	mutex = xSemaphoreCreateRecursiveMutex();
+	xTaskCreate(task1, "Task 1", 6667, NULL, c, NULL);
+	xTaskCreate(task1, "Task 2", 8889, NULL, c, &t);
+	xTaskCreate(task1, "Task 3", 9993, NULL, c, &t2);
 	Guard guard(d);
 	c++;
 	int x = c;
 	do_stuff(a);
 	do_stuff(b);
+	xSemaphoreTakeRecursive(mutex, 5);
 	return x;
 }
--- test/value_analysis.py
+++ test/value_analysis.py
@@ -1,15 +1,35 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3
 import json
+import pyllco
 
 # Note: init_test must be imported first
 from init_test import init_test, fail_if
 from ara.graph import ABBType, CFGView
 
+def value_to_str(value):
+    if value is None:
+        return "None"
+    if isinstance(value, pyllco.ConstantPointerNull):
+        return "null"
+    if isinstance(value, pyllco.Function):
+        return f"Function: {value.get_name()}"
+    if isinstance(value, pyllco.GlobalVariable):
+        return f"GlobalVariable: {value.get_name()}"
+    if isinstance(value, pyllco.Constant):
+        return value.get()
+    if isinstance(value, pyllco.AllocaInst):
+        return f"Alloca: {value.get_name()}"
+    return str(type(value)) + ": " + str(value)
+
+
+def debug_print(*args):
+    # print(*args)
+    pass
+
 
 def main():
     """Test for correct value analysis recognition."""
-    config = {"steps": ["ValueAnalysis", {"name": "ValueAnalysis",
-                                          "entry_point": "main"}]}
+    config = {"steps": ["ValueAnalysis"]}
     m_graph, data, _ = init_test(extra_config=config)
     cfg = m_graph.cfg
     syscalls = CFGView(cfg, vfilt=cfg.vp.type.fa == ABBType.syscall)
@@ -17,6 +37,7 @@ def main():
     values = {}
 
     for syscall in syscalls.vertices():
+        debug_print("Syscall:", syscalls.get_syscall_name(syscall))
         args = syscalls.vp.arguments[syscall]
 
         val_per_args = []
@@ -25,12 +46,22 @@ def main():
             val_per_arg = []
             for call_path, value in argument.items():
                 val_per_arg.append(
-                    [call_path.print(functions=True), value.get()]
+                    [call_path.print(functions=True), value_to_str(value)]
                 )
+            debug_print("Argument:", val_per_arg)
             val_per_args.append(val_per_arg)
+        return_value = args.get_return_value()
+        if return_value is None:
+            debug_print("no return value")
+        else:
+            assert(len(return_value) == 1)
+            debug_print("RETVAL:",
+                        value_to_str(return_value.get_value(raw=True)))
 
         values[syscalls.get_syscall_name(syscall)] = val_per_args
 
+    # import json
+    # print(json.dumps(values))
     fail_if(data != values, "Data not equal")
 
 
