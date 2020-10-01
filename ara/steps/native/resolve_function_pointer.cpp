5aefe6df55f20fb41c1949de83f31884e37edd29
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-10-01 02:54:35 +0200

    ResolveFunctionPointer: + accept/blocklist, translation_map

--- ara/steps/native/resolve_function_pointer.cpp
+++ ara/steps/native/resolve_function_pointer.cpp
@@ -9,8 +9,21 @@ namespace ara::step {
 	using namespace SVF;
 
 	std::string ResolveFunctionPointer::get_description() {
-		return "Resolve all function pointers that are not already resolved by SVF.\nThis step modifies only SVF "
-		       "datastructures.";
+		return "Resolve all function pointers that are not already resolved by SVF.\n"
+		       "This step modifies only SVF datastructures.";
+	}
+
+	void ResolveFunctionPointer::init_options() {
+		accept_list = accept_list_template.instantiate(get_name());
+		block_list = block_list_template.instantiate(get_name());
+		translation_map = translation_map_template.instantiate(get_name());
+		opts.emplace_back(accept_list);
+		opts.emplace_back(block_list);
+		opts.emplace_back(translation_map);
+	}
+
+	Step::OptionVec ResolveFunctionPointer::get_local_options() {
+		return {accept_list_template, block_list_template, translation_map_template};
 	}
 
 	/**
@@ -20,6 +33,20 @@ namespace ara::step {
 	bool ResolveFunctionPointer::is_valid_call_target(const llvm::FunctionType& caller_type,
 	                                                  const llvm::Function& candidate) const {
 		if (candidate.empty() || is_intrinsic(candidate)) {
+			return false;
+		}
+
+		const auto& cand_name = candidate.getName().str();
+
+		if (cand_name == constants::ARA_ENTRY_POINT) {
+			return false;
+		}
+
+		if (block_names.find(cand_name) != block_names.end()) {
+			return false;
+		}
+
+		if (accept_names.size() != 0 && accept_names.find(cand_name) == accept_names.end()) {
 			return false;
 		}
 
@@ -89,10 +116,37 @@ namespace ara::step {
 		}
 
 		logger.info() << "Resolve call to function pointer. Callsite: " << *call_inst << std::endl;
+
+		const auto& debug_loc = call_inst->getDebugLoc();
+		if (const auto* scope = llvm::dyn_cast<llvm::DIScope>(debug_loc.getScope())) {
+			std::filesystem::path source = std::filesystem::canonical(
+			    std::filesystem::path(scope->getDirectory().str()) / std::filesystem::path(scope->getFilename().str()));
+			unsigned line = debug_loc.getLine();
+			const auto& entry = pointer_targets.find(std::make_pair(source, line));
+			if (entry != pointer_targets.end()) {
+				logger.info() << "Link with predefined functions from translation map." << std::endl;
+				for (const auto& func_name : entry->second) {
+					const llvm::Function* func = graph.get_module().getFunction(func_name);
+					fail_if_empty(func, "translation_map", std::string("missing function ") + func_name);
+					link_indirect_pointer(cbn, callgraph, *func, module);
+				}
+				return;
+			}
+		} else {
+			logger.warn() << "Cannot find source code location of callsite " << call_inst << std::endl;
+		}
+
 		const llvm::FunctionType* call_type = call_inst->getFunctionType();
 
 		if (signature_to_func.size() == 0) {
 			for (llvm::Function& func : graph.get_module()) {
+				const auto& func_name = func.getName().str();
+				if (block_names.find(func_name) != block_names.end()) {
+					continue;
+				}
+				if (accept_names.size() != 0 && accept_names.find(func_name) == accept_names.end()) {
+					continue;
+				}
 				signature_to_func[func.getFunctionType()].emplace_back(func);
 			}
 		}
@@ -155,10 +209,82 @@ namespace ara::step {
 		logger.info() << "Handled " << handled_blocks << " blocks with unresolved function pointers\n" << std::endl;
 	}
 
+	void ResolveFunctionPointer::parse_json(const char* opt_name, option::TOptEntity<option::String>& option,
+	                                        std::function<void(const llvm::json::Array&)> do_with_array) {
+		const auto& opt = option.get();
+		if (opt) {
+			std::string source;
+			// check if list is a file or directy a json list
+			// TODO replace this with startswith, once we have C++20
+			if (opt->find("[") != 0) {
+				std::ifstream ifs(*opt);
+				source = std::string((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+			} else {
+				source = *opt;
+			}
+
+			// actual JSON parsing
+			auto val = llvm::json::parse(source);
+			fail_if_empty(val, opt_name, "LLVM JSON parsing failed");
+			llvm::json::Array* arr = val->getAsArray();
+			fail_if_empty(arr, opt_name, "Invalid JSON. Expecting a list.");
+
+			do_with_array(*arr);
+		}
+	}
+
+	void ResolveFunctionPointer::parse_list(const char* opt_name, option::TOptEntity<option::String>& option,
+	                                        std::set<std::string>& target) {
+		target.clear();
+
+		std::function<void(const llvm::json::Array&)> do_with_array = [&](const llvm::json::Array& arr) {
+			for (const llvm::json::Value& func_val : arr) {
+				auto func = func_val.getAsString();
+				fail_if_empty(func, opt_name, "Invalid JSON. Expecting a list of strings.");
+				target.insert(func->str());
+			}
+		};
+		parse_json(opt_name, option, do_with_array);
+	}
+
+	void ResolveFunctionPointer::parse_translation_map(const char* opt_name,
+	                                                   option::TOptEntity<option::String>& option) {
+		pointer_targets.clear();
+		std::function<void(const llvm::json::Array&)> do_with_array = [&](const llvm::json::Array& arr) {
+			const auto& opt = option.get();
+			for (const llvm::json::Value& entry : arr) {
+				auto entry_d = entry.getAsObject();
+				fail_if_empty(entry_d, opt_name, "Invalid JSON. Expecting a list of dicts.");
+				auto source_file = entry_d->getString("source_file");
+				fail_if_empty(source_file, opt_name, "Invalid JSON. Expecting a source_file entry.");
+				std::filesystem::path source_path = std::filesystem::canonical(
+				    std::filesystem::path(*opt).parent_path() / std::filesystem::path(*source_file));
+				auto line_number = entry_d->getInteger("line_number");
+				fail_if_empty(line_number, opt_name, "Invalid JSON. Expecting a line_number entry.");
+				auto call_targets = entry_d->getArray("call_targets");
+				fail_if_empty(call_targets, opt_name, "Invalid JSON. Expecting a call_targets entry.");
+
+				std::set<string> call_t;
+				for (const llvm::json::Value& func_val : *call_targets) {
+					auto func = func_val.getAsString();
+					fail_if_empty(func, opt_name, "Invalid JSON. Expecting a list of strings as call_targets.");
+					call_t.insert(func->str());
+				}
+				pointer_targets[std::make_pair<std::filesystem::path, unsigned>(std::move(source_path), *line_number)] =
+				    std::move(call_t);
+			}
+		};
+		parse_json(opt_name, option, do_with_array);
+	}
+
 	void ResolveFunctionPointer::run() {
 		dl = DataLayout(&graph.get_module());
 		SVF::PAG* pag = SVF::PAG::getPAG();
 		SVF::ICFG* icfg = safe_deref(pag).getICFG();
+
+		parse_list("accept_list", accept_list, accept_names);
+		parse_list("block_list", block_list, block_names);
+		parse_translation_map("translation_map", translation_map);
 
 		// this is actually a singleton, so the creation was done in SVFAnalyses
 		SVF::Andersen* ander = SVF::AndersenWaveDiff::createAndersenWaveDiff(pag);
--- ara/steps/native/resolve_function_pointer.h
+++ ara/steps/native/resolve_function_pointer.h
@@ -4,14 +4,60 @@
 
 #include "step.h"
 
+#include <filesystem>
 #include <graph.h>
 #include <optional>
 
 namespace ara::step {
+	/**
+	 * Resolve function pointer. The algorithm works with multiple steps:
+	 * Do for every function pointer:
+	 *   1. If a translation_map is given, use always this.
+	 *   2. Match the signature. If a signature is found that fits and is either in the accept_list if given or not in
+	 *      the block_list if given, then the call target is valid.
+	 *   3. If no direct fitting signature is found, match the sizes of the arguments. If the argument is a pointer,
+	 *      match the size of the pointee. If a fitting target is found that is either in the accept_list if given or
+	 *      not in the block_list if given, then the call target is valid.
+	 *
+	 * The format of the accept_list and block_list is either a file path pointing to a valid file with a string or
+	 * directly a string that can be interpreted as JSON list. The list must be in the format
+	 * [ "function_name_1", "function_name_2" ].
+	 * The function names are the mangled names (if C++ is used as source).
+	 *
+	 * The translation map is a file path pointing to a valid file that can be interpreted as JSON list with the
+	 * following format:
+	 * [
+	 *     {
+	 *         "source_file": "/path/to/source_file.c",
+	 *         "line_number": 5,
+	 *         "call_targets": [ "function_name_1", "function_name_2" ]
+	 *     }
+	 * ]
+	 * Interpreted as: The callsite in /path/to/source_file.c line 5 calls either function_name_1 or function_name_2.
+	 * The source_file path must either be absolute or relative to the working directory of the ARA invocation.
+	 */
 	class ResolveFunctionPointer : public ConfStep<ResolveFunctionPointer> {
 	  private:
+		const static inline option::TOption<option::String> accept_list_template{
+		    "accept_list", "Filepath or JSON list with valid call targets for function pointer."};
+		option::TOptEntity<option::String> accept_list;
+
+		const static inline option::TOption<option::String> block_list_template{
+		    "block_list", "Filepath or JSON list with invalid call targets for function pointer."};
+		option::TOptEntity<option::String> block_list;
+
+		const static inline option::TOption<option::String> translation_map_template{
+		    "translation_map", "Filepath or JSON dictionary with a manual function pointer mapping.\nFor the format "
+		                       "see resolve_function_pointer.h"};
+		option::TOptEntity<option::String> translation_map;
+
+		virtual void init_options() override;
+
 		std::optional<llvm::DataLayout> dl;
 		std::map<const llvm::FunctionType*, std::vector<std::reference_wrapper<llvm::Function>>> signature_to_func;
+		std::set<std::string> block_names;
+		std::set<std::string> accept_names;
+		std::map<std::pair<std::filesystem::path, unsigned>, std::set<std::string>> pointer_targets;
 
 		void link_indirect_pointer(const SVF::CallBlockNode& cbn, SVF::PTACallGraph& callgraph,
 		                           const llvm::Function& target, const SVF::LLVMModuleSet& svfModule);
@@ -21,12 +67,29 @@ namespace ara::step {
 		void resolve_indirect_function_pointers(SVF::ICFG& icfg, SVF::PTACallGraph& callgraph,
 		                                        const SVF::LLVMModuleSet& module);
 
+		// const T& obj not possible since operator bool for llvm::Expected is not const
+		template <class T, class String>
+		void fail_if_empty(T& obj, const char* opt_name, String additional_message) {
+			if (!obj) {
+				std::stringstream ss;
+				ss << opt_name << ": " << additional_message;
+				fail(ss.str());
+			}
+		}
+
+		void parse_json(const char* opt_name, option::TOptEntity<option::String>& option,
+		                std::function<void(const llvm::json::Array&)> do_with_array);
+		void parse_list(const char* opt_name, option::TOptEntity<option::String>& option,
+		                std::set<std::string>& target);
+		void parse_translation_map(const char* opt_name, option::TOptEntity<option::String>& option);
+
 	  public:
 		ResolveFunctionPointer(PyObject* py_logger, graph::Graph graph, PyObject* py_step_manager)
 		    : ConfStep<ResolveFunctionPointer>(py_logger, graph, py_step_manager), dl(std::nullopt) {}
 
 		static std::string get_name() { return "ResolveFunctionPointer"; }
 		static std::string get_description();
+		static Step::OptionVec get_local_options();
 
 		virtual std::vector<std::string> get_single_dependencies() override { return {"SVFAnalyses"}; }
 
