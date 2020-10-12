5099706504d8c63df751150285f070cba3e7474e
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-10-12 18:00:56 +0200

    CFGStats: dump to json file

--- ara/steps/cfg_stats.py
+++ ara/steps/cfg_stats.py
@@ -1,71 +1,80 @@
 """Container for CFGStats."""
 from ara.graph import ABBType, CFGView, CFType, Graph
 from .step import Step
+from graph_tool.topology import label_components
 
 import graph_tool
+import json
 import sys
+import numpy
 
 class CFGStats(Step):
     """Gather statistics about the Control Flow Graph."""
 
     def get_single_dependencies(self):
-        return ["IRReader"]
+        return ["LLVMMap"]
 
     def run(self):
-        self._log.info("Executing CFGStats step.")
         cfg = self._graph.cfg
+        icfg = CFGView(cfg, efilt=cfg.ep.type.fa == CFType.icf)
+        lcfg = CFGView(cfg, efilt=cfg.ep.type.fa == CFType.lcf)
+        syscalls = CFGView(cfg, vfilt=cfg.vp.type.fa == ABBType.syscall)
+        calls = CFGView(cfg, vfilt=cfg.vp.type.fa == ABBType.call)
+        computation = CFGView(cfg, vfilt=cfg.vp.type.fa == ABBType.computation)
+        functs = CFGView(cfg, vfilt=cfg.vp.is_function)
+        abbs = CFGView(cfg,
+                       vfilt=numpy.logical_and(cfg.vp.type.fa != ABBType.not_implemented,
+                                               numpy.logical_not(cfg.vp.is_function.fa)))
 
-        # Count number of Nodes (ABBs + non implemented functions)
-        num_abbs = 0;
+        for abb in abbs.vertices():
+            print("abb", abbs.vp.name[abb])
 
-        for v in cfg.vertices() :
-            if self._graph.cfg.vp.is_function[v]:
-                continue
-            num_abbs += 1
+        num_abbs = abbs.num_vertices()
+        num_syscalls = syscalls.num_vertices()
+        num_calls = calls.num_vertices()
+        num_computation = computation.num_vertices()
+        num_functions = functs.num_vertices()
+        num_ledges = lcfg.num_edges()
+        num_iedges = icfg.num_edges()
 
-        self._log.info("Number of ABBs: " + str(num_abbs))
+        # components
+        _, lhist = label_components(lcfg, None, None, False)
+        num_lcomp = len(lhist)
 
-        # Count number of control flow sensitive edges
-
-        num_ledges = 0;
-        num_iedges = 0;
-
-        for e in cfg.edges() :
-            if cfg.ep.type[e] == CFType.lcf:
-                num_ledges += 1
-            if cfg.ep.type[e] == CFType.icf:
-                num_iedges += 1
-
-        self._log.info("Number of local edges: " + str(num_ledges))
-        self._log.info("Number of interp. edges: " + str(num_iedges))
-
-        # Count number of connected components
-        lcfg = CFGView(cfg, efilt=lambda e: cfg.ep.type[e] != CFType.icf)
-        icfg = CFGView(cfg, efilt=lambda e: cfg.ep.type[e] != CFType.lcf)
-
-        num_lcomp = 0
-        lproperty_map, lhist = graph_tool.topology.label_components(lcfg,
-                                                                    None,
-                                                                    None,
-                                                                    False)
-        for comp in lhist :
-            num_lcomp += 1
-        self._log.info("Number of local components: " + str(num_lcomp))
-
-        num_icomp = 0
-        iproperty_map, ihist = graph_tool.topology.label_components(icfg,
-                                                                    None,
-                                                                    None,
-                                                                    False)
-        for comp in ihist :
-            num_icomp += 1
-        self._log.info(f"Number of interprocedural components: {num_icomp}")
+        _, ihist = label_components(icfg, None, None, False)
+        num_icomp = len(ihist)
 
         # Calculate cyclomatic complexities
         lv = num_ledges - num_abbs + 2 * num_lcomp
         iv = num_iedges - num_abbs + 2 * num_icomp
 
+        self._log.info(f"Number of ABBs: {num_abbs}")
+        self._log.info(f"Number of syscalls: {num_syscalls}")
+        self._log.info(f"Number of calls: {num_calls}")
+        self._log.info(f"Number of computation: {num_computation}")
+        self._log.info(f"Number of functions: {num_functions}")
+        self._log.info(f"Number of local edges: {num_ledges}")
+        self._log.info(f"Number of interp. edges: {num_iedges}")
+        self._log.info(f"Number of local components: {num_lcomp}")
+        self._log.info(f"Number of interprocedural components: {num_icomp}")
         self._log.info(f"Local cyclomatic complexity: {lv}")
         self._log.info(f"Interprocedural cyclomatic complexity: {iv}")
-        # Only used for bulk testing
-        sys.stdout.write(str(num_abbs) + " " + str(lv) + " " + str(iv) + "\n")
+
+        if self.dump.get():
+            uuid = self._step_manager.get_execution_id()
+            stat_file = f'{uuid}.json'
+            stat_file = self.dump_prefix.get() + stat_file
+
+            with open(stat_file, 'w') as f:
+                json.dump({"num_abbs": num_abbs,
+                           "num_syscalls": num_syscalls,
+                           "num_calls": num_calls,
+                           "num_computation": num_computation,
+                           "num_functions": num_functions,
+                           "num_local_edges": num_ledges,
+                           "num_interprocedural_edges": num_iedges,
+                           "num_local_components": num_lcomp,
+                           "num_interprocedural_components": num_icomp,
+                           "local_cyclomatic_complexity": lv,
+                           "interprocedural_cyclomatic_complexity": iv}, f,
+                          indent=4)
