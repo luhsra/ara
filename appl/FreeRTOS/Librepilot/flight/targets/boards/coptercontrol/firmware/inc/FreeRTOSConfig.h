a742b957d31fc6e01f7ddbf5151ef26a479cc6ec
Björn Fiedler <fiedler@sra.uni-hannover.de>
2020-10-15 16:10:10 +0200

    librepilot: fix configTOTAL_HEAP_SIZE

--- appl/FreeRTOS/Librepilot/flight/targets/boards/coptercontrol/firmware/inc/FreeRTOSConfig.h
+++ appl/FreeRTOS/Librepilot/flight/targets/boards/coptercontrol/firmware/inc/FreeRTOSConfig.h
@@ -30,7 +30,7 @@
 #define configTICK_RATE_HZ                           ((portTickType)1000)
 #define configMAX_PRIORITIES                         5
 #define configMINIMAL_STACK_SIZE                     ((unsigned short)48)
-#define configTOTAL_HEAP_SIZE                        ((size_t)(53 * 256))
+#define configTOTAL_HEAP_SIZE                        ((size_t)(55 * 256))
 #define configMAX_TASK_NAME_LEN                      (6)
 #define configUSE_TRACE_FACILITY                     0
 #define configUSE_16_BIT_TICKS                       0
