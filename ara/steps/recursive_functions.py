d317343da51daad1c8063017d082e040c5837428
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-11-10 19:31:22 +0100

    RecursiveFunctions: speed up

--- ara/steps/recursive_functions.py
+++ ara/steps/recursive_functions.py
@@ -1,8 +1,9 @@
 """Container for RecursiveFuntions."""
-from ara.graph import Graph
+from ara.graph import Graph, CFGView
 from .step import Step
 
 from graph_tool.topology import all_paths
+import numpy
 
 class RecursiveFunctions(Step):
     """Mark all function that are in the Callgraph as recursive or not."""
@@ -14,15 +15,22 @@ class RecursiveFunctions(Step):
         callgraph = self._graph.callgraph
         cfg = self._graph.cfg
 
-        visited = set()
+        to_visit = callgraph.new_vp("bool")
+        to_visit.a = numpy.invert(callgraph.vp.recursive.a)
+        callgraph.set_vertex_filter(to_visit)
 
         for v in callgraph.vertices():
-            if v in visited:
-                continue
+            path_nodes = []
             for path in all_paths(callgraph, v, v):
                 for circuit_node in path:
-                    callgraph.vp.recursive[callgraph.vertex(circuit_node)] = True
-                    visited.add(circuit_node)
+                    cn = callgraph.vertex(circuit_node)
+                    callgraph.vp.recursive[cn] = True
+                    path_nodes.append(cn)
+            for n in path_nodes + [v]:
+                to_visit[n] = False
+            callgraph.set_vertex_filter(to_visit)
+
+        callgraph.clear_filters()
 
         if self.dump.get():
             self._step_manager.chain_step(
