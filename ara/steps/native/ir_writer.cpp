6c252cba0a05019ec2cf40ff729247ca3ac6ebf1
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-11-27 15:32:50 +0100

    IRWriter: add output to log and for specified functions

--- ara/steps/native/ir_writer.cpp
+++ ara/steps/native/ir_writer.cpp
@@ -2,41 +2,58 @@
 
 #include "ir_writer.h"
 
+#include <boost/algorithm/string.hpp>
 #include <llvm/Support/FileSystem.h>
 
 namespace ara::step {
-
-	// ATTENTION: put in anonymous namespace to get an unique symbol
-	namespace {
-		template <typename Graph>
-		void do_graph_stuff(Graph& g, graph::CFG& cfg, Logger& logger) {
-			for (auto v : boost::make_iterator_range(boost::vertices(g))) {
-				logger.debug() << "Vertex: " << cfg.name[v] << std::endl;
-			}
-		}
-	} // namespace
-
-	std::string IRWriter::get_description() { return "Print current IR code to file."; }
+	std::string IRWriter::get_description() { return "Print current IR code."; }
 
 	void IRWriter::init_options() {
 		ir_file_option = ir_file_option_template.instantiate(get_name());
 		opts.emplace_back(ir_file_option);
+		functions_opt = functions_opt_template.instantiate(get_name());
+		opts.emplace_back(functions_opt);
 	}
 
 	void IRWriter::run() {
-		logger.info() << "Execute IRWriter step." << std::endl;
-
 		const auto& filename = ir_file_option.get();
-		logger.info() << "Writing IR to " << *filename << '.' << std::endl;
+		std::string fn = *filename;
+		boost::replace_all(fn, "{dump_prefix}", *dump_prefix.get());
 
-		std::error_code error;
-		llvm::raw_fd_ostream out_file(*filename, error, llvm::sys::fs::OpenFlags::OF_Text);
-		if (out_file.has_error()) {
-			logger.err() << out_file.error() << std::endl;
-			out_file.close();
-			std::abort();
+		logger.info() << "Writing IR to " << fn << '.' << std::endl;
+		llvm::raw_ostream* output = nullptr;
+		std::unique_ptr<llvm::raw_fd_ostream> out_file = nullptr;
+		if (fn == "log") {
+			output = &logger.info().llvm_ostream();
+		} else {
+			std::error_code error;
+			out_file = std::make_unique<llvm::raw_fd_ostream>(fn, error, llvm::sys::fs::OpenFlags::OF_Text);
+			if (out_file->has_error()) {
+				logger.err() << out_file->error() << std::endl;
+				out_file->close();
+				fail("Error opening file");
+			}
+			output = out_file.get();
 		}
-		graph.get_module().print(out_file, nullptr);
-		out_file.close();
+
+		const auto& mod = graph.get_module();
+
+		const auto& functions = functions_opt.get();
+		if (functions) {
+			std::unordered_set<std::string> functs(functions->begin(), functions->end());
+			for (const auto& func : mod) {
+				if (functs.find(func.getName().str()) != functs.end()) {
+					func.print(*output, nullptr);
+				}
+			}
+		} else {
+			mod.print(*output, nullptr);
+		}
+
+		if (out_file) {
+			out_file->close();
+		} else {
+			logger.info().flush();
+		}
 	}
 } // namespace ara::step
--- ara/steps/native/ir_writer.h
+++ ara/steps/native/ir_writer.h
@@ -11,17 +11,25 @@ namespace ara::step {
 	class IRWriter : public ConfStep<IRWriter> {
 	  private:
 		using ConfStep<IRWriter>::ConfStep;
-		const static inline option::TOption<option::String> ir_file_option_template{"ir_file",
-		                                                                            "Filename to write ir code into",
-		                                                                            /* ty = */ option::String(),
-		                                                                            /* default = */ "dumps/dumped.ir"};
+		const static inline option::TOption<option::String> ir_file_option_template{
+		    "ir_file",
+		    "Filename to write ir code into, the special name 'log' will write to the logging output with level info.",
+		    /* ty = */ option::String(),
+		    /* default = */ "{dump_prefix}dumped.ll"};
 		option::TOptEntity<option::String> ir_file_option;
+
+		const static inline option::TOption<option::List<option::String>> functions_opt_template{
+		    "functions", "Dump IR only for these specific functions (if not specified dump all)"};
+		option::TOptEntity<option::List<option::String>> functions_opt;
+
 		virtual void init_options() override;
 
 	  public:
 		static std::string get_name() { return "IRWriter"; }
 		static std::string get_description();
-		static Step::OptionVec get_local_options() { return {ir_file_option_template}; }
+		static Step::OptionVec get_local_options() { return {ir_file_option_template, functions_opt_template}; }
+
+		virtual std::vector<std::string> get_single_dependencies() override { return {"IRReader"}; }
 
 		virtual void run() override;
 	};
