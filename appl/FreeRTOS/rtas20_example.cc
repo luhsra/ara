2ce440e0efba9c66fc82e7680f2a272a5407ef5e
Björn Fiedler <fiedler@sra.uni-hannover.de>
2020-12-03 15:04:03 +0100

    appl/FreeRTOS: fix grouping for rtas20_example

--- appl/FreeRTOS/rtas20_example.cc
+++ appl/FreeRTOS/rtas20_example.cc
@@ -1,54 +1,98 @@
+#include "FreeRTOSConfig.h"
+#define configSUPPORT_STATIC_ALLOCATION 1
 #include "FreeRTOS.h"
+#include "task.h"
 #include "queue.h"
 #include "semphr.h"
+#include "message_buffer.h"
+#ifdef __cplusplus
+extern "C" {
+#if 0
+}
+#endif
+#endif
+
+
+#define SemaphoreHandle_t QueueHandle_t
+#define xTaskCreate(name, fn, stack, prio) xTaskCreate((TaskFunction_t)fn, name, stack, NULL, prio, NULL)
+#define xTaskCreateStatic(name, fn, ssize, prio, stack, tcb)\
+  xTaskCreate(name, fn, ssize, prio)
+/* xTaskCreateStatic((TaskFunction_t)fn, name, ssize, NULL, prio, stack, tcb) */
+#define vStartScheduler() vTaskStartScheduler()
+#define xCreateMutex() xSemaphoreCreateMutex()
 void do_preparations();
 void do_critical();
 void do_critical2();
 void do_work();
 int unknown_condition();
-void uncertainTask(void *param);
-void *uncertainArgument = (void *) (((int)uncertainTask) & ~(1<<31)); void argumentTask(void*);
-QueueHandle_t mutex;
+void uncertainTask();
+void *uncertainArgument = (void *) (((int)uncertainTask) & ~(1<<31));
+void argumentTask();
+void useComputation();
 
-void useComputation(void *param);
+void t1_entry();
+void t2_entry();
+void t3_entry();
+void t4_entry();
+void t5_entry();
+void library_init();
+int dynPrio = ((int)t1_entry>>3)&3;
+void printf(char *) {return;}
 
-void task1(void *param) {
-  do_preparations();
-  mutex = xSemaphoreCreateMutex();
-  xTaskCreate(useComputation, "User", 100, NULL, 1, NULL);
-  for(;;) {
-    if (xQueueSemaphoreTake(mutex, portMAX_DELAY) == pdTRUE) {
-      do_critical2();
-      xSemaphoreGive(mutex);
-    }
-  }
-  do_work();
-}
 
-void useComputation(void *param) {
-  for (;;) {
-    if (xQueueSemaphoreTake(mutex, portMAX_DELAY) == pdTRUE) {
-      do_critical();
-      xSemaphoreGive(mutex);
-    }
-  }
-}
+
+/// SICHTBAR ab hier
+
+//startx1
+StackType_t stack[1024];
+StaticTask_t tcb;
 
 int main() {
-  xTaskCreate(task1, "Task1", 100, NULL, 2, NULL);
+  printf("Starting system\n");
+  xTaskCreate("task1", t1_entry, 512, 2);
   if (unknown_condition()) {
-    xTaskCreate(uncertainTask, "uncertainTask", 120, NULL, 1, NULL);
+    xTaskCreate("task3", t3_entry, 128, 1);
   }
-  xTaskCreate(argumentTask, "argumentTask", 110, uncertainArgument, 3, NULL);
-  vTaskStartScheduler();
+  library_init();
+  xTaskCreateStatic("task4", t4_entry,
+                    256, 2, stack, &tcb);
+  vStartScheduler();
+}
+SemaphoreHandle_t mutex1;
+void library_init() {
+    mutex1 = xCreateMutex();
+    return;
+}
+//endx1
+//startx2
+void t1_entry() {
+  xTaskCreate("task2", t2_entry, 256, dynPrio);
+  for(;;) { /* ... */ }
 }
 
+void t2_entry() { for (;;) { /* ... */  } }
 
-void uncertainTask(void *) {
+void t3_entry() { for (;;) { /* ... */  } }
+
+void t4_entry() {
+    xTaskCreate("task5", t5_entry, 512, 2);
+    for (;;) { /* ... */  }
+}
+
+MessageBufferHandle_t buffer1;
+void t5_entry() {
+    buffer1 = xMessageBufferCreate(12);
+    for (;;) { /* ... */  }
+}
+
+//endx2
+
+
+void uncertainTask() {
   for(;;);
 }
 
-void argumentTask(void *) {
+void argumentTask() {
   for(;;);
 }
 
@@ -63,10 +107,9 @@ void do_critical() {
 
 void do_critical2() {
   critical_data -=  (int)main-13;
-  
+
 }
 
-void do_preparations() {
-  critical_data = (int)main + 12;
+#ifdef __cplusplus
 }
-
+#endif
