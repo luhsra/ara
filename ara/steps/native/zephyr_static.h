71db8199c722cd18f5946abdf44b5eaa3003fec6
Kenny Albes <KennyA@posteo.de>
2021-01-09 16:50:23 +0100

    zephyr_static: relaxed step dependencies

--- ara/steps/native/zephyr_static.h
+++ ara/steps/native/zephyr_static.h
@@ -17,6 +17,10 @@ namespace ara::step {
 		static std::string get_description();
 		static Step::OptionVec get_local_options() { return {}; }
 
+		virtual std::vector<std::string> get_single_dependencies() override {
+			return {"IRReader"};
+		}
+
 		virtual void run() override;
 	};
 } // namespace ara::step
