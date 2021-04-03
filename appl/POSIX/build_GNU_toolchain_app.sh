2b8852a32525b43c045bb56983d2aa47cf667ef2
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-04-03 20:14:34 +0200

    Added Debug flags to POSIX build system

--- appl/POSIX/build_GNU_toolchain_app.sh
+++ appl/POSIX/build_GNU_toolchain_app.sh
@@ -5,16 +5,17 @@
 #               --> install via "pip3 install wllvm"
 #
 # Usage: 
-#   build_GNU_toolchain_app.sh <project path> <binary file> <output file>
+#   build_GNU_toolchain_app.sh <project path> <binary file> <output file> <configure arguments (optional)>
 
-readonly PROJECT_PATH="$1"  # The path to the project source dir of the program to be build.
-readonly BINARY_FILE="$2"   # The path to the binary file which will be created by the projects Makefile.
-readonly OUTPUT_FILE="$3"   # This is the output file of this script.
+readonly PROJECT_PATH="$1"      # The path to the project source dir of the program to be build.
+readonly BINARY_FILE="$2"       # The path to the binary file which will be created by the projects Makefile.
+readonly OUTPUT_FILE="$3"       # This is the output file of this script.
+readonly CONFIGURE_ARGS="$4"    # Arguments to be applied to the ./configure script in ${PROJECT_PATH}. [optional]
 
 if [ "x${PROJECT_PATH}" = "x" ] || [ "x${BINARY_FILE}" = "x" ] || [ "x${OUTPUT_FILE}" = "x" ] ; then
     echo "Missing argument(s)!"
     echo "Usage:"
-    echo "   build_musl_lib_c_to_llvm_ll.sh <musl libc source code> <output file>" 
+    echo "   build_GNU_toolchain_app.sh <project path> <binary file> <output file> <configure arguments (optional)>" 
     exit
 fi
 
@@ -22,13 +23,13 @@ fi
 cd "$(dirname "$0")" || exit
 
 if ! [ -e "${PROJECT_PATH}"/configure ]; then
-    echo "The path: \"${PROJECT_PATH}\" is not a project source dir"
+    echo "The path: \"${PROJECT_PATH}\" is not a project source dir!"
     exit
 fi
 
 # Compile project with wllvm
 export LLVM_COMPILER=clang
-(cd "${PROJECT_PATH}" && CC=wllvm WLLVM_CONFIGURE_ONLY=1 ./configure)
+(cd "${PROJECT_PATH}" && CC=wllvm WLLVM_CONFIGURE_ONLY=1 ./configure ${CONFIGURE_ARGS})
 (cd "${PROJECT_PATH}" && make)
 
 # Generate .bc bitcode
