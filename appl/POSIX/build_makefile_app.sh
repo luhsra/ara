ea690ad0e9738e9d10896800579ba5acae0e1081
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-04-30 01:42:00 +0200

    Makefile speed up with -j4

--- appl/POSIX/build_makefile_app.sh
+++ appl/POSIX/build_makefile_app.sh
@@ -50,9 +50,9 @@ fi
 export LLVM_COMPILER=clang
 if [ "${EXEC_CONFIGURE}" = "true" ] ; then
     (cd "${PROJECT_PATH}" && CC=wllvm WLLVM_CONFIGURE_ONLY=1 ./configure ${CONFIGURE_ARGS})
-    (cd "${PROJECT_PATH}" && make)
+    (cd "${PROJECT_PATH}" && make -j4)
 else
-    (cd "${PROJECT_PATH}" && make CC=wllvm ${MAKE_ARGS})
+    (cd "${PROJECT_PATH}" && make -j4 CC=wllvm ${MAKE_ARGS})
 fi
 
 if [ "${EXEC_MAKE_INSTALL}" = "true" ] ; then
