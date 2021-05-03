e3c13d0e0f0f543257f4a838bdc6c8b009885125
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-05-03 21:30:34 +0200

    Implemented smart POSIX class that redirects __{syscall} -> {syscall} and generates auto stubs of a syscall_set.

--- appl/POSIX/_add_to_musl_libc/override_weak_aliases.c
+++ appl/POSIX/_add_to_musl_libc/override_weak_aliases.c
@@ -4,7 +4,7 @@
 #define PTHREAD_CREATE_ARGS \
     pthread_t *__restrict thread, const pthread_attr_t *__restrict attr, void *(*start_routine)(void *), void *__restrict arg
 
-int __pthread_create(PTHREAD_CREATE_ARGS);
-int pthread_create(PTHREAD_CREATE_ARGS) {
-    return __pthread_create(thread, attr, start_routine, arg);
-}
+//int __pthread_create(PTHREAD_CREATE_ARGS);
+//int pthread_create(PTHREAD_CREATE_ARGS) {
+//    return __pthread_create(thread, attr, start_routine, arg);
+//}
--- ara/os/posix/posix.py
+++ ara/os/posix/posix.py
@@ -5,6 +5,7 @@
 
 from ara.graph import SyscallCategory
 from ..os_base import OSBase
+from ..os_util import syscall
 from .posix_utils import debug_log, logger, handle_soc, add_normal_cfg
 from .file import FileSyscalls
 from .file_descriptor import FileDescriptorSyscalls
@@ -12,9 +13,103 @@ from .mutex import MutexSyscalls
 from .signal import SignalSyscalls
 from .thread import ThreadSyscalls
 from .other_syscalls import OtherSyscalls
+from .warning_syscalls import WarningSyscalls
+from .syscall_set import syscall_set
 
-class POSIX(OSBase, MutexSyscalls, FileSyscalls, FileDescriptorSyscalls,
-                    SignalSyscalls, ThreadSyscalls, OtherSyscalls):
+'''
+    Hold on! To understand this file you need some information.
+
+    The musl libc makes heavy use of weak aliases.
+    E.g. The implementation of pthread_create() is provided in the function __pthread_create().
+    pthread_create() is only a weak alias to __pthread_create().
+    ARA can only detect the original function (__pthread_create). 
+    And the musl libc also call internally syscall functions with "__" prefixed.
+    The __{syscall name} version is called "[musl libc] weak alias version" in this OS Model.
+
+    To detect the syscalls correctly we need to provide an implementation for these prefixed functions.
+    This is done by redirecting POSIX.__syscall -> POSIX.syscall. (See _POSIXMetaClass)
+
+    To add a syscall stub the only thing you need to do is adding the syscall name to the syscall_set. (See syscall_set.py)
+    
+    If you want to implement a new instance for the InstanceGraph create a new module in this package and make sure
+    that _POSIXSyscalls inherit from the new syscall class that contains the new syscalls.
+'''
+
+@syscall
+def syscall_stub(graph, abb, state, args, va):
+    """ An empty stub for all not implemented syscalls in the syscall_set. """
+    pass
+
+def get_weak_alias(syscall):
+    """ Returns the musl libc weak alias name version of the syscall name.
+
+        For example: "pthread_create" -> "__pthread_create"
+
+        For all names which start with a '_' there is no weak alias version.
+        In this case this function will return None.
+    """
+    return "__" + syscall if syscall[0] != '_' else None
+
+def is_weak_alias(syscall):
+    """ Returns True if the syscall name is a musl libc weak alias name. """
+    return len(syscall) > 2 and syscall[0:2] == "__" and syscall[2] != '_'
+
+
+class _POSIXSyscalls(MutexSyscalls, FileSyscalls, FileDescriptorSyscalls,
+                     SignalSyscalls, ThreadSyscalls, OtherSyscalls,
+                     WarningSyscalls):
+    """ This class combines all implemented syscall methods. """
+    pass
+
+class _POSIXMetaClass(type(_POSIXSyscalls)):
+    """ This is the MetaClass for the POSIX class. 
+        
+        The only purpose of this class is to provide the methods __dir__ and __getattr__ for the POSIX class.
+    """
+
+    def __dir__(cls):
+        """ Returns the union of all implemented syscall names and names in the syscall_set as list.
+
+            The musl libc weak alias versions of all names from above are also included in this list.
+        """
+        implented_syscalls = set(filter((lambda name : name[0:2] != "__" and name[-2:] != "__"), dir(_POSIXSyscalls)))
+        total_syscalls = implented_syscalls.union(syscall_set)
+        syscall_list = list()
+        for syscall in total_syscalls:
+            syscall_list.append(syscall)
+            weak_alias_version = get_weak_alias(syscall)
+            if weak_alias_version != None:
+                syscall_list.append(weak_alias_version)
+        # Uncomment this line if you need all functions from dir():
+        #syscall_list.extend(["get_special_steps", "has_dynamic_instances", "init", "interpret", "config", "get_name"])
+        return syscall_list
+
+    def __getattr__(cls, syscall):
+        """ This method provides all attributes that are not directly included in POSIX.
+
+            These are all stub syscalls and musl libc weak aliases.
+            e.g. __pthread_create() will be redirected to the implementation of pthread_create()
+            e.g. A non implemented Syscall in syscall_set will be redirected to syscall_stub()
+        """
+        if is_weak_alias(syscall):
+            orig_syscall = syscall[2:]
+            syscall_func = getattr(_POSIXSyscalls, orig_syscall, None)
+            if syscall_func != None:
+                return syscall_func
+            elif orig_syscall in syscall_set:
+                return syscall_stub
+            else:
+                raise AttributeError
+
+        if syscall in syscall_set:
+            return syscall_stub
+        else:
+            raise AttributeError
+
+
+class POSIX(OSBase, _POSIXSyscalls, metaclass=_POSIXMetaClass):
+
+    __metaclass__ = _POSIXMetaClass
 
     @staticmethod
     def get_special_steps():
