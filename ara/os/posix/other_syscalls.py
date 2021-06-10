d44597c3f4e41f156013b2aac9642d32cbec2b6c
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-06-11 01:29:14 +0200

    Implemented sleep()

--- ara/os/posix/other_syscalls.py
+++ ara/os/posix/other_syscalls.py
@@ -1,11 +1,17 @@
-from ara.graph import SyscallCategory
+from ara.graph import SyscallCategory, SigType
 
-from ..os_util import syscall
+from ..os_util import syscall, Arg
 from .posix_utils import logger, do_not_interpret_syscall, add_self_edge
 
 class OtherSyscalls:
 
+
     @syscall(categories={SyscallCategory.comm})
     def pause(graph, abb, state, args, va):
-        logger.debug("found pause() syscall")
         return add_self_edge(state, "pause()")
+
+    # unsigned sleep(unsigned seconds);
+    @syscall(categories={SyscallCategory.comm},
+             signature=(Arg('seconds', hint=SigType.value),))
+    def sleep(graph, abb, state, args, va):
+        return add_self_edge(state, f"sleep({args.seconds})")
