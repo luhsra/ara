c9f702bd045cfebe7b43d1af9fc3545ef0852c63
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-06-28 19:15:42 +0200

    Removed libmicrohttpd demo.c application from auto build and disabled libmicrohttpd postprocessor. We only want to analyze fileserver dirs demo appl for libmicrohttpd.

--- appl/POSIX/project_makefiles/libmicrohttpd.mk
+++ appl/POSIX/project_makefiles/libmicrohttpd.mk
@@ -23,7 +23,7 @@ $(BUILD_DIR)/libmicrohttpd.ll: build_makefile_app.sh $(BUILD_DIR)/musl_libc.ll $
 	BINARY_FILE="$(LIBMICROHTTPD_DIR)/src/microhttpd/.libs/libmicrohttpd.a" \
 	OUTPUT_FILE="$@" \
 	EXEC_CONFIGURE="$(MHD_REBUILD_MAKEFILE)" \
-	CONFIGURE_ARGS="--disable-nls --enable-https=no --without-gnutls --with-threads=posix --disable-curl --disable-largefile --disable-messages --disable-dauth --disable-httpupgrade --disable-epoll --disable-poll --enable-itc=pipe" \
+	CONFIGURE_ARGS="--disable-nls --enable-https=no --without-gnutls --with-threads=posix --disable-curl --disable-largefile --disable-messages --disable-dauth --disable-httpupgrade --disable-epoll --disable-poll --enable-itc=pipe --disable-postprocessor" \
 	$(USE_MUSL_CLANG) \
 	CFLAGS="$(CFLAGS_NO_MUSL_INCL)" \
 		./build_makefile_app.sh
