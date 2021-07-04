8eb4e2c5abb644ae804919c6133e86427f33f493
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-07-04 14:58:18 +0200

    Updated posix_rebuild_libmicrohttpd_llvm_ir.sh to cover new libmicrohttpd demo application.

--- tools/posix_rebuild_libmicrohttpd_llvm_ir.sh
+++ tools/posix_rebuild_libmicrohttpd_llvm_ir.sh
@@ -6,10 +6,14 @@
 cd "$(dirname "$0")" || exit 1
 
 rm ../build/appl/POSIX/microhttpd_demo.ll
+rm ../build/appl/POSIX/microhttpd_fileserver_dirs.ll
 rm ../build/appl/POSIX/libmicrohttpd.ll
 rm ../build/appl/POSIX/objs/microhttpd_demo.ll
 rm ../build/appl/POSIX/objs/microhttpd_demo_without_lib.ll
+rm ../build/appl/POSIX/objs/microhttpd_fileserver_dirs.ll
+rm ../build/appl/POSIX/objs/microhttpd_fileserver_dirs_without_lib.ll
 rm ../build/appl/POSIX/bin/microhttpd_demo
+rm ../build/appl/POSIX/bin/microhttpd_fileserver_dirs
 
 #(cd ../appl/POSIX && make clean-libmicrohttpd)
 (cd ../appl/POSIX && make -j$(nproc))
