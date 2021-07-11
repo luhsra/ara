58b5b9d1ea76ba526d654f2539be7e0075b7a909
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-07-11 19:56:10 +0200

    Renamed project_makefiles -> modules and unit_tests -> test_appl

--- appl/POSIX/modules/no_musl_linkage_test_appl.mk
+++ appl/POSIX/modules/no_musl_linkage_test_appl.mk
@@ -1,7 +1,7 @@
 
 # This file handles unit tests that should not be linked with the musl libc
 
-NO_MUSL_LINKAGE_DIR = unit_tests/no_musl_linkage
+NO_MUSL_LINKAGE_DIR = $(TEST_C_FILES)/no_musl_linkage
 
 NO_MUSL_LINKAGE_NAMES = $(call list_filenames_of_dir,.c,$(NO_MUSL_LINKAGE_DIR))
 NO_MUSL_LINKAGE_TARGETS = $(call do_for_all,$(NO_MUSL_LINKAGE_NAMES),$(AWK_TO_OBJS_TARGET))
