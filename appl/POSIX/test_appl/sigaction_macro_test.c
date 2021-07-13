16c689195bb68c5961bf4a007d988c9315f1a9fa
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-07-13 23:55:57 +0200

    Added sigaction_macro_test.

---
+++ appl/POSIX/test_appl/sigaction_macro_test.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+void signal_catching_func(int sig) {
+    pause();
+    return;
+}
+
+int test_sigaction(int a, int b, int c) {
+    return 0;
+}
+
+struct sigaction act;
+struct sigaction old_act;
+
+int main() {
+    act.sa_handler = signal_catching_func;
+    if (sigaction(SIGALRM, &act, NULL) != 0) {
+        perror("sigaction(act)");
+    }
+    if (sigaction(SIGALRM, NULL, &old_act) != 0) {
+        perror("sigaction(old_act)");
+    }
+    // The following should not be detected:
+    int (*sigaction_ptr)(int, const struct sigaction *, struct sigaction *) = sigaction;
+    sigaction_ptr(SIGSEGV, &act, NULL);
+}
