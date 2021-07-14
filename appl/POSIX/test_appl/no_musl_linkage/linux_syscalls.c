c3ec155f750f8dbafd3dd48a2d5634909fc204d3
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-07-14 21:47:11 +0200

    Updated linux_syscalls test for the new Pipe implementation.

--- appl/POSIX/test_appl/no_musl_linkage/linux_syscalls.c
+++ appl/POSIX/test_appl/no_musl_linkage/linux_syscalls.c
@@ -2,18 +2,23 @@
 #include <fcntl.h>
 #include <sys/uio.h>
 #include <string.h>
+#include <time.h>
 
 #define SYS_READ 0
 #define SYS_WRITE 1
 #define SYS_OPEN 2
 #define SYS_WRITEV 20
-#define SYS_PIPE 22
+#define SYS_PAUSE 34
 
 long __syscall3(long n, long a1, long a2, long a3) {
     return 0;
 }
 
-long __syscall1(long n, long a1) {
+long __syscall2(long n, long a1, long a2) {
+    return 0;
+}
+
+long __syscall0(long n) {
     return 0;
 }
 
@@ -21,10 +26,8 @@ int main() {
     int fd = __syscall3(SYS_OPEN, (long)"testdir/testfile.txt", O_RDWR, (long)NULL);
     char input[25];
     __syscall3(SYS_READ, (long)fd, (long)input, sizeof(25));
-    int fildes[2];
-    __syscall1(SYS_PIPE, (long)fildes);
-    char* str = "write this to the pipe";
-    __syscall3(SYS_WRITE, (long)fildes[1], (long)str, strlen(str) + 1);
+    char* str = "write this to the file";
+    __syscall3(SYS_WRITE, (long)fd, (long)str, strlen(str) + 1);
     char* msg_buf_1 = "Test data to write";
     char* msg_buf_2 = " to the file.";
     struct iovec iov[2];
@@ -33,4 +36,6 @@ int main() {
     iov[1].iov_base = msg_buf_2;
     iov[1].iov_len = strlen(msg_buf_2);
     __syscall3(SYS_WRITEV, (long)fd, (long)iov, 2);
+
+    __syscall0(SYS_PAUSE);
 }
