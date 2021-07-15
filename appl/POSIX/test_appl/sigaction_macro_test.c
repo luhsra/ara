60299193e78881e8d8e5d1fcd0b97b15153d6413
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-07-15 17:40:15 +0200

    Fixed bug in test_appl/sigaction_macro_test.c

--- appl/POSIX/test_appl/sigaction_macro_test.c
+++ appl/POSIX/test_appl/sigaction_macro_test.c
@@ -20,9 +20,11 @@ int main() {
     act.sa_handler = signal_catching_func;
     if (sigaction(SIGALRM, &act, NULL) != 0) {
         perror("sigaction(act)");
+        return -1;
     }
     if (sigaction(SIGALRM, NULL, &old_act) != 0) {
         perror("sigaction(old_act)");
+        return -2;
     }
     // The following should not be detected:
     int (*sigaction_ptr)(int, const struct sigaction *, struct sigaction *) = sigaction;
