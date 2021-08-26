de6cea14e1253727a046c78fd008e83831f595a4
Gerion Entrup <entrup@sra.uni-hannover.de>
2021-08-26 15:34:39 +0200

    switch to new SIA

--- ara/steps/__init__.py
+++ ara/steps/__init__.py
@@ -13,8 +13,8 @@ def provide_steps():
     from .manual_corrections import ManualCorrections
     from .printer import Printer
     from .recursive_functions import RecursiveFunctions
-    # from .sia import SIA, InteractionAnalysis
-    from .sse import SIA, InteractionAnalysis, MultiSSE
+    from .sia import SIA, InteractionAnalysis
+    from .sse import MultiSSE
     from .syscall import Syscall
     from .sysfuncts import SysFuncts
     from .system_relevant_functions import SystemRelevantFunctions
