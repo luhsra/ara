963440ff8a5520415409754b2d7d11b63ace74f2
Gerion Entrup <entrup@sra.uni-hannover.de>
2021-08-26 16:41:22 +0200

    statistics: add workaround for -fstack-check functions
    With an ARM toolchain that is built with "-fstack-check" the linker
    requires some stack checking functions that we don't provide.
    
    Therefore, this commit provides some dummy functions for that.

--- libs/sra/generic/statistics.cc
+++ libs/sra/generic/statistics.cc
@@ -4,6 +4,10 @@
 extern __time_marker_t __start_time_markers;
 extern __time_marker_t __end_time_markers;
 
+// fix compilation with -fstack-check without extra linking
+extern "C" __attribute__((weak)) void __stack_chk_fail(void){};
+extern "C" __attribute__((weak)) void __stack_chk_guard(void){};
+
 extern "C" void print_startup_statistics(void) {
   __time_marker_t * start = &__start_time_markers;
   kout.init();
