0e0aec8db072261e2d31a9d876178da459f28d8d
Gerion Entrup <entrup@sra.uni-hannover.de>
2021-10-05 01:46:48 +0200

    graph: add single_check

--- ara/graph/__init__.py
+++ ara/graph/__init__.py
@@ -1,7 +1,7 @@
-from .graph import Graph, CFG, CFGView, InstanceGraph
+from .graph import Graph, CFG, CFGView, InstanceGraph, single_check
 from .mix import ABBType, CFType, SyscallCategory, SigType, NodeLevel
 from .graph_data import Argument, Arguments, CallPath
 
 __all__ = ["Graph", "CFGView", "InstanceGraph", "CFG",
            "ABBType", "CFType", "SyscallCategory", "SigType", "NodeLevel",
-           "Argument", "Arguments", "CallPath"]
+           "Argument", "Arguments", "CallPath", "single_check"]
