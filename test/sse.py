a4646860cc6b20d254bd2a752657f232e7aa5b49
Gerion Entrup <entrup@sra.uni-hannover.de>
2021-10-05 02:02:53 +0200

    SSE: explicit CFG dumping
    Remove that once the test is ready.

--- test/sse.py
+++ test/sse.py
@@ -6,7 +6,15 @@ from init_test import init_test, fail_if
 
 def main():
     """Test for correct SSE execution."""
-    config = {"steps": ["SSE"]}
+    config = {"steps": ["LoadOIL",
+                        {
+                            "name": "Printer",
+                            "dot": "cfg.dot",
+                            "from_entry_point": False,
+                            "graph_name": "CFG",
+                            "subgraph": "abbs"
+                        },
+                        "SSE"]}
     inp = {"oilfile": lambda argv: argv[3]}
     m_graph, data, log, _ = init_test(extra_config=config, extra_input=inp)
 
