ba2c2f192ea317c945ade109359adfb14fe04cc4
Gerion Entrup <entrup@sra.uni-hannover.de>
2021-10-14 18:14:05 +0200

    Graph: export CFG, too

--- ara/graph/__init__.py
+++ ara/graph/__init__.py
@@ -1,7 +1,7 @@
-from .graph import Graph, CFGView, InstanceGraph
+from .graph import Graph, CFG, CFGView, InstanceGraph
 from .mix import ABBType, CFType, SyscallCategory, SigType, NodeLevel
 from .graph_data import Argument, Arguments, CallPath
 
-__all__ = ["Graph", "CFGView", "InstanceGraph",
+__all__ = ["Graph", "CFGView", "InstanceGraph", "CFG",
            "ABBType", "CFType", "SyscallCategory", "SigType", "NodeLevel",
            "Argument", "Arguments", "CallPath"]
