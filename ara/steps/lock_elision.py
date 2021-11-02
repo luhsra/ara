c6ee621d5d28ffb74a524d6e65746ab220a2bed0
Gerion Entrup <entrup@sra.uni-hannover.de>
2021-11-02 22:22:23 +0100

    lock-elision: implement step and add more tests

--- ara/steps/lock_elision.py
+++ ara/steps/lock_elision.py
@@ -1,15 +1,36 @@
 """Container for Dummy."""
-from ara.graph import Graph
+from ara.os.autosar import Spinlock
 from .step import Step
-from .option import Option, Integer
 
 
 class LockElision(Step):
     """Detect lock usages within the SSTG that are not necessary."""
 
+    # Operating system specific API to get locks
+    # This is a list of tuples. One tuple is OS specific.
+    # Is has the following structure:
+    # (Class of lock, Function which returns the name,
+    #  Function which specifies that the lock is spinning)
+    LOCKS = [(Spinlock, lambda x: x.name, lambda x: x.is_spinning)]
+
     def get_single_dependencies(self):
         return ["MultiSSE"]
 
     def run(self):
-        sstg = self._graph.reduced_sstg
-        self._log.info("sstg")
+        mstg = self._graph.mstg
+        lock_count = {}
+        for ms in mstg.get_metastates().vertices():
+            for state_vert in mstg.vertex(ms).out_neighbors():
+                state = mstg.vp.state[state_vert]
+                for lock_type, get_name, get_status in LockElision.LOCKS:
+                    for inst, obj in state.instances.get(lock_type):
+                        name = get_name(obj)
+                        if name not in lock_count:
+                            lock_count[name] = 0
+                        if obj in state.context and get_status(state.context[obj]):
+                            lock_count[get_name(obj)] += 1
+
+        for lock, amount in lock_count.items():
+            self._log.warn(f"Lock {lock} spins {amount} times")
+
+        self._set_step_data(lock_count)
