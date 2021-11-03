5a782cffba211ccff3fc57adb65ac53e2e7167d0
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2021-11-03 19:48:26 +0100

    tests/value_analysis.py: Test case is working now. Creation syscalls must be handled before interaction syscalls

--- test/value_analysis.py
+++ test/value_analysis.py
@@ -60,9 +60,69 @@ def create_callpath(graph, cp_desc):
             raise RuntimeError(f"Callsite not found {cp_desc}")
     return cp
 
+def perform_va_for_syscall(va, m_graph, syscalls, data, name, syscall):
+    line = syscalls.vp.line[syscall]
+    debug_print(20 * "-")
+    debug_print(f"Handle syscall {name} (line {line}).")
+    record = data[name][str(line)]
+    for entry in record:
+        callpath = create_callpath(m_graph, entry["callpath"])
+        for idx, [hint, val] in enumerate(entry["args"]):
+            debug_print(f"Searching arg {idx}, expected: {val}, "
+                        f"cp: {callpath}")
+
+            expected_error = None
+            no_except = True
+            if isinstance(val, str) and val.startswith('Error:'):
+                expected_error = val.split(' ')[1]
+
+            try:
+                result = va.get_argument_value(syscall, idx,
+                                                callpath=callpath,
+                                                hint=get_hint(hint))
+            except Exception as e:
+                if type(e).__name__ == expected_error:
+                    debug_print(f"Got expected error: {type(e).__name__}")
+                    no_except = False
+                else:
+                    raise e
+            if no_except:
+                py_val = value_to_py(result.value, result.attrs)
+                debug_print("Retrieved", py_val)
+                if result.offset:
+                    num_offsets = [x.get_offset() for x in result.offset]
+                    debug_print(f"With Offsets: "
+                                f"{num_offsets} ({result.offset})")
+                    assert val == [py_val, num_offsets]
+                else:
+                    assert val == py_val, f"Expected {val}, got {py_val}"
+                if "is_creation" in entry:
+                    obj = entry["is_creation"]
+                    if "arg" in obj and obj['arg'] == idx:
+                        debug_print(f"Assign {obj['name']} "
+                                    f"to argument {idx}.")
+                        va.assign_system_object(result.value,
+                                                obj['name'],
+                                                result.offset)
+        if "is_creation" in entry:
+            obj = entry["is_creation"]
+            debug_print(f"Assign {obj['name']}.")
+            if "arg" not in obj:
+                store = va.get_return_value(syscall, callpath)
+                debug_print(f"Got store: {store}")
+                result = va.get_memory_value(store, callpath)
+                debug_print(f"Got store: {store}, value: {result.value}, "
+                            f"offset: {result.offset}")
+                va.assign_system_object(result.value,
+                                        obj["name"],
+                                        result.offset)
 
 def main():
     """Test for correct value analysis recognition."""
+
+    # All creation syscalls that we are testing.
+    creation_syscalls = set({"xQueueCreateMutex", "xTaskCreateStatic", "xTaskCreate"})
+
     config = {"steps": ["Syscall"] + ValueAnalyzer.get_dependencies()}
     m_graph, data, log, _ = init_test(extra_config=config)
     global logger
@@ -73,64 +133,22 @@ def main():
 
     syscalls = CFGView(cfg, vfilt=cfg.vp.type.fa == ABBType.syscall)
 
+    # Search for syscalls in cfg and order them by type
+    found_creation_syscalls = []
+    found_interaction_syscalls = []
     for syscall in syscalls.vertices():
-        name = syscalls.get_syscall_name(syscall)
-        line = syscalls.vp.line[syscall]
-        debug_print(20 * "-")
-        debug_print(f"Handle syscall {name} (line {line}).")
-        record = data[name][str(line)]
-        for entry in record:
-            callpath = create_callpath(m_graph, entry["callpath"])
-            for idx, [hint, val] in enumerate(entry["args"]):
-                debug_print(f"Searching arg {idx}, expected: {val}, "
-                            f"cp: {callpath}")
+        name = cfg.get_syscall_name(syscall)
+        if name in creation_syscalls:
+            found_creation_syscalls.append((name, syscall))
+        else:
+            found_interaction_syscalls.append((name, syscall))
 
-                expected_error = None
-                no_except = True
-                if isinstance(val, str) and val.startswith('Error:'):
-                    expected_error = val.split(' ')[1]
+    # Creation syscalls must be handled before interaction syscalls.
+    for syscall in found_creation_syscalls:
+        perform_va_for_syscall(va, m_graph, syscalls, data, syscall[0], syscall[1])
 
-                try:
-                    result = va.get_argument_value(syscall, idx,
-                                                   callpath=callpath,
-                                                   hint=get_hint(hint))
-                except Exception as e:
-                    if type(e).__name__ == expected_error:
-                        debug_print(f"Got expected error: {type(e).__name__}")
-                        no_except = False
-                    else:
-                        raise e
-                if no_except:
-                    py_val = value_to_py(result.value, result.attrs)
-                    debug_print("Retrieved", py_val)
-                    if result.offset:
-                        num_offsets = [x.get_offset() for x in result.offset]
-                        debug_print(f"With Offsets: "
-                                    f"{num_offsets} ({result.offset})")
-                        assert val == [py_val, num_offsets]
-                    else:
-                        assert val == py_val
-                    if "is_creation" in entry:
-                        obj = entry["is_creation"]
-                        if "arg" in obj and obj['arg'] == idx:
-                            debug_print(f"Assign {obj['name']} "
-                                        f"to argument {idx}.")
-                            va.assign_system_object(result.value,
-                                                    obj['name'],
-                                                    result.offset)
-            if "is_creation" in entry:
-                obj = entry["is_creation"]
-                debug_print(f"Assign {obj['name']}.")
-                if "arg" not in obj:
-                    store = va.get_return_value(syscall, callpath)
-                    debug_print(f"Got store: {store}")
-                    result = va.get_memory_value(store, callpath)
-                    debug_print(f"Got store: {store}, value: {result.value}, "
-                                f"offset: {result.offset}")
-                    va.assign_system_object(result.value,
-                                            obj["name"],
-                                            result.offset)
-
+    for syscall in found_interaction_syscalls:
+        perform_va_for_syscall(va, m_graph, syscalls, data, syscall[0], syscall[1])
 
 if __name__ == '__main__':
     main()
