9339db63f735d73a80d1b45da54bcdb3946cf2c8
Björn Fiedler <fiedler@sra.uni-hannover.de>
2022-01-20 15:55:06 +0100

    appl: paper/timing += spinlock

--- appl/AUTOSAR/multicore/paper/timing.cc
+++ appl/AUTOSAR/multicore/paper/timing.cc
@@ -18,6 +18,7 @@ void fetch_sensor_a();
 void fetch_sensor_b();
 void compute_with_a();
 void compute_with_b();
+void update_status(int);
 
 
 #if LOCKS_JSON
@@ -30,12 +31,18 @@ DeclareTask(T1); // autostart
 DeclareTask(T2);
 DeclareTask(T3);
 
+// CPU2
+DeclareTask(T4);
+
 DeclareSpinlock(S1);
-DeclareSpinlock(S2);
 
 TEST_MAKE_OS_MAIN( StartOS(0) )
 
 TASK(T1) {
+	GetSpinlock(S1);
+	update_status(1);
+	ReleaseSpinlock(S1);
+	ActivateTask(T4);
 	fetch_sensor_a();
 	ActivateTask(T3);
 	fetch_sensor_b();
@@ -50,6 +57,16 @@ TASK(T3) {
 
 TASK(T2) {
 	compute_with_b();
+	GetSpinlock(S1);
+	update_status(2);
+	ReleaseSpinlock(S1);
 	TerminateTask();
 }
 
+
+TASK(T4) {
+	GetSpinlock(S1);
+	update_status(4);
+	ReleaseSpinlock(S1);
+	TerminateTask();
+}
