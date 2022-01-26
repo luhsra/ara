0967339147b2bc364c20b277cbeacc2d8b726bcd
Björn Fiedler <fiedler@sra.uni-hannover.de>
2022-01-26 17:14:45 +0100

    appl: interrupts as tasks

---
+++ appl/AUTOSAR/multicore/paper/interrupt_as_tasks.cc
@@ -0,0 +1,61 @@
+/**
+ * @defgroup apps Applications
+ * @brief The applications...
+ */
+
+/**
+ * @file
+ * @ingroup apps
+ * @brief Just a simple test application
+ */
+#include "autosar/os.h"
+#include "test/test.h"
+#include "machine.h"
+
+typedef struct {int a;} result;
+
+void fetch_sensor_a();
+void fetch_sensor_b();
+void compute_with_a();
+void compute_with_b();
+void update_status(int);
+extern "C" void ara_timing_info(int, int);
+
+
+#if LOCKS_JSON
+{"S1": 4, "S2": 0, "timed_locks": {"S1": 2, "S2": 0}}
+#endif //LOCKS_JSON
+
+// CPU 0
+DeclareTask(T1); // autostart
+// CPU 1
+DeclareTask(T2);
+DeclareTask(T3);
+
+// CPU2
+DeclareTask(T4);
+
+DeclareSpinlock(S1);
+
+TEST_MAKE_OS_MAIN( StartOS(0) )
+
+TASK(T1) {
+	ara_timing_info(200, 300);
+	ActivateTask(T2);
+	ChainTask(T1);
+}
+
+TASK(T2) {
+	ara_timing_info(10, 20);
+	compute_with_a();
+	TerminateTask();
+}
+
+TASK(T3) {
+	TerminateTask();
+}
+
+
+TASK(T4) {
+	TerminateTask();
+}
