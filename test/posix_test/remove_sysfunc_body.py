14b01379667d71ed49ffcb51b998dba11e8a3749
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2022-03-01 20:39:34 +0100

    POSIX: remove debug prints in ../test/posix_test/remove_sysfunc_body.py

--- test/posix_test/remove_sysfunc_body.py
+++ test/posix_test/remove_sysfunc_body.py
@@ -3,16 +3,12 @@
 if __name__ == '__main__':
     __package__ = 'test.posix_test'
 
-from ..init_test import fail_if_json_not_equal, init_test, fail_if
+from ..init_test import fail_if_json_not_equal, init_test
 from ..common_json_graphs import json_callgraph
 import sys
 
-def get_func_name(callgraph, cfg, node):
-    return cfg.vp.name[callgraph.vp.function[node]]
-
 def test_remove_sysfunc_step(drop_llvm_suffix: bool, os_name: str):
     """Test the RemoveSysfuncBody step."""
-    print(drop_llvm_suffix)
     config = {"steps": ["IRReader", "CallGraph"],
                 "IRReader": {
                     "no_sysfunc_body": True
@@ -24,9 +20,7 @@ def test_remove_sysfunc_step(drop_llvm_suffix: bool, os_name: str):
     graph, data, log, _ = init_test(extra_config=config, os_name=os_name)
     call_edges = json_callgraph(graph.callgraph)
 
-    #print(call_edges)
     fail_if_json_not_equal(data, call_edges)
-    fail_if(data != call_edges, f"Data not equal for os_name={os_name} and drop_llvm_suffix={drop_llvm_suffix}")
 
 def str_to_bool(string: str):
     return string.lower() in ["true", "1", "y", "yes"]
