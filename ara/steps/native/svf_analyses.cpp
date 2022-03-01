3eaa2f7afb9bbfb48c8999abd60e191c6178d4a8
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2022-03-01 21:36:13 +0100

    SVFAnalyses: Set option -enable-tcg=false to disable SVFs ThreadCallGraph

--- ara/steps/native/svf_analyses.cpp
+++ ara/steps/native/svf_analyses.cpp
@@ -15,6 +15,10 @@ namespace ara::step {
 	std::string SVFAnalyses::get_description() { return "Run SVF analyses."; }
 
 	void SVFAnalyses::run() {
+		// Disable SVF ThreadCallGraph because this option will not work with POSIX os model.
+		const char* command_line[] = {"ara", "-enable-tcg=false"};
+		llvm::cl::ParseCommandLineOptions(2, command_line);
+
 		logger.info() << "Building SVF graphs." << std::endl;
 		SVFModule* svfModule = LLVMModuleSet::getLLVMModuleSet()->buildSVFModule(graph.get_module());
 		assert(svfModule != nullptr && "SVF Module is null");
