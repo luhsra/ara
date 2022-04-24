45d56a47c98da804f1452cfdd4c7ea9aa0369a0c
Gerion Entrup <entrup@sra.uni-hannover.de>
2022-04-25 00:41:44 +0200

    CallGraphStats: add call path depth calculation

--- ara/steps/callgraph_stats.py
+++ ara/steps/callgraph_stats.py
@@ -1,7 +1,8 @@
 """Container for CFGStats."""
 from ara.graph import ABBType, CFGView, CFType, Graph, SyscallCategory
 from .step import Step
-from graph_tool.topology import label_components
+from graph_tool.topology import label_components, min_spanning_tree, pseudo_diameter
+from graph_tool import GraphView
 
 import graph_tool
 import json
@@ -21,11 +22,22 @@ class CallGraphStats(Step):
         num_functions = callgraph.num_vertices()
         num_callees = callgraph.num_edges()
 
+        # maximal call depth
+        max_cd = 0
+        for syscall in callgraph.get_syscalls().vertices():
+            rev = GraphView(callgraph, reversed=True)
+            mst = GraphView(
+                rev, efilt=min_spanning_tree(rev, root=rev.vertex(syscall)))
+            dist, ends = pseudo_diameter(mst, source=mst.vertex(syscall))
+            max_cd = max([max_cd, int(dist) - 1])
+
         self._log.info(f"Number of function: {num_functions}")
         self._log.info(f"Number of callees: {num_callees}")
+        self._log.info(f"Maximum call path depth: {max_cd}")
 
         if self.dump.get():
             with open(self.dump_prefix.get() + '.json', 'w') as f:
                 values = {"num_functions": num_functions,
-                          "num_callees": num_callees}
+                          "num_callees": num_callees,
+                          "max_path_depth": max_cd}
                 json.dump(values, f, indent=4)
