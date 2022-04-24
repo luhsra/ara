dc34e87a1f32d0a50cd62358516c16b20dc234d1
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2022-04-24 03:26:56 +0200

    InstanceGraphStats: os and step_settings option for test case

--- test/instance_graph_stats.py
+++ test/instance_graph_stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import json
+import sys
 import os.path
 
 # Note: init_test must be imported first
@@ -7,9 +8,17 @@ from init_test import fail_if_json_not_equal, init_test
 
 
 def main():
-    """Test for correct instance detection."""
+    """Test for correct instance detection.
+    
+    arguments: instance_graph_stats.py <expected json stats> <LLVM IR file> <OS>(optional) <step setting file>(optional)
+    """
+    os_name = sys.argv[3] if len(sys.argv) > 3 and sys.argv[3] != '-' else None
     config = {"steps": ["InstanceGraphStats"], "InstanceGraphStats": {"dump": True}}
-    m_graph, expected, log, _ = init_test(extra_config=config)
+    if len(sys.argv) > 4:     
+        setting_file = sys.argv[4]
+        with open(setting_file, "r") as f:
+            config = json.load(f)
+    m_graph, expected, log, _ = init_test(extra_config=config, os_name=os_name)
 
     with open("dumps/InstanceGraphStats.json", "r") as f:
         actual = json.load(f)
