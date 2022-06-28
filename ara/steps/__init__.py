5c375dde2b7dc1cfed54c3b14db28d6b55e1862c
Gerion Entrup <entrup@sra.uni-hannover.de>
2022-06-28 18:11:49 +0200

    ara: init, resort

--- ara/steps/__init__.py
+++ ara/steps/__init__.py
@@ -3,12 +3,13 @@ import ara.steps.py_logging
 
 def provide_steps():
     from .step import provide_steps as _native_provide
+
     from .apply_timings import ApplyTimings
     from .callgraph_stats import CallGraphStats
-    from .classify_specializations_freertos import ClassifySpecializationsFreeRTOS
-    from .create_abbs import CreateABBs
     from .cfg_optimize import CFGOptimize
     from .cfg_stats import CFGStats
+    from .classify_specializations_freertos import ClassifySpecializationsFreeRTOS
+    from .create_abbs import CreateABBs
     from .dummy import Dummy
     from .dump_cfg import DumpCFG
     from .dump_instances import DumpInstances
@@ -16,23 +17,23 @@ def provide_steps():
     from .fake_timings import FakeTimings
     from .generator import Generator
     from .icfg import ICFG
+    from .instance_graph_stats import InstanceGraphStats
     from .ipi_avoidance import IPIAvoidance
     from .load_oil import LoadOIL
     from .lock_elision import LockElision
     from .manual_corrections import ManualCorrections
+    from .mark_loop_head import MarkLoopHead
+    from .multisse import MultiSSE
     from .printer import Printer
     from .recursive_functions import RecursiveFunctions
     from .reduce_sstg import ReduceSSTG
     from .register_task_entry import RegisterTaskEntry
     from .sia import SIA, InteractionAnalysis
-    from .mark_loop_head import MarkLoopHead
-    from .multisse import MultiSSE
     from .sse import SSE
     from .sstg_stats import SSTGStats
     from .syscall import Syscall
     from .sysfuncts import SysFuncts
     from .system_relevant_functions import SystemRelevantFunctions
-    from .instance_graph_stats import InstanceGraphStats
 
     for step in _native_provide():
         yield step
@@ -50,8 +51,9 @@ def provide_steps():
     yield FakeTimings
     yield Generator
     yield ICFG
-    yield InteractionAnalysis
     yield IPIAvoidance
+    yield InstanceGraphStats
+    yield InteractionAnalysis
     yield LoadOIL
     yield LockElision
     yield ManualCorrections
@@ -67,7 +69,6 @@ def provide_steps():
     yield SysFuncts
     yield Syscall
     yield SystemRelevantFunctions
-    yield InstanceGraphStats
 
 
 def get_native_component(name: str):
