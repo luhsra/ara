d161eac0486511068e131aa1eb5e5ef267e74e7b
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2022-07-28 02:00:50 +0200

    visualization: add node highlight support of entity_on_node() in Tracer API

--- ara/visualization/trace/tracer_api/tracer.py
+++ ara/visualization/trace/tracer_api/tracer.py
@@ -1,6 +1,8 @@
 
+from ara.visualization.trace import trace_lib
+from graph_tool.libgraph_tool_core import Vertex
 from datetime import datetime
-from ara.visualization.trace.trace_components import BaseTraceElement
+from ara.visualization.trace.trace_components import BaseTraceElement, CFGNodeHighlightTraceElement, CallgraphNodeHighlightTraceElement, NodeHighlightTraceElement, ResetPartialChangesTraceElement
 from dataclasses import dataclass
 from pathlib import Path
 from typing import List
@@ -8,19 +10,22 @@ from ara.graph.graph import CFG, SVFG, Callgraph, InstanceGraph
 
 from ara.visualization.trace.trace_type import AlgorithmTrace
 from ara.steps.step import Step
+from ara.visualization.util import GraphTypes
 
 
 @dataclass
 class Entity:
     #id: int
-	name: str
+    name: str
+    color: trace_lib.Color
+    undo_previous_elems: ResetPartialChangesTraceElement    # Reset operation to undo all changes made by the entity on its last trace
 
 @dataclass
 class GraphNode:
-    node_id: int
-    graph: type
+    node: Vertex    # Can also be function name string but only for Callgraph
+    graph: GraphTypes
     def __str__(self):
-        return f"node {self.node_id} in {self.graph.__name__}"
+        return f"node {self.node} in {self.graph.value}"
 
 class Path:
     pass
@@ -44,14 +49,45 @@ class Tracer:
             output_str += self._format_trace_output(ent, msg + str(node))
         return output_str
 
+    def _highlight_nodes(self, nodes: List[GraphNode], color=trace_lib.Color.RED) -> List[BaseTraceElement]:
+        highlight_node_elems = []
+        for node in nodes:
+            if node.graph == GraphTypes.CALLGRAPH:
+                highlight_node_elems.append(CallgraphNodeHighlightTraceElement(node.node,
+                                                                               self.low_level_trace.callgraph,
+                                                                               color))
+            elif node.graph == GraphTypes.ABB:
+                highlight_node_elems.append(CFGNodeHighlightTraceElement(node.node,
+                                                                         self.low_level_trace.cfg, 
+                                                                         self.low_level_trace.callgraph,
+                                                                         color))
+            else:
+                highlight_node_elems.append(NodeHighlightTraceElement(node.node, node.graph, color))
+        return highlight_node_elems
+
+    def _handle_entity_reset(self, ent: Entity, actions: List[BaseTraceElement]):
+        """Fill actions with undo operation of all changes from last trace of the entity.
+        
+        Also updates the undo_previous_elems field with the changes in actions
+        """
+        undo_previous_elems = ent.undo_previous_elems
+        ent.undo_previous_elems = ResetPartialChangesTraceElement(actions.copy())
+        if undo_previous_elems != None:
+            # undo previous change:
+            actions.insert(0, undo_previous_elems)
+
+
     def get_entity(self, name: str):
         """Creates a new entity on this trace"""
-        return Entity(name)
+        # TODO: choose multiple colors automatically
+        return Entity(name, trace_lib.Color.GREEN, None)
 
     def entity_on_node(self, ent: Entity, nodes: List[GraphNode]):
-        if type(nodes) != List:
+        if isinstance(nodes, GraphNode):
             nodes = [nodes]
-        self.low_level_trace.add_element(None, self._format_node_output(ent, nodes, "is on "))
+        actions = self._highlight_nodes(nodes, ent.color)
+        self._handle_entity_reset(ent, actions)
+        self.low_level_trace.add_element(actions, self._format_node_output(ent, nodes, "is on "))
 
     def entity_is_looking_at(self, ent: Entity, paths: List[Path]):
         pass
@@ -59,7 +95,7 @@ class Tracer:
     def get_subtrace(self, name: str):
         return Tracer(self.trace_name + ":" + name, None, None, None, None, low_level_trace=self.low_level_trace)
 
-    def add_element(self, element:BaseTraceElement, log_message:str=None):
+    def add_element(self, element: List[BaseTraceElement], log_message:str=None):
         """Direct interface to low level trace API.
         
         Just calls self.low_level_trace.add_element().
