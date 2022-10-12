9df07fa0b1ddd7bae0ac680a57028b61177978c6
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2022-10-12 23:20:54 +0200

    visualization: fix bugs in Tracer API and implement methods used by C++ wrapper

--- ara/visualization/trace/tracer_api/tracer.py
+++ ara/visualization/trace/tracer_api/tracer.py
@@ -8,8 +8,7 @@ from typing import List
 from ara.graph.graph import CFG, SVFG, Callgraph, InstanceGraph
 
 from ara.visualization.trace.trace_type import AlgorithmTrace
-from ara.steps.step import Step
-from ara.visualization.util import GraphTypes
+from ara.graph.mix import GraphTypes
 
 
 @dataclass
@@ -20,29 +19,44 @@ class Entity:
     undo_previous_elems: ResetPartialChangesTraceElement  # Reset operation to undo all changes made by the entity on its last trace
 
 
+def _value_to_graphtypes(value):
+    if type(value) != GraphTypes:
+        if type(value) != int:
+            assert False, f"Unknown GraphTypes type. type is {type(value)}"
+        value = GraphTypes(value)
+    return value
+
+
 @dataclass
 class GraphNode:
     node: Vertex
     graph: GraphTypes
 
+    def __post_init__(self):
+        self.graph = _value_to_graphtypes(self.graph)
+
     def __str__(self):
-        return f"node {self.node} in {self.graph.value}"
+        return f"node {self.node} in {self.graph.name}"
 
 
 @dataclass
 class GraphPath:
-    edges: List[Edge]
+    path: List[Edge]  # argument will be copied
     graph: GraphTypes
+
+    def __post_init__(self):
+        self.graph = _value_to_graphtypes(self.graph)
+        self.path = self.path.copy()
 
     def __str__(self):
         output_str = "path ["
         first_run = True
-        for edge in self.edges:
+        for edge in self.path:
             if not first_run:
                 output_str += ", "
             output_str += str(edge)
             first_run = False
-        output_str += f"] in {self.graph.value}"
+        output_str += f"] in {self.graph.name}"
         return output_str
 
 
@@ -120,18 +134,28 @@ class Tracer:
             actions, self._format_list_output(ent, nodes, "is on "))
 
     def entity_is_looking_at(self, ent: Entity, paths: List[GraphPath]):
+        # TODO: handle null elements and None
         if isinstance(paths, GraphPath):
             paths = [paths]
         self.low_level_trace.add_element(
             None, self._format_list_output(ent, paths, "is looking at "))
 
-    def go_to_node(self, ent: Entity, path: GraphPath):
+    def go_to_node(self, ent: Entity, path: GraphPath, forward: bool = True):
+        """go the path to a node
+        
+        ent:        Entity to highlight
+        path:       path to highlight
+        forward:    True if path is followed in forward direction:
+                        In this case the destination node is the target of the last edge in path.
+                    If you follow input edges, set forward to False:
+                        In this case the destination node is the source of the last edge in path.
+        """
         self.low_level_trace.add_element(
             None,
             self._format_trace_output(
                 ent,
-                f"goes to node {path[len(path) - 1].target()} via path {path}")
-        )
+                f"goes to node {path.path[-1].target() if forward else path.path[-1].source()} via {path}"
+            ))
 
     def duplicate(self, old_entity: Entity, new_name: str) -> Entity:
         # just do a new entity there is no connection to the old entity that can be copied in some way
@@ -160,8 +184,31 @@ class Tracer:
         """
         return self.low_level_trace.add_element(element, log_message)
 
+    def _get_graph_by_type(self, graph_type: int):
+        if graph_type == GraphTypes.SVFG.value:
+            return self.low_level_trace.svfg
+        elif graph_type == GraphTypes.CALLGRAPH.value:
+            return self.low_level_trace.callgraph
+        elif graph_type == GraphTypes.INSTANCE.value:
+            return self.low_level_trace.instances
+        elif graph_type == GraphTypes.ABB.value:
+            return self.low_level_trace.cfg
+        assert False
+        return None
 
-def init_fast_trace(step: Step, name: str = None):
+    def get_vertex_by_id(self, v_id: int, graph_type: int) -> Vertex:
+        """Used by C++ wrapper to get a vertex object by id"""
+        return self._get_graph_by_type(graph_type).vertex(v_id)
+
+    def get_edge_by_nodes(self, source: int, target: int,
+                          graph_type: int) -> Edge:
+        """Used by C++ wrapper to get an edge object by its source and target nodes"""
+        return self._get_graph_by_type(graph_type).edge(
+            source, target
+        )  # TODO: improve performance by e.g. not using Edge object directly
+
+
+def init_fast_trace(step, name: str = None):
     """Same as init_trace() but without graph copies"""
     if name == None:
         name = step.get_name()
@@ -170,7 +217,7 @@ def init_fast_trace(step: Step, name: str = None):
                         graph.svfg)
 
 
-def init_trace(step: Step, name: str = None):
+def init_trace(step, name: str = None):
     """Initializes a new visualization trace. Called on startup of traceable steps.
     Call it with the current step that is running e.g. ini_trace(self)"""
     if name == None:
