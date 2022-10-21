4ae6870e09930b29c9d5829304d56d293df648ec
Jan Neugebauer <jan.neugebauer@stud.uni-hannover.de>
2022-10-21 17:11:07 +0200

    visualization: merge GUIs main() and ARAs main() into one main class for both

--- ara/ara.py
+++ ara/ara.py
@@ -15,131 +15,149 @@ from .os import get_os_names, get_os
 from .steplisting import print_avail_steps
 
 
-def main():
-    """Entry point for ARA."""
-    parser = argparse.ArgumentParser(
-        prog=sys.argv[0],
-        description=sys.modules[__name__].__doc__,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--list-steps', '-l', action="store_true",
-                        default=False, help="list all available steps")
-    parser.add_argument('--verbose', '-v', help="alias for --log-level=info",
-                        action="store_true", default=False)
-    parser.add_argument('--log-level', help="choose the log level",
-                        choices=['warn', 'info', 'debug'],
-                        default=os.environ.get('ARA_LOGLEVEL', 'warn'))
-    parser.add_argument('--dump', action='store_true', default=bool(os.environ.get('ARA_DUMP', '')),
-                        help="emit a meaningful dot graph where possible")
-    parser.add_argument('--dump-prefix', default='dumps/{step_name}.{uuid}.',
-                        help="path that prefixes all dot files. The string "
-                             "'{step_name}' is replaced with the step name. "
-                             "The string '{uuid}' is replace with the uuid.")
-    parser.add_argument('--runtime-stats', action='store_true', default=False,
-                        help="emit statistics about step runtimes.")
-    parser.add_argument('--runtime-stats-file', choices=['logger', 'dump'],
-                        help="Choose whether emit the data with the logging"
-                        " framework or in a separate dump file.",
-                        default='logger')
-    parser.add_argument('--runtime-stats-format', choices=['human', 'json'],
-                        help="Choose whether emit the data in a human readable"
-                        " format or in machine readable JSON.",
-                        default='human')
-    parser.add_argument('--entry-point', '-e', help="system entry point",
-                        default='main')
-    parser.add_argument('--isr', '-i', action='append',
-                        help="entry point for interrupt service routine")
-    parser.add_argument('--step', '-s', action='append',
-                        help="choose steps that will be executed")
-    parser.add_argument('input_file', help="the LLVM-IR input file", nargs='?')
-    parser.add_argument('--oilfile', help="name of oilfile")
-    parser.add_argument('--timings', help="file for ABB timings. "
-                                          "See ApplyTimings for more info.")
-    parser.add_argument('--generator_output', metavar="FILE",
-                        help="file to store generated OS code")
-    parser.add_argument('--step-settings', metavar="FILE", action='append',
-                        help="settings for individual steps. '-' is STDIN")
-    parser.add_argument('--dependency_file',
-                        help="file to write make-style dependencies into for "
-                             "build system integration")
-    parser.add_argument('--ir_output', '-o',
-                        help="File to store modified IR into", metavar="FILE")
-    parser.add_argument('--Werr', help="Treat warnings as errors",
-                        action='store_true')
-    parser.add_argument('--manual-corrections', metavar="FILE",
-                        help="File with manual corrections")
-    parser.add_argument('--trace_algorithm', action='store_true', default=False,
-                        help="Create a trace of supported algorithms for the gui to visualize")
+class Main:
 
-    parser.add_argument('--os', help="the OS of the given application",
-                        choices=get_os_names(), required=True)
+    def __init__(self):
+        self.args = None
+        self.extra_settings = {}
+        self.graph = Graph()
+        self.s_manager = StepManager(self.graph)
 
-    # option for [sysfuncts, systemrelevantfunctions]:
-    parser.add_argument('--with-stubs', help="analyze system functions that "
-                                             "are only stubs. usually, this is"
-                                             " only helpful for debugging "
-                                             "purposes.",
-                        action='store_true', default=False)
-
-    args = parser.parse_args()
-
-    if args.log_level != 'debug' and args.verbose:
-        args.log_level = 'info'
-
-    logger = init_logging(level=args.log_level, root_name='ara',
-                          werr=args.Werr)
-
-    g = Graph()
-    s_manager = StepManager(g)
-    avail_steps = s_manager.get_steps()
-
-    if args.list_steps:
-        print(print_avail_steps(avail_steps))
-        sys.exit(0)
-    elif not args.input_file:
-        parser.error('an input_file is required (except -l or -h is set)')
-
-    logger.debug(f"Processing file: {args.input_file}")
-
-    extra_settings = {}
-
-    if args.step_settings:
-        extra_settings = {}
-        for ssettings in args.step_settings:
-            try:
-                if ssettings == '-':
-                    extra_settings = {**extra_settings, **json.load(sys.stdin)}
-                else:
-                    with open(ssettings) as efile:
-                        extra_settings = {**extra_settings, **json.load(efile)}
-            except Exception as e:
-                parser.error(f'File for --step-settings is malformed: {e}')
-
-    if extra_settings.get("steps", None) and args.step:
-        parser.error("Provide steps either with '--step' or in '--step-settings'.")
-
-    if args.manual_corrections:
-        if args.step:
-            extra_settings["steps"] = [args.step, "ManualCorrections"]
-            args.step = None
+    def main(self, gui=False):
+        """Entry point for ARA."""
+        parser = argparse.ArgumentParser(
+            prog=sys.argv[0],
+            description=sys.modules[__name__].__doc__,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.add_argument('--list-steps', '-l', action="store_true",
+                            default=False, help="list all available steps")
+        parser.add_argument('--verbose', '-v', help="alias for --log-level=info",
+                            action="store_true", default=False)
+        parser.add_argument('--log-level', help="choose the log level",
+                            choices=['warn', 'info', 'debug'],
+                            default=os.environ.get('ARA_LOGLEVEL', 'warn'))
+        parser.add_argument('--dump', action='store_true', default=bool(os.environ.get('ARA_DUMP', '')),
+                            help="emit a meaningful dot graph where possible")
+        parser.add_argument('--dump-prefix', default='dumps/{step_name}.{uuid}.',
+                            help="path that prefixes all dot files. The string "
+                                "'{step_name}' is replaced with the step name. "
+                                "The string '{uuid}' is replace with the uuid.")
+        parser.add_argument('--runtime-stats', action='store_true', default=False,
+                            help="emit statistics about step runtimes.")
+        parser.add_argument('--runtime-stats-file', choices=['logger', 'dump'],
+                            help="Choose whether emit the data with the logging"
+                            " framework or in a separate dump file.",
+                            default='logger')
+        parser.add_argument('--runtime-stats-format', choices=['human', 'json'],
+                            help="Choose whether emit the data in a human readable"
+                            " format or in machine readable JSON.",
+                            default='human')
+        parser.add_argument('--entry-point', '-e', help="system entry point",
+                            default='main')
+        parser.add_argument('--isr', '-i', action='append',
+                            help="entry point for interrupt service routine")
+        parser.add_argument('--step', '-s', action='append',
+                            help="choose steps that will be executed")
+        parser.add_argument('input_file', help="the LLVM-IR input file", nargs='?')
+        parser.add_argument('--oilfile', help="name of oilfile")
+        parser.add_argument('--timings', help="file for ABB timings. "
+                                            "See ApplyTimings for more info.")
+        parser.add_argument('--generator_output', metavar="FILE",
+                            help="file to store generated OS code")
+        parser.add_argument('--step-settings', metavar="FILE", action='append',
+                            help="settings for individual steps. '-' is STDIN")
+        parser.add_argument('--dependency_file',
+                            help="file to write make-style dependencies into for "
+                                "build system integration")
+        parser.add_argument('--ir_output', '-o',
+                            help="File to store modified IR into", metavar="FILE")
+        parser.add_argument('--Werr', help="Treat warnings as errors",
+                            action='store_true')
+        parser.add_argument('--manual-corrections', metavar="FILE",
+                            help="File with manual corrections")
+        if gui:
+            parser.add_argument('--no_trace_algorithm', action='store_true',
+                                default=False, help=
+                                "Create a trace of supported algorithms for the gui to visualize")
         else:
-            extra_settings["steps"].append("ManualCorrections")
+            parser.add_argument('--trace_algorithm', action='store_true', default=False,
+                                help="Create a trace of supported algorithms for the gui to visualize")
 
-    g.os = get_os(args.os)
+        parser.add_argument('--os', help="the OS of the given application",
+                            choices=get_os_names(), required=True)
 
-    if args.step is None and not extra_settings.get("steps", None):
-        args.step = ['SIA']
+        # option for [sysfuncts, systemrelevantfunctions]:
+        parser.add_argument('--with-stubs', help="analyze system functions that "
+                                                "are only stubs. usually, this is"
+                                                " only helpful for debugging "
+                                                "purposes.",
+                            action='store_true', default=False)
 
-    s_args = dict([(x, y) for x, y in vars(args).items() if y is not None])
-    s_manager.execute(s_args, extra_settings, args.step)
+        self.args = parser.parse_args()
 
-    if args.ir_output:
-        s_manager.execute(s_args,
-                          {'steps': [{'name': 'IRWriter',
-                                      'ir_file': args.ir_output}]}, None)
+        if self.args.log_level != 'debug' and self.args.verbose:
+            self.args.log_level = 'info'
 
-    logger.info("History: \n" + "\n".join([f"{se.uuid} {se.name}"
-                                           for se in s_manager.get_history()]))
+        logger = init_logging(level=self.args.log_level, root_name='ara',
+                              werr=self.args.Werr)
+
+        avail_steps = self.s_manager.get_steps()
+
+        if self.args.list_steps:
+            print(print_avail_steps(avail_steps))
+            sys.exit(0)
+        elif not self.args.input_file:
+            parser.error('an input_file is required (except -l or -h is set)')
+
+        logger.debug(f"Processing file: {self.args.input_file}")
+
+        self.extra_settings = {}
+
+        if self.args.step_settings:
+            self.extra_settings = {}
+            for ssettings in self.args.step_settings:
+                try:
+                    if ssettings == '-':
+                        self.extra_settings = {**self.extra_settings, **json.load(sys.stdin)}
+                    else:
+                        with open(ssettings) as efile:
+                            self.extra_settings = {**self.extra_settings, **json.load(efile)}
+                except Exception as e:
+                    parser.error(f'File for --step-settings is malformed: {e}')
+
+        if self.extra_settings.get("steps", None) and self.args.step:
+            parser.error("Provide steps either with '--step' or in '--step-settings'.")
+
+        if self.args.manual_corrections:
+            if self.args.step:
+                self.extra_settings["steps"] = [self.args.step, "ManualCorrections"]
+                self.args.step = None
+            else:
+                self.extra_settings["steps"].append("ManualCorrections")
+
+        self.graph.os = get_os(self.args.os)
+
+        if self.args.step is None and not self.extra_settings.get("steps", None):
+            self.args.step = ['SIA']
+
+        s_args = dict([(x, y) for x, y in vars(self.args).items() if y is not None])
+        
+        if gui:
+            s_args['trace_algorithm'] = not self.args.no_trace_algorithm
+            self.s_manager.init_execution(s_args, self.extra_settings, self.args.step)
+            return
+
+        self.s_manager.execute(s_args, self.extra_settings, self.args.step)
+
+        if self.args.ir_output:
+            self.s_manager.execute(s_args,
+                            {'steps': [{'name': 'IRWriter',
+                                        'ir_file': self.args.ir_output}]}, None)
+
+        logger.info("History: \n" + "\n".join([f"{se.uuid} {se.name}"
+                                            for se in self.s_manager.get_history()]))
 
 
 if __name__ == '__main__':
-    main()
+    main = Main()
+    main.main(gui=False)
--- ara/visualization/ara_manager.py
+++ ara/visualization/ara_manager.py
@@ -12,12 +12,8 @@ from PySide6.QtCore import QObject
 
 from PySide6.QtCore import Slot
 
+from ara.ara import Main
 from .signal import ara_signal
-from .trace.trace_type import AlgorithmTrace
-from ..graph.graph import Graph
-from ..steplisting import print_avail_steps
-from ..stepmanager import StepManager
-from ..util import init_logging
 from .trace import trace_handler
 
 
@@ -27,174 +23,28 @@ class ARAManager(QObject):
         Should be run in its own thread.
     """
 
+    def copy_main_fields(self):
+        self.args = self.main.args
+        self.extra_settings = self.main.extra_settings
+        self.graph = self.main.graph
+        self.s_manager = self.main.s_manager
+
     def __init__(self):
         super().__init__(None)
-        self.args = None
-        self.extra_settings = {}
-        self.graph = Graph()
-        self.s_manager = StepManager(self.graph)
+        self.main = Main()
+        self.copy_main_fields()
 
     @Slot()
     def init(self):
         """
-            Entry Point for ARA.
-            It's mostly a copy of the main in ./ara.py, with slight changes to allow interaction with the gui.
+            Entry Point for ARAs GUI.
         """
+        self.main.main(gui=True)
+        self.copy_main_fields()
 
-        parser = argparse.ArgumentParser(
-            prog=sys.argv[0],
-            description=sys.modules[__name__].__doc__,
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-        parser.add_argument('--list-steps',
-                            '-l',
-                            action="store_true",
-                            default=False,
-                            help="list all available steps")
-        parser.add_argument('--verbose',
-                            '-v',
-                            help="alias for --log-level=info",
-                            action="store_true",
-                            default=False)
-        parser.add_argument('--log-level',
-                            help="choose the log level",
-                            choices=['warn', 'info', 'debug'],
-                            default=os.environ.get('ARA_LOGLEVEL', 'warn'))
-        parser.add_argument('--dump',
-                            action='store_true',
-                            default=bool(os.environ.get('ARA_DUMP', '')),
-                            help="emit a meaningful dot graph where possible")
-        parser.add_argument(
-            '--dump-prefix',
-            default='dumps/{step_name}.{uuid}.',
-            help="path that prefixes all dot files. The string "
-            "'{step_name}' is replaced with the step name. "
-            "The string '{uuid}' is replace with the uuid.")
-        parser.add_argument('--runtime-stats',
-                            action='store_true',
-                            default=False,
-                            help="emit statistics about step runtimes.")
-        parser.add_argument(
-            '--runtime-stats-file',
-            choices=['logger', 'dump'],
-            help="Choose whether emit the data with the logging"
-            " framework or in a separate dump file.",
-            default='logger')
-        parser.add_argument(
-            '--runtime-stats-format',
-            choices=['human', 'json'],
-            help="Choose whether emit the data in a human readable"
-            " format or in machine readable JSON.",
-            default='human')
-        parser.add_argument('--entry-point',
-                            '-e',
-                            help="system entry point",
-                            default='main')
-        parser.add_argument('--isr',
-                            '-i',
-                            action='append',
-                            help="entry point for interrupt service routine")
-        parser.add_argument('--step',
-                            '-s',
-                            action='append',
-                            help="choose steps that will be executed")
-        parser.add_argument('input_file',
-                            help="the LLVM-IR input file",
-                            nargs='?')
-        parser.add_argument('--oilfile', help="name of oilfile")
-        parser.add_argument('--generator_output',
-                            metavar="FILE",
-                            help="file to store generated OS code")
-        parser.add_argument('--step-settings',
-                            metavar="FILE",
-                            action='append',
-                            help="settings for individual steps. '-' is STDIN")
-        parser.add_argument(
-            '--dependency_file',
-            help="file to write make-style dependencies into for "
-            "build system integration")
-        parser.add_argument('--ir_output',
-                            '-o',
-                            help="File to store modified IR into",
-                            metavar="FILE")
-        parser.add_argument('--Werr',
-                            help="Treat warnings as errors",
-                            action='store_true')
-        parser.add_argument('--manual-corrections',
-                            metavar="FILE",
-                            help="File with manual corrections")
-        parser.add_argument(
-            '--no_trace_algorithm',
-            action='store_true',
-            default=False,
-            help=
-            "Create a trace of supported algorithms for the gui to visualize")
-
-        self.args = parser.parse_args()
-
-        if self.args.log_level != 'debug' and self.args.verbose:
-            self.args.log_level = 'info'
-
-        logger = init_logging(level=self.args.log_level,
-                              root_name='ara',
-                              werr=self.args.Werr)
-
-        avail_steps = self.s_manager.get_steps()
-
-        if self.args.list_steps:
-            print(print_avail_steps(avail_steps))
-            sys.exit(0)
-        elif not self.args.input_file:
-            parser.error('an input_file is required (except -l or -h is set)')
-
-        logger.debug(f"Processing file: {self.args.input_file}")
-
-        self.extra_settings = {}
-
-        if self.args.step_settings:
-            self.extra_settings = {}
-            for ssettings in self.args.step_settings:
-                try:
-                    if ssettings == '-':
-                        self.extra_settings = {
-                            **self.extra_settings,
-                            **json.load(sys.stdin)
-                        }
-                    else:
-                        with open(ssettings) as efile:
-                            self.extra_settings = {
-                                **self.extra_settings,
-                                **json.load(efile)
-                            }
-                except Exception as e:
-                    parser.error(f'File for --step-settings is malformed: {e}')
-
-        if self.extra_settings.get("steps", None) and self.args.step:
-            parser.error(
-                "Provide steps either with '--step' or in '--step-settings'.")
-
-        if self.args.manual_corrections:
-            if self.args.step:
-                self.extra_settings["steps"] = [
-                    self.args.step, "ManualCorrections"
-                ]
-                self.args.step = None
-            else:
-                self.extra_settings["steps"].append("ManualCorrections")
-
-        if self.args.step is None and not self.extra_settings.get(
-                "steps", None):
-            self.args.step = ['SIA']
-
-        arg_dict = vars(self.args)
-        arg_dict['trace_algorithm'] = not self.args.no_trace_algorithm
-        self.init_execution(arg_dict, self.extra_settings, self.args.step)
         ara_signal.SIGNAL_MANAGER.sig_init_done.emit()
         ara_signal.SIGNAL_MANAGER.sig_execute_chain.emit(
             self.s_manager.get_execution_chain())
-
-    # @Slot()
-    def init_execution(self, program_config, extra_config, esteps: List[str]):
-        self.s_manager.init_execution(program_config, extra_config, esteps)
 
     def get_trace(self):
         return self.s_manager.get_trace()
