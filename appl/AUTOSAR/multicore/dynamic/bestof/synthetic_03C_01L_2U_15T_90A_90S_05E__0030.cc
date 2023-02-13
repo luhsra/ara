26129c621d4072c3ff7cb0c651951d36d32da396
Björn Fiedler <fiedler@sra.uni-hannover.de>
2023-02-13 13:57:43 +0100

    bestof generated benchmarks: invalid edge descriptor
    ninja
    appl/AUTOSAR/multicore/dynamic/bestof/synthetic_03C_01L_2U_15T_90A_90S_05E__0030.multisse.with_timing.stepdata.json

---
+++ appl/AUTOSAR/multicore/dynamic/bestof/synthetic_03C_01L_2U_15T_90A_90S_05E__0030.cc
@@ -0,0 +1,320 @@
+#include "autosar/os.h"
+#include "test/test.h"
+#include "machine.h"
+
+//#include "fail/trace.h"
+
+void ara_timing_info(int, int);
+
+
+
+DeclareSpinlock(LOCK0);
+
+
+DeclareEvent(EVENT0, 1);
+DeclareEvent(EVENT1, 2);
+DeclareEvent(EVENT2, 3);
+DeclareEvent(EVENT3, 4);
+DeclareEvent(EVENT4, 5);
+
+
+DeclareTask(CPU0Task0Subtask0);
+DeclareTask(CPU0Task0Subtask3);
+DeclareTask(CPU0Task0Subtask5);
+DeclareTask(CPU0Task3Subtask4);
+DeclareTask(CPU0Task3Subtask8);
+DeclareTask(CPU0Task5Subtask2);
+DeclareTask(CPU0Task5Subtask6);
+DeclareTask(CPU0Task5Subtask7);
+DeclareTask(CPU1Task1Subtask1);
+DeclareTask(CPU1Task1Subtask12);
+DeclareTask(CPU1Task1Subtask13);
+DeclareTask(CPU2Task2Subtask11);
+DeclareTask(CPU2Task4Subtask9);
+DeclareTask(CPU2Task4Subtask10);
+DeclareTask(CPU2Task4Subtask14);
+
+
+TEST_MAKE_OS_MAIN( StartOS(0) )
+
+// Prototypes
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU0Task0Subtask0 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU0Task0Subtask3 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU0Task0Subtask5 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU0Task3Subtask4 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU0Task3Subtask8 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU0Task5Subtask2 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU0Task5Subtask6 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU0Task5Subtask7 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU1Task1Subtask1 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU1Task1Subtask12 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU1Task1Subtask13 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU2Task2Subtask11 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU2Task4Subtask10 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU2Task4Subtask14 (void);
+
+noinline EXTERN_C_DECL void AUTOSAR_TASK_FUNC_CPU2Task4Subtask9 (void);
+
+
+
+void AUTOSAR_TASK_FUNC_CPU0Task0Subtask0 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU0Task0Subtask0" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask0.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(20, 148); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask0.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(10, 32); // 0
+  ActivateTask(CPU0Task0Subtask5);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask0.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(10, 194); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask0.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(15, 31); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU0Task0Subtask3 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU0Task0Subtask3" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask3.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(13, 168); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask3.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(20, 58); // 0
+  SetEvent(CPU1Task1Subtask13,EVENT2);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask3.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(7, 163); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask3.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(13, 80); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU0Task0Subtask5 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU0Task0Subtask5" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask5.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(10, 117); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask5.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(16, 154); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task0Subtask5.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(12, 177); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU0Task3Subtask4 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU0Task3Subtask4" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task3Subtask4.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(10, 25); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task3Subtask4.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(12, 64); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task3Subtask4.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(17, 74); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU0Task3Subtask8 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU0Task3Subtask8" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task3Subtask8.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(1, 185); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task3Subtask8.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(10, 118); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task3Subtask8.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(9, 78); // 0
+  WaitEvent(EVENT1);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task3Subtask8.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(11, 148); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU0Task5Subtask2 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU0Task5Subtask2" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask2.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(6, 57); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask2.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(8, 39); // 0
+  GetSpinlock(LOCK0);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask2.4 sys_allowed=False inside_critical=True> x*/
+  ara_timing_info(8, 19); // 1
+  ReleaseSpinlock(LOCK0);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask2.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(16, 59); // 0
+  WaitEvent(EVENT0);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask2.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(1, 17); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask2.5 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(11, 173); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU0Task5Subtask6 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU0Task5Subtask6" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask6.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(14, 27); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask6.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(4, 107); // 0
+  ActivateTask(CPU2Task4Subtask14);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask6.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(8, 99); // 0
+  SetEvent(CPU0Task3Subtask8,EVENT1);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask6.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(6, 29); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask6.4 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(3, 137); // 0
+  ActivateTask(CPU0Task3Subtask8);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask6.5 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(15, 86); // 0
+  SetEvent(CPU2Task4Subtask14,EVENT4);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask6.6 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(15, 110); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU0Task5Subtask7 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU0Task5Subtask7" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask7.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(12, 174); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask7.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(10, 40); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask7.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(16, 30); // 0
+  ActivateTask(CPU0Task5Subtask2);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask7.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(13, 164); // 0
+  ActivateTask(CPU0Task5Subtask6);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU0Task5Subtask7.4 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(16, 111); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU1Task1Subtask1 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU1Task1Subtask1" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask1.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(7, 193); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask1.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(14, 74); // 0
+  WaitEvent(EVENT3);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask1.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(5, 13); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask1.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(1, 68); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU1Task1Subtask12 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU1Task1Subtask12" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask12.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(19, 35); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask12.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(12, 86); // 0
+  ActivateTask(CPU1Task1Subtask13);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask12.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(6, 140); // 0
+  ActivateTask(CPU1Task1Subtask1);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask12.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(13, 199); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask12.4 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(3, 33); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU1Task1Subtask13 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU1Task1Subtask13" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask13.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(3, 117); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask13.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(19, 168); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask13.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(11, 154); // 0
+  WaitEvent(EVENT2);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU1Task1Subtask13.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(11, 156); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU2Task2Subtask11 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU2Task2Subtask11" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task2Subtask11.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(13, 55); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task2Subtask11.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(9, 169); // 0
+  SetEvent(CPU0Task5Subtask2,EVENT0);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task2Subtask11.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(5, 174); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task2Subtask11.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(17, 161); // 0
+  SetEvent(CPU1Task1Subtask1,EVENT3);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task2Subtask11.4 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(15, 17); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU2Task4Subtask10 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU2Task4Subtask10" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask10.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(19, 142); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask10.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(5, 117); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask10.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(9, 183); // 0
+  ActivateTask(CPU0Task0Subtask3);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask10.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(7, 182); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU2Task4Subtask14 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU2Task4Subtask14" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask14.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(1, 172); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask14.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(8, 70); // 0
+  GetSpinlock(LOCK0);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask14.5 sys_allowed=False inside_critical=True> x*/
+  ara_timing_info(1, 15); // 1
+  ReleaseSpinlock(LOCK0);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask14.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(11, 113); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask14.3 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(11, 128); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask14.4 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(13, 26); // 0
+  WaitEvent(EVENT4);
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask14.6 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(20, 109); // 0
+  TerminateTask();
+
+}
+
+void AUTOSAR_TASK_FUNC_CPU2Task4Subtask9 (void) {
+  // kout << "AUTOSAR_TASK_FUNC_CPU2Task4Subtask9" << endl;
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask9.0 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(11, 108); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask9.1 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(15, 95); // 0
+  /* <InsertionPoint AUTOSAR_TASK_FUNC_CPU2Task4Subtask9.2 sys_allowed=True inside_critical=False> x*/
+  ara_timing_info(13, 192); // 0
+  TerminateTask();
+
+}
